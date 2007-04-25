c11a6484b81fb604582d442141cd301f75e30447
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-04-25 18:34:37 +0000

    thumbnail size in the index html
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@15 7f39781d-f577-437e-ae19-be835c7a54ca

--- core/ext/index.ext.php
+++ core/ext/index.ext.php
@@ -134,7 +134,9 @@ class Index extends Extension {
 		$h_view_link = make_link("post/view/{$image->id}", $query);
 		$h_tip = html_escape($image->get_tooltip());
 		$h_thumb_link = $image->get_thumb_link();
-		return "<td><a href='$h_view_link'><img title='$h_tip' alt='$h_tip' src='$h_thumb_link'></a></td>\n";
+		$tsize = get_thumbnail_size($image->width, $image->height);
+		return "<td><a href='$h_view_link'><img title='$h_tip' alt='$h_tip'
+				width='{$tsize[0]}' height='{$tsize[1]}' src='$h_thumb_link'></a></td>\n";
 	}
 // }}}
 // rss {{{
