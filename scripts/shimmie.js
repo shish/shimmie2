5c16e860a112eed4478255baab510bcf5aa4ae53
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-05-08 21:14:53 +0000

    cleaning out scripts
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@80 7f39781d-f577-437e-ae19-be835c7a54ca

--- scripts/shimmie.js
+++ scripts/shimmie.js
@@ -4,8 +4,6 @@ window.onload = function(e) {
 	var sections=get_sections();
 	for(var i=0;i<sections.length;i++) toggle(sections[i]);
 
-//	initAjax("searchBox", "search_completions");
-//	initAjax("tagBox", "upload_completions");
 	initGray("search_input", "Search");
 	initGray("commentBox", "Comment");
 	initGray("tagBox", "tagme");
@@ -17,60 +15,6 @@ window.onload = function(e) {
 	}
 }
 
-
-function endWord(sentance) {
-	words = sentance.split(" ");
-	return words[words.length-1];
-}
-
-var resultCache = new Array();
-resultCache[""] = new Array();
-
-function complete(boxname, text) {
-	box = byId(boxname);
-	words = box.value.split(" ");
-	box.value = "";
-	for(n=0; n<words.length-1; n++) {
-		box.value += words[n]+" ";
-	}
-	box.value += text+" ";
-	box.focus();
-	return false;
-}
-
-function fillCompletionZone(boxname, areaname, results) {
-	byId(areaname).innerHTML = "";
-	for(i=0; i<results.length; i++) {
-		byId(areaname).innerHTML += "<br><a href=\"#\" onclick=\"complete('"+boxname+"', '"+results[i]+"');\">"+results[i]+"</a>";
-	}
-}
-
-function initAjax(boxname, areaname) {
-	var box = byId(boxname);
-	if(!box) return;
-
-	addEvent(
-		box,
-		"keyup", 
-		function f() {
-			starter = endWord(box.value);
-				
-			if(resultCache[starter]) {
-				fillCompletionZone(boxname, areaname, resultCache[starter]);
-			} 
-			else { 
-				ajaxRequest( 
-					"ajax.php?start="+starter, 
-					function g(text) { 
-						resultCache[starter] = text.split("\n");
-						fillCompletionZone(boxname, areaname, resultCache[starter]);
-					} 
-				); 
-			} 
-		},
-		false
-	);
-}
 
 function initGray(boxname, text) {
 	var box = byId(boxname);
@@ -112,20 +56,6 @@ function showUp(elem) {
 	if(e.value.match(/^http|^ftp/)) {
 		e.type = "text";
 		alert("Box is web upload");
-	}
-}
-
-
-function check_int(box, min, max) {
-	check(box, (box.value >= min && box.value <= max));
-}
-
-function check(box, bool) {
-	if(bool) {
-		box.style.background = "#AFA";
-	}
-	else {
-		box.style.background = "#FAA";
 	}
 }
 
