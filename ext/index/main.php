4bc53455ed9b6e7acb9f5725a6bfed439a09cd75
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-07-10 22:33:49 +0000

    index themed
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@257 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/index/main.php
+++ ext/index/main.php
@@ -1,8 +1,11 @@
 <?php
 
 class Index extends Extension {
-// event handling {{{
+	var $theme;
+
 	public function receive_event($event) {
+		if(is_null($this->theme)) $this->theme = get_theme_object("index", "IndexTheme");
+		
 		if(is_a($event, 'PageRequestEvent') && ($event->page == "index")) {
 			$search_terms = array();
 			$page_number = 1;
@@ -14,51 +17,17 @@ class Index extends Extension {
 
 			if(isset($_GET['search'])) {
 				$search_terms = explode(' ', $_GET['search']);
-				$query = "search=".url_escape($_GET['search']);
-			}
-			else {
-				$query = null;
 			}
 
-			global $page;
 			global $config;
 			global $database;
 
 			$total_pages = $database->count_pages($search_terms);
 			$count = $config->get_int('index_width') * $config->get_int('index_height');
 			$images = $database->get_images(($page_number-1)*$count, $count, $search_terms);
-
-			if(count($search_terms) == 0) {
-				$page_title = $config->get_string('title');
-			}
-			else {
-				$page_title = html_escape($_GET['search']);
-				/*
-				$page_title = "";
-				foreach($search_terms as $term) {
-					$u_term = url_escape($term);
-					$h_term = html_escape($term);
-					$page_title .= "<a href='".make_link("post/list", "search=$u_term")."'>$h_term</a>";
-				}
-				*/
-				if(count($images) > 0) {
-					$page->set_subheading("Page $page_number / $total_pages");
-				}
-			}
-			if($page_number > 1 || count($search_terms) > 0) {
-				// $page_title .= " / $page_number";
-			}
 			
-			$page->set_title($page_title);
-			$page->set_heading($page_title);
-			$page->add_block(new Block("Navigation", $this->build_navigation($page_number, $total_pages, $search_terms), "left", 0));
-			if(count($images) > 0) {
-				$page->add_block(new Block("Images", $this->build_table($images, $query), "main", 10));
-				$page->add_block(new Paginator("index", $query, $page_number, $total_pages));
-			}
-			else {
-				$page->add_block(new Block("No Images Found", "No images were found to match the search criteria"));
-			}
+			$this->theme->set_page($page_number, $total_pages, $search_terms);
+			$this->theme->display_page($event->page, $page_title, $images);
 		}
 
 		if(is_a($event, 'SetupBuildingEvent')) {
@@ -81,58 +50,6 @@ class Index extends Extension {
 			$event->config->set_string_from_post("image_tip");
 		}
 	}
-// }}}
-// HTML generation {{{
-	private function build_navigation($page_number, $total_pages, $search_terms) {
-		$prev = $page_number - 1;
-		$next = $page_number + 1;
-
-		$u_tags = url_escape(implode(" ", $search_terms));
-		$query = empty($u_tags) ? null : "search=$u_tags";
-
-		
-		$h_prev = ($page_number <= 1) ? "Prev" : "<a href='".make_link("index/$prev", $query)."'>Prev</a>";
-		$h_index = "<a href='".make_link("index")."'>Index</a>";
-		$h_next = ($page_number >= $total_pages) ? "Next" : "<a href='".make_link("index/$next", $query)."'>Next</a>";
-
-		$h_search_string = count($search_terms) == 0 ? "Search" : html_escape(implode(" ", $search_terms));
-		$h_search_link = make_link("index");
-		$h_search = "
-			<p><form action='$h_search_link' method='GET'>
-				<input id='search_input' name='search' type='text'
-						value='$h_search_string' autocomplete='off' />
-				<input type='submit' value='Find' style='display: none;' />
-			</form>
-			<div id='search_completions'></div>";
-
-		return "$h_prev | $h_index | $h_next<br>$h_search";
-	}
-
-	private function build_table($images, $query) {
-		global $config;
-
-		$width = $config->get_int('index_width');
-		$height = $config->get_int('index_height');
-
-		$table = "<table>\n";
-		for($i=0; $i<$height; $i++) {
-			$table .= "<tr>\n";
-			for($j=0; $j<$width; $j++) {
-				$image = isset($images[$i*$width+$j]) ? $images[$i*$width+$j] : null;
-				if(!is_null($image)) {
-					$table .= "\t<td>" . build_thumb_html($image, $query) . "</td>\n";
-				}
-				else {
-					$table .= "\t<td>&nbsp;</td>\n";
-				}
-			}
-			$table .= "</tr>\n";
-		}
-		$table .= "</table>\n";
-
-		return $table;
-	}
-// }}}
 }
 add_event_listener(new Index());
 ?>
---
+++ ext/index/theme.php
@@ -0,0 +1,91 @@
+<?php
+
+class IndexTheme extends Themelet {
+	public function set_page($page_number, $total_pages, $search_terms) {
+		$this->page_number = $page_number;
+		$this->total_pages = $total_pages;
+		$this->search_terms = $search_terms;
+	}
+
+	public function display_page($page, $page_title, $images) {
+		if(count($this->search_terms) == 0) {
+			$query = null;
+			$page_title = $config->get_string('title');
+		}
+		else {
+			$search_string = implode(' ', $this->search_terms);
+			$query = "search=".url_escape($search_string);
+			$page_title = html_escape($search_string);
+			if(count($images) > 0) {
+				$page->set_subheading("Page $page_number / $total_pages");
+			}
+		}
+		if($page_number > 1 || count($this->search_terms) > 0) {
+			// $page_title .= " / $page_number";
+		}
+
+		$nav = $this->build_navigation($this->page_number, $this->total_pages, $this->search_terms);
+		$page->set_title($page_title);
+		$page->set_heading($page_title);
+		$page->add_block(new Block("Navigation", $nav, "left", 0));
+		if(count($images) > 0) {
+			$page->add_block(new Block("Images", $this->build_table($images, $query), "main", 10));
+			$page->add_block(new Paginator("index", $query, $this->page_number, $this->total_pages));
+		}
+		else {
+			$page->add_block(new Block("No Images Found", "No images were found to match the search criteria"));
+		}
+	}
+
+
+	private function build_navigation($page_number, $total_pages, $search_terms) {
+		$prev = $page_number - 1;
+		$next = $page_number + 1;
+
+		$u_tags = url_escape(implode(" ", $search_terms));
+		$query = empty($u_tags) ? null : "search=$u_tags";
+
+		
+		$h_prev = ($page_number <= 1) ? "Prev" : "<a href='".make_link("index/$prev", $query)."'>Prev</a>";
+		$h_index = "<a href='".make_link("index")."'>Index</a>";
+		$h_next = ($page_number >= $total_pages) ? "Next" : "<a href='".make_link("index/$next", $query)."'>Next</a>";
+
+		$h_search_string = count($search_terms) == 0 ? "Search" : html_escape(implode(" ", $search_terms));
+		$h_search_link = make_link("index");
+		$h_search = "
+			<p><form action='$h_search_link' method='GET'>
+				<input id='search_input' name='search' type='text'
+						value='$h_search_string' autocomplete='off' />
+				<input type='submit' value='Find' style='display: none;' />
+			</form>
+			<div id='search_completions'></div>";
+
+		return "$h_prev | $h_index | $h_next<br>$h_search";
+	}
+
+	private function build_table($images, $query) {
+		global $config;
+
+		$width = $config->get_int('index_width');
+		$height = $config->get_int('index_height');
+
+		$table = "<table>\n";
+		for($i=0; $i<$height; $i++) {
+			$table .= "<tr>\n";
+			for($j=0; $j<$width; $j++) {
+				$image = isset($images[$i*$width+$j]) ? $images[$i*$width+$j] : null;
+				if(!is_null($image)) {
+					$table .= "\t<td>" . build_thumb_html($image, $query) . "</td>\n";
+				}
+				else {
+					$table .= "\t<td>&nbsp;</td>\n";
+				}
+			}
+			$table .= "</tr>\n";
+		}
+		$table .= "</table>\n";
+
+		return $table;
+	}
+}
+?>
