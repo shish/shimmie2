6dd48c2335911daefe20bf180c6fa7f5ad6af0c3
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-07-15 18:46:28 +0000

    Image->get_x_link functions should return escaped urls
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@281 7f39781d-f577-437e-ae19-be835c7a54ca

--- core/image.class.php
+++ core/image.class.php
@@ -92,17 +92,17 @@ class Image {
 
 	public function get_image_link() {
 		global $config;
-		return $this->parse_link_template($config->get_string('image_ilink'));
+		return url_escape($this->parse_link_template($config->get_string('image_ilink')));
 	}
 
 	public function get_short_link() {
 		global $config;
-		return $this->parse_link_template($config->get_string('image_slink'));
+		return url_escape($this->parse_link_template($config->get_string('image_slink')));
 	}
 
 	public function get_thumb_link() {
 		global $config;
-		return $this->parse_link_template($config->get_string('image_tlink'));
+		return url_escape($this->parse_link_template($config->get_string('image_tlink')));
 	}
 
 	public function get_tooltip() {
