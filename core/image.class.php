061ed4a3fe1975ff31b5070accba32154874b4c5
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-07-15 19:09:57 +0000

    wait, no
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@282 7f39781d-f577-437e-ae19-be835c7a54ca

--- core/image.class.php
+++ core/image.class.php
@@ -92,17 +92,17 @@ class Image {
 
 	public function get_image_link() {
 		global $config;
-		return url_escape($this->parse_link_template($config->get_string('image_ilink')));
+		return $this->parse_link_template($config->get_string('image_ilink'));
 	}
 
 	public function get_short_link() {
 		global $config;
-		return url_escape($this->parse_link_template($config->get_string('image_slink')));
+		return $this->parse_link_template($config->get_string('image_slink'));
 	}
 
 	public function get_thumb_link() {
 		global $config;
-		return url_escape($this->parse_link_template($config->get_string('image_tlink')));
+		return $this->parse_link_template($config->get_string('image_tlink'));
 	}
 
 	public function get_tooltip() {
