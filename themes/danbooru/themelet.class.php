91f1ae986ae7f526f77ab81be4194a34cb296ab4
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-07-17 17:57:13 +0000

    add_x_block -> add_block
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@317 7f39781d-f577-437e-ae19-be835c7a54ca

--- themes/danbooru/themelet.class.php
+++ themes/danbooru/themelet.class.php
@@ -4,8 +4,8 @@ class Themelet {
 	public function display_error($page, $title, $message) {
 		$page->set_title($title);
 		$page->set_heading($title);
-		$page->add_side_block(new NavBlock());
-		$page->add_main_block(new Block("Error", $message));
+		$page->add_block(new NavBlock());
+		$page->add_block(new Block("Error", $message));
 	}
 }
 ?>
