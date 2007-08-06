461e56eb00774da7b44435349379dfcbefb72a16
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-08-06 19:16:34 +0000

    commas~
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@435 7f39781d-f577-437e-ae19-be835c7a54ca

--- themes/danbooru/tag_list.theme.php
+++ themes/danbooru/tag_list.theme.php
@@ -20,7 +20,7 @@ class TagListTheme extends Themelet {
 		$page->disable_left();
 		$page->set_title("Tag List");
 		$page->set_heading($this->heading);
-		$page->add_block(new Block("Navigation", str_replace("<br>", " ", $this->navigation), "main", 0));
+		$page->add_block(new Block("Navigation", str_replace("<br>", ", ", $this->navigation), "main", 0));
 		$page->add_block(new Block("&nbsp;", $this->list));
 	}
 
