e7a7617ae63f2ee89bc29be5a0b3eccbf7afe8c8
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-08-15 11:45:36 +0000

    rss generator string
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@458 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/rss_images/main.php
+++ ext/rss_images/main.php
@@ -50,14 +50,14 @@ class RSS_Images extends Extension {
 
 		$title = $config->get_string('title');
 		$base_href = $config->get_string('base_href');
-		$version = $config->get_string('version');
+		$version = VERSION;
 		$xml = "<"."?xml version=\"1.0\" encoding=\"utf-8\" ?".">
 <rss version=\"2.0\">
     <channel>
         <title>$title</title>
         <description>The latest uploads to the image board</description>
 		<link>$base_href</link>
-		<generator>$version</generator>
+		<generator>Shimmie-$version</generator>
 		<copyright>(c) 2007 Shish</copyright>
 		$data
 	</channel>
