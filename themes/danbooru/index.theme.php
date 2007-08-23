4191df847f295f61e730fca9a52279b9d461c926
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-08-23 11:15:18 +0000

    danbooru theme to 2.1
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@467 7f39781d-f577-437e-ae19-be835c7a54ca

--- themes/danbooru/index.theme.php
+++ themes/danbooru/index.theme.php
@@ -1,12 +1,6 @@
 <?php
 
-class IndexTheme extends Themelet {
-	public function set_page($page_number, $total_pages, $search_terms) {
-		$this->page_number = $page_number;
-		$this->total_pages = $total_pages;
-		$this->search_terms = $search_terms;
-	}
-
+class CustomIndexTheme extends IndexTheme {
 	public function display_page($page, $images) {
 		global $config;
 
@@ -40,7 +34,7 @@ class IndexTheme extends Themelet {
 	}
 
 
-	private function build_navigation($page_number, $total_pages, $search_terms) {
+	protected function build_navigation($page_number, $total_pages, $search_terms) {
 		$h_search_string = count($search_terms) == 0 ? "" : html_escape(implode(" ", $search_terms));
 		$h_search_link = make_link();
 		$h_search = "
@@ -52,31 +46,6 @@ class IndexTheme extends Themelet {
 			<div id='search_completions'></div>";
 
 		return $h_search;
-	}
-
-	private function build_table($images, $query) {
-		global $config;
-
-		$width = $config->get_int('index_width');
-		$height = $config->get_int('index_height');
-
-		$table = "<table>\n";
-		for($i=0; $i<$height; $i++) {
-			$table .= "<tr>\n";
-			for($j=0; $j<$width; $j++) {
-				$image = isset($images[$i*$width+$j]) ? $images[$i*$width+$j] : null;
-				if(!is_null($image)) {
-					$table .= "\t<td>" . build_thumb_html($image, $query) . "</td>\n";
-				}
-				else {
-					$table .= "\t<td>&nbsp;</td>\n";
-				}
-			}
-			$table .= "</tr>\n";
-		}
-		$table .= "</table>\n";
-
-		return $table;
 	}
 }
 ?>
