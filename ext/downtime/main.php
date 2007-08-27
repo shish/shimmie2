d51f2b30322bafd27af658349405bd3619a2aba9
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-08-27 23:45:04 +0000

    downtime reacts to PageRequestEvent
    git-svn-id: file:///home/shish/svn/shimmie2/branches/branch_2.1@486 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/downtime/main.php
+++ ext/downtime/main.php
@@ -6,17 +6,17 @@ class Downtime extends Extension {
 	public function receive_event($event) {
 		if(is_null($this->theme)) $this->theme = get_theme_object("downtime", "DowntimeTheme");
 
-		$this->check_downtime($event);
-
 		if(is_a($event, 'SetupBuildingEvent')) {
 			$sb = new SetupBlock("Downtime");
 			$sb->add_bool_option("downtime", "Disable non-admin access: ");
 			$sb->add_longtext_option("downtime_message", "<br>");
 			$event->panel->add_block($sb);
 		}
+
 		if(is_a($event, 'PageRequestEvent')) {
 			global $config;
 			if($config->get_bool("downtime")) {
+				$this->check_downtime($event);
 				$this->theme->display_notification($event->page);
 			}
 		}
