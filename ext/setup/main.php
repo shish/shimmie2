50b9880d2dfebf3108ec44edd4a42e66587f41ec
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-10-27 23:42:18 +0000

    set default theme to 'default' (in branch_2.1)
    git-svn-id: file:///home/shish/svn/shimmie2/branches/branch_2.1@581 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/setup/main.php
+++ ext/setup/main.php
@@ -134,6 +134,7 @@ class Setup extends Extension {
 			$config->set_default_string("front_page", "post/list");
 			$config->set_default_string("main_page", "post/list");
 			$config->set_default_string("base_href", "./index.php?q=");
+			$config->set_default_string("theme", "default");
 		}
 
 		if(is_a($event, 'PageRequestEvent') && ($event->page_name == "setup")) {
