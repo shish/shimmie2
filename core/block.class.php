0d126084ce7dedbea499cb9a5e52eff784d8853b
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-12-04 22:24:58 +0000

    move subclass to below main class, for consistency with later work
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@622 7f39781d-f577-437e-ae19-be835c7a54ca

--- core/block.class.php
+++ core/block.class.php
@@ -12,4 +12,13 @@ class Block {
 		$this->position = $position;
 	}
 }
+
+class NavBlock extends Block {
+	public function NavBlock() {
+		$this->header = "Navigation";
+		$this->body = "<a href='".make_link()."'>Index</a>";
+		$this->section = "left";
+		$this->position = 0;
+	}
+}
 ?>
