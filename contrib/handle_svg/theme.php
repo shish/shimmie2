b2ef2502db84c7d7b2bc1ef3525c32213d2f05e5
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-12-13 23:27:22 +0000

    proper tags, no need for loader hack?
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@660 7f39781d-f577-437e-ae19-be835c7a54ca

--- contrib/handle_svg/theme.php
+++ contrib/handle_svg/theme.php
@@ -3,10 +3,11 @@
 class SVGFileHandlerTheme extends Themelet {
 	public function display_image($page, $image) {
 		$link = make_link("get_svg/{$image->id}/{$image->id}.svg");
+		$ilink = $image->get_image_link();
 		// FIXME: object and embed have "height" and "width"
 		$html = "
-			<object data='$link' type='image/svg+xml' width='300' height='300' />
-			    <embed src='$link' type='image/svg+xml' width='300' height='300' />
+			<object data='$ilink' type='image/svg+xml' width='300' height='300'>
+			    <embed src='$ilink' type='image/svg+xml' width='300' height='300' />
 			</object>
 		";
 		$page->add_block(new Block("Image", $html, "main", 0));
