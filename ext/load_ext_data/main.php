c49fd1178a5891d3bf5f5e01f19e7c153bb0e634
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2007-12-30 11:51:05 +0000

    stable too
    git-svn-id: file:///home/shish/svn/shimmie2/branches/branch_2.1@672 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/load_ext_data/main.php
+++ ext/load_ext_data/main.php
@@ -6,12 +6,18 @@ class LoadExtData extends Extension {
 
 			$data_href = get_base_href();
 
-			foreach(glob("ext/*/style.css") as $css_file) {
-				$page->add_header("<link rel='stylesheet' href='$data_href/$css_file' type='text/css'>");
+			$css_files = glob("ext/*/style.css");
+			if($css_files) {
+				foreach($css_files as $css_file) {
+					$page->add_header("<link rel='stylesheet' href='$data_href/$css_file' type='text/css'>");
+				}
 			}
 
-			foreach(glob("ext/*/script.js") as $js_file) {
-				$page->add_header("<script src='$data_href/$js_file' type='text/javascript'></script>");
+			$js_files = glob("ext/*/script.js");
+			if($js_files) {
+				foreach($js_files as $js_file) {
+					$page->add_header("<script src='$data_href/$js_file' type='text/javascript'></script>");
+				}
 			}
 		}
 	}
