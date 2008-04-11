1fd2fe084b81ce95e4378b3ff441bf365d735c50
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-04-11 05:47:16 +0000

    apply cleanups to 2.2
    git-svn-id: file:///home/shish/svn/shimmie2/branches/branch_2.2@807 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/admin/theme.php
+++ ext/admin/theme.php
@@ -25,5 +25,25 @@ class AdminPageTheme extends Themelet {
 		";
 		$page->add_block(new Block("Admin", $html, "left"));
 	}
+
+	/*
+	 * Show a form which links to admin_utils with POST[action] set to one of:
+	 *  'lowercase all tags'
+	 *  'recount tag use'
+	 *  'purge unused tags'
+	 */
+	public function display_form($page) {
+		$html = "
+			<p><form action='".make_link("admin_utils")."' method='POST'>
+				<select name='action'>
+					<option value='lowercase all tags'>All tags to lowercase</option>
+					<option value='recount tag use'>Recount tag use</option>
+					<option value='purge unused tags'>Purge unused tags</option>
+				</select>
+				<input type='submit' value='Go'>
+			</form>
+		";
+		$page->add_block(new Block("Misc Admin Tools", $html));
+	}
 }
 ?>
---
+++ themes/default/script.js
@@ -0,0 +1,136 @@
+var defaultTexts = new Array();
+
+window.onload = function(e) {
+	var sections=get_sections();
+	for(var i=0;i<sections.length;i++) toggle(sections[i]);
+
+	initGray("search_input", "Search");
+	initGray("commentBox", "Comment");
+	initGray("tagBox", "tagme");
+	
+	// if we're going to show with JS, hide with JS first
+	pass_confirm = byId("pass_confirm");
+	if(pass_confirm) {
+		pass_confirm.style.display = "none";
+	}
+}
+
+
+function initGray(boxname, text) {
+	var box = byId(boxname);
+	if(!box) return;
+
+	var clr = function () {cleargray(box, text);};
+	var set = function () {setgray(box, text);};
+
+	addEvent(box, "focus", clr, false);
+	addEvent(box, "blur",  set, false);
+
+	if(box.value == text) {
+		box.style.color = "#999";
+		box.style.textAlign = "center";
+	}
+	else {
+		box.style.color = "#000";
+		box.style.textAlign = "left";
+	}
+}
+
+function cleargray(box, text) {
+	if(box.value == text) {
+		box.value = "";
+		box.style.color = "#000";
+		box.style.textAlign = "left";
+	}
+}
+function setgray(box, text) {
+	if(box.value == "") {
+		box.style.textAlign = "center";
+		box.style.color = "gray";
+		box.value = text;
+	}
+}
+
+function showUp(elem) {
+	e = document.getElementById(elem)
+	if(!e) return;
+	e.style.display = "";
+//	alert(e.type+": "+e.value);
+	if(e.value.match(/^http|^ftp/)) {
+		e.type = "text";
+		alert("Box is web upload");
+	}
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+*                              LibShish-JS                                  *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+function addEvent(obj, event, func, capture){
+	if (obj.addEventListener){
+		obj.addEventListener(event, func, capture);
+	} else if (obj.attachEvent){
+		obj.attachEvent("on"+event, func);
+	}
+}
+
+
+function byId(id) {
+	return document.getElementById(id);
+}
+
+
+function getHTTPObject() { 
+	if (window.XMLHttpRequest){
+		return new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject){
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
+
+function ajaxRequest(url, callback) {
+	var http = getHTTPObject();
+	http.open("GET", url, true);
+	http.onreadystatechange = function() {
+		if(http.readyState == 4) callback(http.responseText);
+	}
+	http.send(null);
+}
+
+
+/* get, set, and delete cookies */
+function getCookie( name ) {
+	var start = document.cookie.indexOf( name + "=" );
+	var len = start + name.length + 1;
+	if ( ( !start ) && ( name != document.cookie.substring( 0, name.length ) ) ) {
+		return null;
+	}
+	if ( start == -1 ) return null;
+	var end = document.cookie.indexOf( ";", len );
+	if ( end == -1 ) end = document.cookie.length;
+	return unescape( document.cookie.substring( len, end ) );
+}
+	
+function setCookie( name, value, expires, path, domain, secure ) {
+	var today = new Date();
+	today.setTime( today.getTime() );
+	if ( expires ) {
+		expires = expires * 1000 * 60 * 60 * 24;
+	}
+	var expires_date = new Date( today.getTime() + (expires) );
+	document.cookie = name+"="+escape( value ) +
+		( ( expires ) ? ";expires="+expires_date.toGMTString() : "" ) + //expires.toGMTString()
+		( ( path ) ? ";path=" + path : "" ) +
+		( ( domain ) ? ";domain=" + domain : "" ) +
+		( ( secure ) ? ";secure" : "" );
+}
+	
+function deleteCookie( name, path, domain ) {
+	if ( getCookie( name ) ) document.cookie = name + "=" +
+			( ( path ) ? ";path=" + path : "") +
+			( ( domain ) ? ";domain=" + domain : "" ) +
+			";expires=Thu, 01-Jan-1970 00:00:01 GMT";
+}
+
