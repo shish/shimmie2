59a0979d7fefc299ce4c0f1f8d99c5bac1214324
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-04-11 05:36:36 +0000

    marge admin utils into admin
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@804 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/admin/main.php
+++ ext/admin/main.php
@@ -40,6 +40,27 @@ class AdminPage extends Extension {
 			}
 		}
 
+		if(is_a($event, 'PageRequestEvent') && ($event->page_name == "admin_utils")) {
+			if($event->user->is_admin()) {
+				set_time_limit(0);
+				
+				switch($_POST['action']) {
+					case 'lowercase all tags':
+						$this->lowercase_all_tags();
+						break;
+					case 'recount tag use':
+						$this->recount_tag_use();
+						break;
+					case 'purge unused tags':
+						$this->purge_unused_tags();
+						break;
+				}
+
+				$event->page->set_mode("redirect");
+				$event->page->set_redirect(make_link("admin"));
+			}
+		}
+
 		if(is_a($event, 'DisplayingImageEvent')) {
 			global $user;
 			if($user->is_admin()) {
@@ -49,11 +70,40 @@ class AdminPage extends Extension {
 
 		if(is_a($event, 'AdminBuildingEvent')) {
 			$this->theme->display_page($event->page);
+			$this->theme->display_form($event->page);
 		}
 
 		if(is_a($event, 'UserBlockBuildingEvent')) {
 			if($event->user->is_admin()) {
 				$event->add_link("Board Admin", make_link("admin"));
+			}
+		}
+	}
+
+	private function lowercase_all_tags() {
+		global $database;
+		$database->execute("UPDATE tags SET tag=lower(tag)");
+	}
+
+	private function recount_tag_use() {
+		global $database;
+		$database->Execute("UPDATE tags SET count=(SELECT COUNT(image_id) FROM image_tags WHERE tag_id=tags.id GROUP BY tag_id)");
+	}
+
+	private function purge_unused_tags() {
+		global $database;
+		$this->recount_tag_use();
+		$database->Execute("DELETE FROM tags WHERE count=0");
+	}
+
+	private function check_for_orphanned_images() {
+		$orphans = array();
+		foreach(glob("images/*") as $dir) {
+			foreach(glob("$dir/*") as $file) {
+				$hash = str_replace("$dir/", "", $file);
+				if(!$this->db_has_hash($hash)) {
+					$orphans[] = $hash;
+				}
 			}
 		}
 	}
