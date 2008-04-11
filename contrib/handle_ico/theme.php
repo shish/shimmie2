09f768afdde59e8e8c2fc412b4d022a05db65479
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-04-11 01:49:18 +0000

    handle icons as objects in the hope of making ie7 happy
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@797 7f39781d-f577-437e-ae19-be835c7a54ca

--- contrib/handle_ico/theme.php
+++ contrib/handle_ico/theme.php
@@ -3,7 +3,13 @@
 class IcoFileHandlerTheme extends Themelet {
 	public function display_image($page, $image) {
 		$ilink = $image->get_image_link();
-		$html = "<img id='main_image' src='$ilink'>";
+		$width = $image->width;
+		$height = $image->height;
+		$html = "
+			<object data='$ilink' type='image/x-icon' width='$width' height='$height'>
+			    <embed src='$ilink' type='image/x-icon' width='$width' height='$height' />
+			</object>
+		";
 		$page->add_block(new Block("Image", $html, "main", 0));
 	}
 }
