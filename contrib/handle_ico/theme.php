7edf9060659f7f0f671144afcd90358fbfb6df65
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-04-14 10:50:46 +0000

    manually set the mime type when serving .ico files
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@817 7f39781d-f577-437e-ae19-be835c7a54ca

--- contrib/handle_ico/theme.php
+++ contrib/handle_ico/theme.php
@@ -2,13 +2,9 @@
 
 class IcoFileHandlerTheme extends Themelet {
 	public function display_image($page, $image) {
-		$ilink = $image->get_image_link();
-		$width = $image->width;
-		$height = $image->height;
+		$ilink = make_link("get_ico/{$image->id}/{$image->id}.ico");
 		$html = "
-			<object data='$ilink' type='image/x-icon' width='$width' height='$height'>
-			    <embed src='$ilink' type='image/x-icon' width='$width' height='$height' />
-			</object>
+			<img id='main_image' src='$ilink'>
 		";
 		$page->add_block(new Block("Image", $html, "main", 0));
 	}
