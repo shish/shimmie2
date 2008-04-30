457d5d8ad98a66f1225b345c24ae05ee70017971
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-04-30 12:33:19 +0000

    a couple more bbcode tags
    git-svn-id: file:///home/shish/svn/shimmie2/branches/branch_2.2@837 7f39781d-f577-437e-ae19-be835c7a54ca

--- ext/bbcode/main.php
+++ ext/bbcode/main.php
@@ -12,6 +12,7 @@ class BBCode extends Extension {
 		$text = preg_replace("/\[b\](.*?)\[\/b\]/s", "<b>\\1</b>", $text);
 		$text = preg_replace("/\[i\](.*?)\[\/i\]/s", "<i>\\1</i>", $text);
 		$text = preg_replace("/\[u\](.*?)\[\/u\]/s", "<u>\\1</u>", $text);
+		$text = preg_replace("/\[s\](.*?)\[\/s\]/s", "<s>\\1</s>", $text);
 		$text = preg_replace("/\[code\](.*?)\[\/code\]/s", "<pre>\\1</pre>", $text);
 		$text = preg_replace("/&gt;&gt;(\d+)/s", "<a href='".make_link("post/view/\\1")."'>&gt;&gt;\\1</a>", $text);
 		$text = preg_replace("/&gt;&gt;([^\d].+)/", "<blockquote><small>\\1</small></blockquote>", $text);
@@ -25,6 +26,7 @@ class BBCode extends Extension {
 		$text = preg_replace("/\[h2\](.*?)\[\/h2\]/s", "<h2>\\1</h2>", $text);
 		$text = preg_replace("/\[h3\](.*?)\[\/h3\]/s", "<h3>\\1</h3>", $text);
 		$text = preg_replace("/\[h4\](.*?)\[\/h4\]/s", "<h4>\\1</h4>", $text);
+		$text = preg_replace("/\[list\](.*?)\[\/list\]/s", "<ul>\\1</ul>", $text);
 		$text = preg_replace("/\[ul\](.*?)\[\/ul\]/s", "<ul>\\1</ul>", $text);
 		$text = preg_replace("/\[ol\](.*?)\[\/ol\]/s", "<ol>\\1</ol>", $text);
 		$text = preg_replace("/\[li\](.*?)\[\/li\]/s", "<li>\\1</li>", $text);
@@ -38,6 +40,7 @@ class BBCode extends Extension {
 		$text = preg_replace("/\[b\](.*?)\[\/b\]/s", "\\1", $text);
 		$text = preg_replace("/\[i\](.*?)\[\/i\]/s", "\\1", $text);
 		$text = preg_replace("/\[u\](.*?)\[\/u\]/s", "\\1", $text);
+		$text = preg_replace("/\[s\](.*?)\[\/s\]/s", "\\1", $text);
 		$text = preg_replace("/\[code\](.*?)\[\/code\]/s", "\\1", $text);
 		$text = preg_replace("/\[url=(.*?)\](.*?)\[\/url\]/s", "\\2", $text);
 		$text = preg_replace("/\[url\](.*?)\[\/url\]/s", "\\1", $text);
@@ -48,6 +51,7 @@ class BBCode extends Extension {
 		$text = preg_replace("/\[h2\](.*?)\[\/h2\]/s", "\\1", $text);
 		$text = preg_replace("/\[h3\](.*?)\[\/h3\]/s", "\\1", $text);
 		$text = preg_replace("/\[h4\](.*?)\[\/h4\]/s", "\\1", $text);
+		$text = preg_replace("/\[list\](.*?)\[\/list\]/s", "\\1", $text);
 		$text = preg_replace("/\[ul\](.*?)\[\/ul\]/s", "\\1", $text);
 		$text = preg_replace("/\[ol\](.*?)\[\/ol\]/s", "\\1", $text);
 		$text = preg_replace("/\[li\](.*?)\[\/li\]/s", "\\1", $text);
