058f2ff1edcc8c9a848fce4e8ac954c92ab1f8b9
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-07-24 15:35:57 +0000

    more compat
    git-svn-id: file:///home/shish/svn/shimmie2/branches/branch_2.2@927 7f39781d-f577-437e-ae19-be835c7a54ca

--- contrib/numeric_score/main.php
+++ contrib/numeric_score/main.php
@@ -105,7 +105,10 @@ class NumericScore extends Extension {
 	private function add_vote($image_id, $user_id, $score) {
 		global $database;
 		$database->Execute(
-			"REPLACE INTO numeric_score_votes(image_id, user_id, score) VALUES(?, ?, ?)",
+			"DELETE FROM numeric_score_votes WHERE image_id=? AND user_id=?",
+			array($image_id, $user_id));
+		$database->Execute(
+			"INSERT INTO numeric_score_votes(image_id, user_id, score) VALUES(?, ?, ?)",
 			array($image_id, $user_id, $score));
 		$database->Execute(
 			"UPDATE images SET numeric_score=(SELECT SUM(score) FROM numeric_score_votes WHERE image_id=?) WHERE id=?",
