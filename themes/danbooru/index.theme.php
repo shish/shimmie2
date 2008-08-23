d43edb8c7f20256a001e2911d7691c9c3878a47c
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-08-23 06:44:10 +0000

    fix this bug in danbooru too
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@998 7f39781d-f577-437e-ae19-be835c7a54ca

--- themes/danbooru/index.theme.php
+++ themes/danbooru/index.theme.php
@@ -41,6 +41,7 @@ class CustomIndexTheme extends IndexTheme {
 			<p><form action='$h_search_link' method='GET'>
 				<input name='search' type='text'
 						value='$h_search_string' autocomplete='off' />
+				<input type='hidden' name='q' value='/post/list'>
 				<input type='submit' value='Find' style='display: none;' />
 			</form>
 			<div id='search_completions'></div>";
