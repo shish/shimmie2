b37e9e859fff145b50cd4e883ebcd95273cf6bfd
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-10-18 11:43:44 +0000

    numbered and searched rss results
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@1099 7f39781d-f577-437e-ae19-be835c7a54ca

--- contrib/rss_images/test.php
+++ contrib/rss_images/test.php
@@ -4,6 +4,18 @@ class RSSImagesTest extends WebTestCase {
         $this->get(TEST_BASE.'/rss/images');
 		$this->assertMime("application/rss+xml");
 		$this->assertNoText("Exception");
+
+        $this->get(TEST_BASE.'/rss/images/1');
+		$this->assertMime("application/rss+xml");
+		$this->assertNoText("Exception");
+
+        $this->get(TEST_BASE.'/rss/images/tagme/1');
+		$this->assertMime("application/rss+xml");
+		$this->assertNoText("Exception");
+
+        $this->get(TEST_BASE.'/rss/images/tagme/2');
+		$this->assertMime("application/rss+xml");
+		$this->assertNoText("Exception");
     }
 }
 ?>
