87830b7fe06b800f55c0dcfeae93bab0bf1035ce
shish <shish@7f39781d-f577-437e-ae19-be835c7a54ca>
2008-12-12 19:45:55 +0000

    copyright notice for this year <_<
    git-svn-id: file:///home/shish/svn/shimmie2/trunk@1132 7f39781d-f577-437e-ae19-be835c7a54ca

--- themes/default/layout.class.php
+++ themes/default/layout.class.php
@@ -64,7 +64,7 @@ $header_html
 			<hr>
 			Images &copy; their respective owners,
 			<a href="http://trac.shishnet.org/shimmie2/">$version</a> &copy; 
-			<a href="http://www.shishnet.org/">Shish</a> 2007,
+			<a href="http://www.shishnet.org/">Shish</a> 2007-2008,
 			based on the <a href="http://trac.shishnet.org/shimmie2/wiki/DanbooruRipoff">Danbooru</a> concept.
 			$debug
 			$contact
