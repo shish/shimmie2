fc9eddd08b7026f7f166ef85dc14ac9ece4fc937
Shish <webmaster@shishnet.org>
2009-01-04 08:33:32 -0800

    scorify the emoticons extension

--- contrib/emoticons/main.php
+++ contrib/emoticons/main.php
@@ -6,22 +6,14 @@
  * Description: Turn :smile: into a link to smile.gif
  */
 
-class Emoticons implements Extension {
-	public function receive_event(Event $event) {
-		if($event instanceof TextFormattingEvent) {
-			$event->formatted = $this->bbcode_to_html($event->formatted);
-			$event->stripped  = $this->bbcode_to_text($event->stripped);
-		}
-	}
-	
-	private function bbcode_to_html($text) {
+class Emoticons extends FormatterExtension {
+	public function format($text) {
 		$data_href = get_base_href();
 		$text = preg_replace("/:([a-z]*?):/s", "<img src='$data_href/ext/emoticons/default/\\1.gif'>", $text);
 		return $text;
 	}
 
-	private function bbcode_to_text($text) {
-		// $text = preg_replace("/:([a-z]*?):/s", "\\1", $text);
+	public function strip($text) {
 		return $text;
 	}
 }
