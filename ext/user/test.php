738fa3ef1e525b5656e01d432a55c5572ab46a5e
Shish <webmaster@shishnet.org>
2009-01-04 11:38:47 -0800

    whitespace...

--- ext/user/test.php
+++ ext/user/test.php
@@ -1,23 +1,23 @@
 <?php
 class UserPageTest extends WebTestCase {
 	function testUserPage() {
-        $this->get(TEST_BASE.'/user');
-        $this->assertTitle("Anonymous's Page");
+		$this->get(TEST_BASE.'/user');
+		$this->assertTitle("Anonymous's Page");
 		$this->assertNoText("Options");
 		$this->assertNoText("More Options");
 
-        $this->get(TEST_BASE.'/user/Shish');
-        $this->assertTitle("Shish's Page");
+		$this->get(TEST_BASE.'/user/Shish');
+		$this->assertTitle("Shish's Page");
 
-        $this->get(TEST_BASE.'/user/MauMau');
-        $this->assertTitle("No Such User");
+		$this->get(TEST_BASE.'/user/MauMau');
+		$this->assertTitle("No Such User");
 
 		$this->assertText("Login");
 		$this->setField('user', USER_NAME);
 		$this->setField('pass', USER_PASS);
 		$this->click("Log In");
 		// should be on the user page
-        $this->assertTitle("test's Page");
+		$this->assertTitle("test's Page");
 		$this->assertText("Options");
 		$this->assertNoText("More Options");
 		$this->click('Log Out');
@@ -27,7 +27,7 @@ class UserPageTest extends WebTestCase {
 		$this->setField('pass', ADMIN_PASS);
 		$this->click("Log In");
 		// should be on the user page
-        $this->assertTitle(ADMIN_NAME+"'s Page");
+		$this->assertTitle(ADMIN_NAME+"'s Page");
 		$this->assertText("Options");
 		$this->assertText("More Options");
 		$this->click('Log Out');
