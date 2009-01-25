63a4ca2587daaf98f1ad2f12efb0f023958a28d7
Shish <shish@shishnet.org>
2009-01-25 12:37:45 +0000

    preliminary hash ban testing

---
+++ contrib/image_hash_ban/test.php
@@ -0,0 +1,19 @@
+<?php
+class ImageHashBanUnitTest extends UnitTestCase {
+	public function _broken_testUploadFailsWhenBanned() {
+		$ihb = new ImageHashBan();
+		$due = new DataUploadEvent();
+
+		try {
+			$ihb->receive_event($due);
+			$this->assertTrue(false); // shouldn't work
+		}
+		catch(DataUploadException $ex) {
+			$this->assertTrue(true); // should fail
+		}
+		catch(Exception $ex) {
+			$this->assertTrue(false); // but not with any other error
+		}
+	}
+}
+?>
