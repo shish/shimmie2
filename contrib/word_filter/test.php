4af20a3cb5c76223f68bed09b09a83a95868fd51
Shish <shish@shishnet.org>
2009-01-25 12:42:37 +0000

    basic word filter test

---
+++ contrib/word_filter/test.php
@@ -0,0 +1,16 @@
+<?php
+class WordFilterUnitTest extends UnitTestCase {
+	public function testURL() {
+		$this->assertEqual(
+			$this->filter("whore"),
+			"nice lady");
+	}
+
+	private function filter($in) {
+		$bb = new WordFilter();
+		$tfe = new TextFormattingEvent($in);
+		$bb->receive_event($tfe);
+		return $tfe->formatted;
+	}
+}
+?>
