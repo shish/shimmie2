d8c394c7a5ace184f131ab3ec06809432e7edc7a
Shish <shish@shishnet.org>
2009-07-19 08:56:24 +0100

    bug fixing & tidying

--- contrib/tag_history/test.php
+++ contrib/tag_history/test.php
@@ -1,20 +1,17 @@
 <?php
 class TagHistoryTest extends ShimmieWebTestCase {
 	function testTagHistory() {
-		$this->log_in_as_user();
+		$this->log_in_as_admin();
 		$image_id = $this->post_image("ext/simpletest/data/pbx_screenshot.jpg", "pbx");
 		$this->get_page("post/view/$image_id");
 		$this->assertTitle("Image $image_id: pbx");
-		$this->setField("tags", "new");
+		$this->setField("tag_edit__tags", "new");
 		$this->click("Set");
 		$this->assertTitle("Image $image_id: new");
 		$this->click("Tag History");
-		$this->assertText("new (Set by test");
+		$this->assertText("new (Set by demo");
 		$this->click("Revert");
 		$this->assertTitle("Image $image_id: pbx");
-		$this->log_out();
-
-		$this->log_in_as_admin();
 		$this->delete_image($image_id);
 		$this->log_out();
 	}
