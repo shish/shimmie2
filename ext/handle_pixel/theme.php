2c5a68833537a1102f36a38e2ab50d31fc2f375d
Shish <shish@shishnet.org>
2009-07-28 00:20:22 +0100

    exif ifd0 info in the sidebar

--- ext/handle_pixel/theme.php
+++ ext/handle_pixel/theme.php
@@ -4,6 +4,21 @@ class PixelFileHandlerTheme extends Themelet {
 	public function display_image(Page $page, Image $image) {
 		$ilink = $image->get_image_link();
 		$html = "<img id='main_image' src='$ilink'>";
+		# FIXME: only read from jpegs?
+		$exif = @exif_read_data($image->get_image_filename(), 0, true);
+		if($exif) {
+			$head = "";
+			foreach ($exif as $key => $section) {
+				foreach ($section as $name => $val) {
+					if($key == "IFD0") {
+						$head .= html_escape("$name: $val")."<br>\n";
+					}
+				}
+			}
+			if($head) {
+				$page->add_block(new Block("EXIF Info", $head, "left"));
+			}
+		}
 		$page->add_block(new Block("Image", $html, "main", 0));
 	}
 }
