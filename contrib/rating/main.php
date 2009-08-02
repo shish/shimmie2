556d3b50ec29534ddad99812fe3d03f0dfe40720
Shish <shish@shishnet.org>
2009-08-02 08:43:00 +0100

    search for ratings with full words

--- contrib/rating/main.php
+++ contrib/rating/main.php
@@ -125,6 +125,11 @@ class Ratings implements Extension {
 				$set = join(', ', $arr);
 				$event->add_querylet(new Querylet("rating IN ($set)"));
 			}
+			if(preg_match("/^rating=(safe|questionable|explicit|unknown)$/", strtolower($event->term), $matches)) {
+				$text = $matches[1];
+				$char = $text[0];
+				$event->add_querylet(new Querylet("rating = ?", array($char)));
+			}
 		}
 	}
 
