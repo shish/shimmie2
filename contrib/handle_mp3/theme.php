f57e67e6e9936b96bbf089f14a451f17bdda5e8d
Shish <shish@shishnet.org>
2009-08-02 14:44:51 +0100

    xspf player is bsd, and why did we have the full size version instead of slim?

--- contrib/handle_mp3/theme.php
+++ contrib/handle_mp3/theme.php
@@ -7,12 +7,12 @@ class MP3FileHandlerTheme extends Themelet {
 		$html = "
 			<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000'
 			        codebase='http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0'
-					width='400' height='170'>
-				<param name='movie' value='$data_href/ext/handle_mp3/xspf_player.swf?song_url=$ilink'/>
+					width='400' height='15'>
+				<param name='movie' value='$data_href/ext/handle_mp3/xspf_player_slim.swf?song_url=$ilink'/>
 				<param name='quality' value='high' />
-				<embed src='$data_href/ext/handle_mp3/xspf_player.swf?song_url=$ilink' quality='high'
+				<embed src='$data_href/ext/handle_mp3/xspf_player_slim.swf?song_url=$ilink' quality='high'
 					pluginspage='http://www.macromedia.com/go/getflashplayer'
-					width='400' height='170'
+					width='400' height='15'
 					type='application/x-shockwave-flash'></embed>
 			</object>
 			<p><a href='$ilink'>Download</a>";
