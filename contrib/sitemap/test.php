be3e582de6eb3598500b4ba1f9e0285fc58bc95b
Shish <shish@shishnet.org>
2009-08-03 18:35:32 +0100

    sitemap test file

---
+++ contrib/sitemap/test.php
@@ -0,0 +1,9 @@
+<?php
+class XMLSitemapTest extends SCoreWebTestCase {
+	function testBasic() {
+		# this will implicitly check that there are no
+		# PHP-level error messages
+		$this->get_page('sitemap.xml');
+	}
+}
+?>
