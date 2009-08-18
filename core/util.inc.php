0ed1db783820677ada6c4fda1c0b3f8519165cbf
Shish <shish@shishnet.org>
2009-08-18 23:06:15 +0100

    use_autodate option

--- core/util.inc.php
+++ core/util.inc.php
@@ -96,12 +96,15 @@ function to_shorthand_int($int) {
  * @retval string
  */
 function autodate($date) {
-	$diff = time() - strtotime($date);
-	if ($diff<60) return $diff . " second" . plural($diff) . " ago"; $diff = round($diff/60);
-	if ($diff<60) return $diff . " minute" . plural($diff) . " ago"; $diff = round($diff/60);
-	if ($diff<24) return $diff . " hour" . plural($diff) . " ago";   $diff = round($diff/24);
-	if ($diff<7)  return $diff . " day" . plural($diff) . " ago";    $diff = round($diff/7);
-	if ($diff<4)  return $diff . " week" . plural($diff) . " ago";
+	global $config;
+	if($config->get_bool('use_autodate', true)) {
+		$diff = time() - strtotime($date);
+		if ($diff<60) return $diff . " second" . plural($diff) . " ago"; $diff = round($diff/60);
+		if ($diff<60) return $diff . " minute" . plural($diff) . " ago"; $diff = round($diff/60);
+		if ($diff<24) return $diff . " hour" . plural($diff) . " ago";   $diff = round($diff/24);
+		if ($diff<7)  return $diff . " day" . plural($diff) . " ago";    $diff = round($diff/7);
+		if ($diff<4)  return $diff . " week" . plural($diff) . " ago";
+	}
 	return "on " . date("F j, Y", strtotime($date));
 }
 
