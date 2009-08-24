3e1edae90d82349ef95bf98d2e58a63d66c94c12
Shish <shish@shishnet.org>
2009-08-24 02:15:08 +0100

    home page updates

--- contrib/home/theme.php
+++ contrib/home/theme.php
@@ -10,7 +10,7 @@ class HomeTheme extends Themelet {
 		<link rel='stylesheet' href='$data_href/themes/$theme_name/style.css' type='text/css'>
 	</head>
 	<style>
-		div#front-page h1 {font-size: 4em; margin-top: 2em; text-align: center; border: none; background: none;}
+		div#front-page h1 {font-size: 4em; margin-top: 2em; margin-bottom: 0px; text-align: center; border: none; background: none;}
 		div#front-page {text-align:center;}
 		.space {margin-bottom: 1em;}
 		div#front-page div#links a {margin: 0 0.5em;}
@@ -24,31 +24,31 @@ EOD
 );
 	}
 
-	public function build_body($sitename, $main_links, $contact_link, $num_comma, $counter_text) {
-		return "
-		<div id='front-page'>
-			<h1>
-				<a style='text-decoration: none;' href='".make_link()."'><span>$sitename</span></a>
-			</h1>
-			<div class='space' id='links'>
-				$main_links
-			</div>
-			<div class='space'>
+	public function build_body($sitename, $main_links, $main_text, $contact_link, $num_comma, $counter_text) {
+		$main_links_html = empty($main_links) ? "" : "<div class='space' id='links'>$main_links</div>";
+		$message_html = empty($main_text)     ? "" : "<div class='space' id='message'>$main_text</div>";
+		$counter_html = empty($counter_text)  ? "" : "<div class='space' id='counter'>$counter_text</div>";
+		$search_html = "
+			<div class='space' id='search'>
 				<form action='".make_link("post/list")."' method='GET'>
-				<input id='search_input' name='search' size='55' type='text' value='' autocomplete='off' /><br/>
+				<input id='search_input' name='search' size='30' type='text' value='' autocomplete='off' />
 				<input type='hidden' name='q' value='/post/list'>
 				<input type='submit' value='Search'/>
 				</form>
 			</div>
-      		<div style='font-size: 80%; margin-bottom: 2em;'>
-		 		<a href='$contact_link'>contact</a> &ndash; Serving $num_comma posts
-			</div>
-
-			<div class='space'>
-				Powered by <a href='http://trac.shishnet.org/shimmie2/'>Shimmie</a>
-			</div>
-			<div class='space'>
-				$counter_text
+		";
+		return "
+		<div id='front-page'>
+			<h1><a style='text-decoration: none;' href='".make_link()."'><span>$sitename</span></a></h1>
+			$main_links_html
+			$search_html
+			$message_html
+			$counter_html
+			<div class='space' id='foot'>
+				<small><small>
+				<a href='$contact_link'>Contact</a> &ndash; Serving $num_comma posts &ndash;
+				Running <a href='http://code.shishnet.org/shimmie2/'>Shimmie</a>
+				</small></small>
 			</div>
 		</div>";
 	}
