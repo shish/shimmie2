f718193449b0ad133e68cb150ac6d8ef2a9c0ce6
Shish <shish@shishnet.org>
2009-10-08 12:47:44 +0100

    add test case to confirm #761

--- ext/admin/test.php
+++ ext/admin/test.php
@@ -14,6 +14,8 @@ class AdminPageTest extends ShimmieWebTestCase {
 		$this->log_in_as_admin();
 		$this->get_page('admin');
 		$this->assert_title("Admin Tools");
+		$this->set_field("action", "purge unused tags");
+		$this->click("Go");
 		$this->log_out();
 
 		# FIXME: make sure the admin tools actually work
