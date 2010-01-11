270cdf2da596f37ffed16e17c103708c3b5e8647
Shish <shish@shishnet.org>
2010-01-11 15:10:07 +0000

    Copyright notice updates

--- themes/warm/layout.class.php
+++ themes/warm/layout.class.php
@@ -83,7 +83,7 @@ $header_html
 		<div id="footer">
 			Images &copy; their respective owners,
 			<a href="http://code.shishnet.org/shimmie2/">Shimmie</a> &copy;
-			<a href="http://www.shishnet.org/">Shish</a> 2007-2009,
+			<a href="http://www.shishnet.org/">Shish</a> &amp; Co 2007-2010,
 			based on the Danbooru concept.
 			$debug
 			$contact
