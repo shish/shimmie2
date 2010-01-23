7d63e08eaf3d17530d2fc4dbe25b81e78fb2d17c
Shish <shish@shishnet.org>
2010-01-23 13:20:32 +0000

    non-admins are allowed to see the extension list now (read-only)

--- ext/ext_manager/test.php
+++ ext/ext_manager/test.php
@@ -2,14 +2,7 @@
 class ExtManagerTest extends SCoreWebTestCase {
 	function testAuth() {
         $this->get_page('ext_manager');
-		$this->assert_response(403);
-		$this->assert_title("Permission Denied");
-
-		$this->log_in_as_user();
-        $this->get_page('ext_manager');
-		$this->assert_response(403);
-		$this->assert_title("Permission Denied");
-		$this->log_out();
+		$this->assert_title("Extensions");
 
 		$this->log_in_as_admin();
         $this->get_page('ext_manager');
