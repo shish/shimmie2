5ddaed8e2844c222526d725a271f5939be5fd4f3
Shish <shish@shishnet.org>
2010-01-26 13:38:08 +0000

    Turn the QR Code extension into a SimpleExtension, trim the boilerplate

--- contrib/qr_code/theme.php
+++ contrib/qr_code/theme.php
@@ -1,12 +1,10 @@
 <?php
 class QRImageTheme extends Themelet {
-	public function links_block(Page $page, $data) {
-		$image_src = $data['image_src'];
-		global $config, $user;
-		$base_href = $config->get_string('base_href');
-		$data_href = get_base_href();
+	public function links_block($link) {
+		global $page;
+		$base_href = get_base_href();
 		$page->add_block( new Block(
-			"QR Code","<img src='".$data_href."/ext/qr_code/qr_img.php?d=".$image_src."&s=3' />","left",50));
+			"QR Code","<img src='$base_href/ext/qr_code/qr_img.php?d=$link&s=3' />","left",50));
 	}
 }
 ?>
