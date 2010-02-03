8de01a61847e9780a1c3833c2b5fe9faa8069171
Shish <shish@shishnet.org>
2010-02-03 13:57:39 +0000

    searchable event log

--- contrib/log_db/theme.php
+++ contrib/log_db/theme.php
@@ -1,10 +1,39 @@
 <?php
 
 class LogDatabaseTheme extends Themelet {
+	protected function heie($var) {
+		if(isset($_GET[$var])) return html_escape($_GET[$var]);
+		else return "";
+	}
 	public function display_events($events) {
-		$table = "<table class='zebra'>";
-		$table .= "<thead><th>Time</th><th>Module</th><th>User</th><th>Message</th></thead>";
-		$table .= "<tbody>\n";
+		$table = "
+<style>
+.sizedinputs TD INPUT {
+	width: 100%;
+}
+</style>
+<table class='zebra'>
+	<thead>
+		<tr><th>Time</th><th>Module</th><th>User</th><th>Message</th></tr>
+		<form action='".make_link("log/view")."' method='GET'>
+			<tr class='sizedinputs'>
+				<td><input type='text' name='time' value='".$this->heie("time")."'></td>
+				<td><input type='text' name='module' value='".$this->heie("module")."'></td>
+				<td><input type='text' name='user' value='".$this->heie("user")."'></td>
+				<td>
+					<select name='priority'>
+						<option value='".SCORE_LOG_DEBUG."'>Debug</option>
+						<option value='".SCORE_LOG_INFO."' selected>Info</option>
+						<option value='".SCORE_LOG_WARNING."'>Warning</option>
+						<option value='".SCORE_LOG_ERROR."'>Error</option>
+						<option value='".SCORE_LOG_CRITICAL."'>Critical</option>
+					</select>
+					<input type='submit' value='Search' style='width: 20%'>
+				</td>
+			</tr>
+		</form>
+	</thead>
+	<tbody>\n";
 		$n = 0;
 		foreach($events as $event) {
 			$oe = ($n++ % 2 == 0) ? "even" : "odd";
