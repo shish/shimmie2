b865e696bcced1d9c1f5723e0a0e2da5c5295e45
Shish <shish@shishnet.org>
2010-02-18 15:58:23 +0000

    more blotter testing

--- contrib/blotter/test.php
+++ contrib/blotter/test.php
@@ -1,20 +1,27 @@
 <?php
 class BlotterTest extends SCoreWebTestCase {
-	function testView() {
+	function testLogin() {
 		$this->log_in_as_admin();
 		$this->assert_text("Blotter Editor");
 		$this->click("Blotter Editor");
 		$this->log_out();
 	}
 
-	function testAdd() {
+	function testAddViewRemove() {
 		$this->log_in_as_admin();
+
 		$this->get_page("blotter/editor");
 		$this->set_field("entry_text", "blotter testing");
 		$this->click("Add");
 		$this->assert_text("blotter testing");
+
+		$this->get_page("blotter");
+		$this->assert_text("blotter testing");
+
+		$this->get_page("blotter/editor");
 		$this->clock("Remove");
 		$this->assert_no_text("blotter testing");
+
 		$this->log_out();
 	}
 }
