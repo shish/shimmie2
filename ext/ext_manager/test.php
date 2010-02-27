b4584f7003250a18489854c62a4f1e02699c3853
Shish <shish@shishnet.org>
2010-02-27 02:41:52 +0000

    indentation

--- ext/ext_manager/test.php
+++ ext/ext_manager/test.php
@@ -1,18 +1,18 @@
 <?php
 class ExtManagerTest extends SCoreWebTestCase {
 	function testAuth() {
-        $this->get_page('ext_manager');
+		$this->get_page('ext_manager');
 		$this->assert_title("Extensions");
 
-        $this->get_page('ext_doc');
+		$this->get_page('ext_doc');
 		$this->assert_response(404);
 
-        $this->get_page('ext_doc/ext_manager');
+		$this->get_page('ext_doc/ext_manager');
 		$this->assert_title("Documentation for Extension Manager");
 		$this->assert_text("view a list of all extensions");
 
 		$this->log_in_as_admin();
-        $this->get_page('ext_manager');
+		$this->get_page('ext_manager');
 		$this->assert_title("Extensions");
 		$this->assert_text("SimpleTest integration");
 		$this->log_out();
