d52aff33964091264da601a7502fc2b9a0c4c33c
Shish <shish@shishnet.org>
2010-03-08 18:25:23 +0000

    next/prev with query test

--- ext/view/test.php
+++ ext/view/test.php
@@ -4,7 +4,8 @@ class ViewTest extends ShimmieWebTestCase {
 		$this->log_in_as_user();
 		$image_id_1 = $this->post_image("ext/simpletest/data/pbx_screenshot.jpg", "test");
 		$image_id_2 = $this->post_image("ext/simpletest/data/bedroom_workshop.jpg", "test2");
-		$idp1 = $image_id_2 + 1;
+		$image_id_3 = $this->post_image("ext/simpletest/data/favicon.png", "test");
+		$idp1 = $image_id_3 + 1;
 		$this->log_out();
 
         $this->get_page("post/view/$image_id_1");
@@ -24,6 +25,11 @@ class ViewTest extends ShimmieWebTestCase {
 
         $this->get_page('post/view/-1');
         $this->assert_title('Image not found');
+
+		# note: skips image #2
+		# note: can't click "Next" / "Prev" since the query is added by javascript
+		$this->get_page("post/prev/$image_id_1?search=test");
+		$this->assert_title("Image $image_id_3: test");
 
 		$this->log_in_as_admin();
 		$this->delete_image($image_id_1);
