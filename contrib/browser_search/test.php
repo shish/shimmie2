59b25a0fe1978bfd25775d05e523d9bc8303a578
Shish <shish@shishnet.org>
2010-03-24 12:36:37 +0000

    browser search test

---
+++ contrib/browser_search/test.php
@@ -0,0 +1,8 @@
+<?php
+class BrowserSearchTest extends SCoreWebTestCase {
+	function testBasic() {
+		$this->get_page("browser_search/please_dont_use_this_tag_as_it_would_break_stuff__search.xml");
+		$this->get_page("browser_search/test");
+	}
+}
+?>
