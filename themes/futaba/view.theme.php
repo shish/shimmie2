49f860683fb6081311f118855b5f45039891c541
Shish <shish@shishnet.org>
2010-07-19 13:53:12 +0100

    more themes, more updates

--- themes/futaba/view.theme.php
+++ themes/futaba/view.theme.php
@@ -1,7 +1,8 @@
 <?php
 
 class CustomViewImageTheme extends ViewImageTheme {
-	public function display_page($page, $image, $editor_parts) {
+	public function display_page($image, $editor_parts) {
+		global $page;
 		$page->set_title("Image {$image->id}: ".html_escape($image->get_tag_list()));
 		$page->set_heading(html_escape($image->get_tag_list()));
 		$page->add_block(new Block(null, $this->build_info($image, $editor_parts), "main", 10));
