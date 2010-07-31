49d4f36071a1bdea5ed6adc313a18317e30c08e0
Christian Walde <walde.christian@googlemail.com>
2010-07-31 16:41:13 +0200

    added first version of mass tagger

---
+++ contrib/mass_tagger/theme.php
@@ -0,0 +1,28 @@
+<?php
+
+class MassTaggerTheme extends Themelet {
+	/*
+	 * Show $text on the $page
+	 */
+	public function display_mass_tagger( Page $page, Event $event, $config ) {
+		$base_href = $config->get_string('base_href');
+		$page->add_header("<link  href='/ext/mass_tagger/mass_tagger.css' type='text/css' rel='stylesheet' />");
+		$page->add_header("<script src='/ext/mass_tagger/mass_tagger.js' type='text/javascript'></script>");
+		$body = "
+			<form action='".make_link("mass_tagger/tag")."' method='POST'>
+				<input id='mass_tagger_activate' type='button' onclick='activate_mass_tagger();' value='Activate'/>
+				<div id='mass_tagger_controls'>
+					Click on images to mark them. Use the 'Index Options' in the Board Config to increase the amount of shown images.
+					<br />
+					<input type='hidden' name='ids' id='mass_tagger_ids' />
+					<label>Tags: <input type='text' name='tag' /></label>
+					
+					<input type='submit' value='Tag Marked Images' />
+				</div>
+			</form>
+		";
+		$block = new Block("Mass Tagger", $body, "left", 50);
+		$page->add_block( $block );
+	}
+}
+?>
