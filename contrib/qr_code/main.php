5f00999f87d0014540ca77122c87a9a55cb4c8ee
Shish <shish@shishnet.org>
2010-12-14 20:34:01 +0000

    use google charts for QR code generation, save 7MB

--- contrib/qr_code/main.php
+++ contrib/qr_code/main.php
@@ -4,7 +4,8 @@
  * Author: Zach Hall <zach@sosguy.net> [http://seemslegit.com]
  * Description: Shows a QR Code for downloading an image to cell phones.
  * 				Based on Artanis's Link to Image Extension <artanis.00@gmail.com>
- * 				Includes QRcode Perl CGI & PHP scripts ver. 0.50 [http://www.swetake.com/qr/qr_cgi_e.html]
+ *              Further modified by Shish to remove the 7MB local QR generator
+ *              and replace it with a link to google chart APIs
  */
 class QRImage extends SimpleExtension {
 	public function onDisplayingImage($event) {
--- contrib/qr_code/theme.php
+++ contrib/qr_code/theme.php
@@ -2,9 +2,8 @@
 class QRImageTheme extends Themelet {
 	public function links_block($link) {
 		global $page;
-		$base_href = get_base_href();
 		$page->add_block( new Block(
-			"QR Code","<img src='$base_href/ext/qr_code/qr_img.php?d=$link&s=3' />","left",50));
+			"QR Code","<img src='http://chart.apis.google.com/chart?chs=150x150&cht=qr&chl=$link' />","left",50));
 	}
 }
 ?>
