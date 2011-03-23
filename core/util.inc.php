811f9e0f663969fb1b022d3efaa90791fb97b2f4
Shish <shish@shishnet.org>
2011-03-23 11:19:34 +0000

    cd variable for sites who want /ab/cb/abcd

--- core/util.inc.php
+++ core/util.inc.php
@@ -502,6 +502,7 @@ function format_text($string) {
 
 function warehouse_path($base, $hash, $create=true) {
 	$ab = substr($hash, 0, 2);
+	$cd = substr($hash, 2, 2);
 	$pa = "$base/$ab/$hash";
 	if($create && !file_exists(dirname($pa))) mkdir(dirname($pa), 0755, true);
 	return $pa;
