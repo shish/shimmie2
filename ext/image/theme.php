53c10ecddf38e4a519860bcf1f9420b556330782
green-ponies (jgen) <jgen.tech@gmail.com>
2011-08-12 22:02:41 -0400

    Confirm image delete operation with javascript.

--- ext/image/theme.php
+++ ext/image/theme.php
@@ -2,6 +2,7 @@
 class ImageIOTheme {
 	/*
 	 * Display a link to delete an image
+	 * (Added inline Javascript to confirm the deletion)
 	 *
 	 * $image_id = the image to delete
 	 */
@@ -12,7 +13,7 @@ class ImageIOTheme {
 		$html = "
 			".make_form(make_link("image_admin/delete"),'POST',false,'delete_image')."
 				<input type='hidden' name='image_id' value='$i_image_id'>
-				<input type='submit' value='Delete'>
+				<input type='submit' value='Delete' onclick='return confirm(\"Delete the image?\");'>
 			</form>
 		";
 		return $html;
