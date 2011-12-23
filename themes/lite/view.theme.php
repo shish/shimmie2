002df1a7ba319eb4f6606d91543642a572f7ebc8
Shish <shish@shishnet.org>
2011-12-23 14:28:42 +0000

    keywords for images with lite theme

--- themes/lite/view.theme.php
+++ themes/lite/view.theme.php
@@ -5,6 +5,11 @@ class CustomViewImageTheme extends ViewImageTheme {
 		global $page;
 		$page->set_title("Image {$image->id}: ".html_escape($image->get_tag_list()));
 		$page->set_heading(html_escape($image->get_tag_list()));
+		$page->add_html_header("<meta name=\"keywords\" content=\"$metatags\">");
+		$page->add_html_header("<meta property=\"og:title\" content=\"$metatags\">");
+		$page->add_html_header("<meta property=\"og:type\" content=\"article\">");
+		$page->add_html_header("<meta property=\"og:image\" content=\"".make_http($image->get_thumb_link())."\">");
+		$page->add_html_header("<meta property=\"og:url\" content=\"".make_http(make_link("post/view/{$image->id}"))."\">");
 		$page->add_block(new Block("Navigation", $this->build_navigation($image), "left", 0));
 		$page->add_block(new Block("Statistics", $this->build_stats($image), "left", 10));
 		$page->add_block(new Block(null, $this->build_image_editor($image, $editor_parts), "main", 10));
