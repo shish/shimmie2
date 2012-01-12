ea6f853891d19f3a539ebe5ace2eeb5d35e7c0cb
green-ponies (jgen) <jgen.tech@gmail.com>
2012-01-12 14:47:24 -0500

    Changes to the index file as well.

--- index.php
+++ index.php
@@ -107,22 +107,22 @@ try {
 	ctx_log_start("Loading themelets");
 	// load the theme parts
 	$_theme = $config->get_string("theme", "default");
-	if(!file_exists("themes/$_theme")) $_theme = "default";
-	if(file_exists("themes/$_theme/custompage.class.php")) require_once "themes/$_theme/custompage.class.php";
-	require_once "themes/$_theme/layout.class.php";
-	require_once "themes/$_theme/themelet.class.php";
+	if(!file_exists('themes/'.$_theme)) $_theme = "default";
+	if(file_exists('themes/'.$_theme.'/custompage.class.php')) require_once 'themes/'.$_theme.'/custompage.class.php';
+	require_once 'themes/'.$_theme.'/layout.class.php';
+	require_once 'themes/'.$_theme.'/themelet.class.php';
 
 	$themelets = glob("ext/*/theme.php");
 	foreach($themelets as $filename) {
 		require_once $filename;
 	}
 
-	$custom_themelets = glob("themes/$_theme/*.theme.php");
+	$custom_themelets = glob('themes/'.$_theme.'/*.theme.php');
 	if($custom_themelets) {
 		$m = array();
 		foreach($custom_themelets as $filename) {
-			if(preg_match("/themes\/$_theme\/(.*)\.theme\.php/",$filename,$m)
-					&& in_array("ext/{$m[1]}/theme.php", $themelets)) {
+			if(preg_match('/themes\/'.$_theme.'\/(.*)\.theme\.php/',$filename,$m)
+					&& in_array('ext/'.$m[1].'/theme.php', $themelets)) {
 				require_once $filename;
 			}
 		}
