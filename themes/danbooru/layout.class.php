899bc225386a6ae03c2b6d7c1074de6ce8dcc599
Daku <dakutree@codeanimu.net>
2012-01-31 00:04:27 +0000

    my favorites/feed tab should only show if ext is enabled

--- themes/danbooru/layout.class.php
+++ themes/danbooru/layout.class.php
@@ -142,7 +142,8 @@ class Layout {
 			case "upload":
 				if(file_exists("ext/numeric_score")){ $custom_sublinks .= "<li><b>Popular by </b><a href='".make_link('popular_by_day')."'>Day</a>/<a href='".make_link('popular_by_month')."'>Month</a>/<a href='".make_link('popular_by_year')."'>Year</a></li>";}
 				$custom_sublinks .= "<li><a href='".make_link('post/list')."'>All</a></li>";
-				$custom_sublinks .= "<li><a href='".make_link("post/list/favorited_by=$username/1")."'>My Favorites</a></li>";
+				if(file_exists("ext/favorites")){ $custom_sublinks .= "<li><a href='".make_link("post/list/favorited_by=$username/1")."'>My Favorites</a></li>";}
+				if(file_exists("ext/rss_images")){ $custom_sublinks .= "<li><a href='".make_link('rss/images')."'>Feed</a></li>";}
 				if(file_exists("ext/random_image")){ $custom_sublinks .= "<li><a href='".make_link("random_image/view")."'>Random Image</a></li>";}
 				if($hw){ $custom_sublinks .= "<li><a href='".make_link("wiki/posts")."'>Help</a></li>";
 				}else{ $custom_sublinks .= "<li><a href='".make_link("ext_doc/index")."'>Help</a></li>";}
