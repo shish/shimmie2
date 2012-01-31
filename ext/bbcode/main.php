1cd4f867b5251f166cbe19798e66ae49159c496d
Shish <shish@shishnet.org>
2012-01-31 12:24:29 +0000

    don't die if [/code] comes before [code]

--- ext/bbcode/main.php
+++ ext/bbcode/main.php
@@ -142,7 +142,7 @@ class BBCode extends FormatterExtension {
 			$start = strpos($text, "[code]");
 			if($start === false) break;
 
-			$end = strpos($text, "[/code]");
+			$end = strpos($text, "[/code]", $start);
 			if($end === false) break;
 
 			$beginning = substr($text, 0, $start);
