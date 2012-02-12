766cc9e959ed02f26fd43ba6064109288c7f6ab9
Shish <shish@shishnet.org>
2012-02-12 11:45:18 +0000

    this function was only referenced once

--- contrib/mass_tagger/script.js
+++ contrib/mass_tagger/script.js
@@ -1,3 +1,12 @@
+function find_thumb_link_containers () {
+    
+    var post_link = "a[href*='/post/view/']";
+    var has_thumb_img = ":has(img[src*='/thumb/'])";
+
+    var list = $( post_link + has_thumb_img ).parent();
+
+    return list;
+}
 
 function toggle_tag( button, id ) {
     id += ":";
