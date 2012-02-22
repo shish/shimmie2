0351ffcd28c0797f184bd0b5df6e3546784b8178
Shish <shish@shishnet.org>
2012-02-22 13:33:35 +0000

    commit_hash is unknown by default

--- core/util.inc.php
+++ core/util.inc.php
@@ -944,7 +944,7 @@ function get_debug_info() {
 		$i_mem = "???";
 	}
 
-	if($config->get_string("commit_hash") == "unknown"){
+	if($config->get_string("commit_hash", "unknown") == "unknown"){
 		$commit = "";
 	}else{
 		$commit = " (".$config->get_string("commit_hash").")";
