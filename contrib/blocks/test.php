ee976ed23beade6ca4c2d563c840b599bb6d5a3c
Shish <shish@shishnet.org>
2012-03-10 14:30:49 +0000

    better blocks testing

--- contrib/blocks/test.php
+++ contrib/blocks/test.php
@@ -4,19 +4,27 @@ class BlocksTest extends SCoreWebTestCase {
 		$this->log_in_as_admin();
 
 		$this->get_page("setup");
-		$this->set_field("_config_blocks_text", "badgers");
+		$this->set_field("_config_blocks_text", "
+Title: some text
+Area: main
+Priority: 100
+Pages: *
+
+waffles
+");
 		$this->click("Save Settings");
 
 		$this->get_page("post/list");
-		$this->assert_text("badgers");
+		$this->assert_text("some text");
+		$this->assert_text("waffles");
 
 		$this->get_page("setup");
 		$this->set_field("_config_blocks_text", "");
 		$this->click("Save Settings");
 
 		$this->get_page("post/list");
-		$this->assert_no_text("Note");
-		$this->assert_no_text("badgers");
+		$this->assert_no_text("some text");
+		$this->assert_no_text("waffles");
 
 		$this->log_out();
 	}
