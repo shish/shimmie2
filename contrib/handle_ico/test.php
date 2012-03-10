d4d2afdd24ac8a0b97e5bcf2f53b320e48c0ed4d
Shish <shish@shishnet.org>
2012-03-10 15:29:42 +0000

    ico testing

--- contrib/handle_ico/test.php
+++ contrib/handle_ico/test.php
@@ -2,10 +2,11 @@
 class IcoHandlerTest extends ShimmieWebTestCase {
 	function testPixelHander() {
 		$this->log_in_as_user();
-		$image_id = $this->post_image("favicon.ico", "shimmie favicon");
+		$image_id = $this->post_image("lib/static/favicon.ico", "shimmie favicon");
 		$this->assert_response(302);
 		$this->log_out();
 
+		$raw = $this->get_page("post/view/$image_id"); // test for no crash
 		$raw = $this->get_page("get_ico/$image_id"); // test for no crash
 
 		$this->log_in_as_admin();
