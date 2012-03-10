3801ce1becaf9b9c2a3366e20b497f06504cdd27
Shish <shish@shishnet.org>
2012-03-10 17:51:56 +0000

    hash ban test

--- contrib/image_hash_ban/test.php
+++ contrib/image_hash_ban/test.php
@@ -10,6 +10,28 @@ class HashBanTest extends ShimmieWebTestCase {
 		$this->click("Ban and Delete");
 		$this->log_out();
 
+		$this->log_in_as_user();
+		$this->get_page("post/view/$image_id");
+		$this->assert_response(404);
+		$image_id = $this->post_image("ext/simpletest/data/pbx_screenshot.jpg", "pbx");
+		$this->get_page("post/view/$image_id");
+		$this->assert_response(404);
+		$this->log_out();
+
+		$this->log_in_as_admin();
+		$this->get_page("image_hash_ban/list/1");
+		$this->click("Remove");
+		$this->log_out();
+
+		$this->log_in_as_user();
+		$image_id = $this->post_image("ext/simpletest/data/pbx_screenshot.jpg", "pbx");
+		$this->get_page("post/view/$image_id");
+		$this->assert_response(200);
+		$this->log_out();
+
+		$this->log_in_as_admin();
+		$this->delete_image($image_id);
+		$this->log_out();
 	}
 }
 ?>
