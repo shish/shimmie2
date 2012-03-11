cf907237c6cf73b4919d22fec3e1ddf92274f8d6
Shish <shish@shishnet.org>
2012-03-11 04:33:24 +0000

    with 5.2.1, we have an accurate, peak memory use

--- core/util.inc.php
+++ core/util.inc.php
@@ -907,12 +907,7 @@ $_load_start = microtime(true);
 function get_debug_info() {
 	global $config, $_event_count, $database, $_execs, $_load_start;
 
-	if(function_exists('memory_get_usage')) {
-		$i_mem = sprintf("%5.2f", ((memory_get_usage()+512)/1024)/1024);
-	}
-	else {
-		$i_mem = "???";
-	}
+	$i_mem = sprintf("%5.2f", ((memory_get_peak_usage(true)+512)/1024)/1024);
 
 	if($config->get_string("commit_hash", "unknown") == "unknown"){
 		$commit = "";
