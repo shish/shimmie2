69f4928916938a195df86454276bb6c05bccf8ad
Shish <shish@shishnet.org>
2012-03-13 07:01:01 +0000

    remove excess html

--- contrib/featured/theme.php
+++ contrib/featured/theme.php
@@ -27,15 +27,10 @@ class FeaturedTheme extends Themelet {
 		$h_tip = html_escape($image->get_tooltip());
 		$tsize = get_thumbnail_size($image->width, $image->height);
 
-
 		return "
-			<center><div>
-			
-				<a href='$h_view_link' style='position: relative; height: {$tsize[1]}px; width: {$tsize[0]}px;'>
-					<img id='thumb_$i_id' title='$h_tip' alt='$h_tip' class='highlighted' style='height: {$tsize[1]}px; width: {$tsize[0]}px;' src='$h_thumb_link'>
-				</a>
-			
-			</div></center>
+			<a href='$h_view_link'>
+				<img id='thumb_$i_id' title='$h_tip' alt='$h_tip' class='highlighted' style='height: {$tsize[1]}px; width: {$tsize[0]}px;' src='$h_thumb_link'>
+			</a>
 		";
 	}
 }
