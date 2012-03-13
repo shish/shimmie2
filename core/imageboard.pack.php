dde5d4c9b527283984462a2e1f95c3430e45454a
Shish <shish@shishnet.org>
2012-03-13 06:33:21 +0000

    html5 validation

--- core/imageboard.pack.php
+++ core/imageboard.pack.php
@@ -563,6 +563,7 @@ class Image {
 		$tmpl = str_replace('$filesize', to_shorthand_int($this->filesize), $tmpl);
 		$tmpl = str_replace('$filename', $_escape($base_fname), $tmpl);
 		$tmpl = str_replace('$title', $_escape($config->get_string("title")), $tmpl);
+		$tmpl = str_replace(' ', '%20', $tmpl);
 
 		// nothing seems to use this, sending the event out to 50 exts is a lot of overhead
 		if(!SPEED_HAX) {
