9a0228720a3a56ecc103a6192a1463912c92a3c0
Shish <shish@shishnet.org>
2012-03-13 07:01:27 +0000

    truncate as a function, which works

--- core/util.inc.php
+++ core/util.inc.php
@@ -83,6 +83,22 @@ function no_escape($input) {
 	return $input;
 }
 
+// Original PHP code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+function truncate($string, $limit, $break=" ", $pad="...") {
+	// return with no change if string is shorter than $limit
+	if(strlen($string) <= $limit) return $string;
+
+	// is $break present between $limit and the end of the string?
+	if(false !== ($breakpoint = strpos($string, $break, $limit))) {
+		if($breakpoint < strlen($string) - 1) {
+			$string = substr($string, 0, $breakpoint) . $pad;
+		}
+	}
+
+	return $string;
+}
+
 /**
  * Turn a human readable filesize into an integer, eg 1KB -> 1024
  *
