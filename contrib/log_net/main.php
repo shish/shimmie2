e4e95a3a8540720010c499753221181daedcb840
Shish <shish@shishnet.org>
2012-03-19 21:19:50 +0000

    suppress the flood of 'suppressing flood' messages...

--- contrib/log_net/main.php
+++ contrib/log_net/main.php
@@ -13,15 +13,17 @@ class LogNet extends Extension {
 	public function onLog(LogEvent $event) {
 		global $user;
 
-		if($this->count < 10 && $event->priority > 10) {
-			// TODO: colour based on event->priority
-			$username = ($user && $user->name) ? $user->name : "Anonymous";
-			$str = sprintf("%-15s %-10s: %s", $_SERVER['REMOTE_ADDR'], $username, $event->message);
-			system("echo ".escapeshellarg($str)." | nc -q 0 localhost 5000");
-			$this->count++;
-		}
-		else {
-			system("echo 'suppressing flood, check the web log' | nc -q 0 localhost 5000");
+		if($event->priority > 10) {
+			if($this->count < 10) {
+				// TODO: colour based on event->priority
+				$username = ($user && $user->name) ? $user->name : "Anonymous";
+				$str = sprintf("%-15s %-10s: %s", $_SERVER['REMOTE_ADDR'], $username, $event->message);
+				system("echo ".escapeshellarg($str)." | nc -q 0 localhost 5000");
+				$this->count++;
+			}
+			else {
+				system("echo 'suppressing flood, check the web log' | nc -q 0 localhost 5000");
+			}
 		}
 	}
 }
