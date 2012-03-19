033540e6f89acbf51b46badc4ce6bec7b3a56b81
Shish <shish@shishnet.org>
2012-03-19 22:25:57 +0000

    if this->count is checked inside event->pri, then checking again for count *is* necessary...

--- contrib/log_net/main.php
+++ contrib/log_net/main.php
@@ -14,14 +14,14 @@ class LogNet extends Extension {
 		global $user;
 
 		if($event->priority > 10) {
+			$this->count++;
 			if($this->count < 10) {
 				// TODO: colour based on event->priority
 				$username = ($user && $user->name) ? $user->name : "Anonymous";
 				$str = sprintf("%-15s %-10s: %s", $_SERVER['REMOTE_ADDR'], $username, $event->message);
 				system("echo ".escapeshellarg($str)." | nc -q 0 localhost 5000");
-				$this->count++;
 			}
-			else {
+			else if($this->count == 10) {
 				system("echo 'suppressing flood, check the web log' | nc -q 0 localhost 5000");
 			}
 		}
