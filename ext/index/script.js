898df24dd94e03c6c571af45d2c30a4a1bc2d0e9
Shish <shish@shishnet.org>
2012-03-22 14:19:46 +0000

    function for selecting blocks

--- ext/index/script.js
+++ ext/index/script.js
@@ -8,3 +8,11 @@ $(function() {
 	// text-align: justify with element margins and doesn't recalculate
 	// these margins when part of the line disappears...
 });
+
+function select_blocked_tags() {
+	var blocked_tags = prompt("Enter tags to ignore", $.cookie("blocked-tags") || "My_Little_Pony");
+	if(blocked_tags) {
+		$.cookie("blocked-tags", blocked_tags.toLowerCase(), {path: '/'});
+		location.reload(true);
+	}
+}
