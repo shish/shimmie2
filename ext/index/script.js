f993b3c19d9cbf6bebe9794dde709a19d987bf5c
Shish <shish@shishnet.org>
2012-03-22 16:28:57 +0000

    js cookie expires

--- ext/index/script.js
+++ ext/index/script.js
@@ -14,7 +14,7 @@ $(function() {
 function select_blocked_tags() {
 	var blocked_tags = prompt("Enter tags to ignore", $.cookie("blocked-tags") || "My_Little_Pony");
 	if(blocked_tags) {
-		$.cookie("blocked-tags", blocked_tags.toLowerCase(), {path: '/'});
+		$.cookie("blocked-tags", blocked_tags.toLowerCase(), {path: '/', expires: 365});
 		location.reload(true);
 	}
 }
