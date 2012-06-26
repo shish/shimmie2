7391dcf0b601210f7645df7cbc539b3f9829c234
Shish <shish@shishnet.org>
2012-06-26 19:58:35 +0100

    configuarable oekaki size, and more consistent creation

--- ext/oekaki/theme.php
+++ ext/oekaki/theme.php
@@ -3,27 +3,21 @@
 // FIXME: Move default canvas size to config file; changeable in board config
 // While we're here, add maximum and minimum image sizes in config
 // Maybe allow the resolution limiter extension to have a say in this 
-$defOekW = 400; // Common default for oekaki boards: 300x300
-$defOekH = 400;
+
 class OekakiTheme extends Themelet {
 	public function display_page() {
-		global $config, $page, $defOekW, $defOekH;
+		global $config, $page;
 
 		$base_href = get_base_href();
 		$http_base = make_http($base_href);
 
-		if (isset($_POST['oekW']) and isset($_POST['oekH'])){
-			$oekW = $_POST['oekW'];
-			$oekH = $_POST['oekH'];
-			if(!ctype_digit($oekW) or !ctype_digit($oekH)){
-				$oekW = $defOekW;
-				$oekH = $defOekH;
-			}
-		} else{
-			$oekW = $defOekW;
-			$oekH = $defOekH;
+		$oekW = $config->get_int("oekaki_width", 400);
+		$oekH = $config->get_int("oekaki_height", 400);
+		if(isset($_POST['oekW']) && isset($_POST['oekH'])) {
+			$oekW = int_escape($_POST['oekW']);
+			$oekH = int_escape($_POST['oekH']);
 		}
-		
+
 		$html = "
     <applet archive='$base_href/ext/oekaki/chibipaint.jar' code='chibipaint.ChibiPaint.class' width='800' height='600'>
       <param name='canvasWidth' value='".$oekW."' />
@@ -31,7 +25,7 @@ class OekakiTheme extends Themelet {
       <param name='postUrl' value='".make_http(make_link("oekaki/upload"))."' />
       <param name='exitUrl' value='".make_http(make_link("oekaki/claim"))."' />
       <param name='exitUrlTarget' value='_self' />
-      JAVA NOT INSTALLED >:(<!-- alternative content for users who don't have Java installed -->
+      JAVA NOT INSTALLED :(<!-- alternative content for users who don't have Java installed -->
     </applet>
 		";
 
@@ -61,20 +55,24 @@ class OekakiTheme extends Themelet {
 	}
 
 	public function display_block() {
-		global $page, $defOekW, $defOekH;
+		global $config, $page;
 		//FIXME: input field alignment could be done more elegantly, without inline styling
 		//FIXME: autocomplete='off' seems to be an invalid HTML tag
-		$page->add_block(new Block(null, 
+
+		$oekW = $config->get_int("oekaki_width", 400);
+		$oekH = $config->get_int("oekaki_height", 400);
+		if(isset($_POST['oekW']) && isset($_POST['oekH'])) {
+			$oekW = int_escape($_POST['oekW']);
+			$oekH = int_escape($_POST['oekH']);
+		}
+
+		$page->add_block(new Block("Oekaki", 
 			"
-			<b>Oekaki</b>
-			<br>
 			<form form enctype='multipart/form-data' action='".make_link("oekaki/create")."' method='POST'>
-				<div style='display: inline; margin: 0; width: auto;'>
-					<input autocomplete='off' style='display: inline; margin: 0; width: auto; font-size: 90%' name='oekW' type='text' size='3' value='".$defOekW."'/>
-					x
-					<input autocomplete='off' style='display: inline; margin: 0; width: auto; font-size: 90%' name='oekH' type='text' size='3' value='".$defOekH."'/>
-					<input autocomplete='off' style='display: inline; margin: 0; width: auto; font-size: 90%' type='submit' value='Go!' />
-				</div>
+				<input autocomplete='off' style='width: auto;' name='oekW' type='text' size='3' value='".$oekW."'/>".
+				"x".
+				"<input autocomplete='off' style='width: auto;' name='oekH' type='text' size='3' value='".$oekH."'/>".
+				"<input autocomplete='off' type='submit' value='Create!' />
 			</form>
 			"
 			, "left", 21)); // upload is 20
