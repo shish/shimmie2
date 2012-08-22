10fadb27d148c4a2c4af5790d53733ec1c239bcb
velocity37 <velocity37@gmail.com>
2012-08-21 18:46:34 -0700

    Make thumb_convert_path not Windows-specific

--- ext/image/main.php
+++ ext/image/main.php
@@ -135,7 +135,7 @@ class ImageIO extends Extension {
 		$config->set_default_int('thumb_height', 192);
 		$config->set_default_int('thumb_quality', 75);
 		$config->set_default_int('thumb_mem_limit', parse_shorthand_int('8MB'));
-		$config->set_default_string('thumb_convert_path', 'convert.exe');
+		$config->set_default_string('thumb_convert_path', 'convert');
 
 		if(function_exists("exif_read_data")) {
 			$config->set_default_bool('image_show_meta', false);
