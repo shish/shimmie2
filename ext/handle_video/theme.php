181318e0de8c9022c7b6e744d7d2e0ae0da3394c
velocity37 <velocity37@gmail.com>
2012-09-23 15:15:17 -0700

    Use standard URL scheme for manual download

--- ext/handle_video/theme.php
+++ ext/handle_video/theme.php
@@ -7,7 +7,7 @@ class VideoFileHandlerTheme extends Themelet {
 		$ext = strtolower($image->get_ext());
 		
 		if ($ext == "mp4") {
-			$html = "Video not playing? <a href='" . make_link("/image/" . $image->id) . "'>Click here</a> to download the file.<br><script language='JavaScript' type='text/javascript'>
+			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><script language='JavaScript' type='text/javascript'>
 if( navigator.userAgent.match(/Firefox/i) ||
 	navigator.userAgent.match(/Opera/i) ||
 	(navigator.userAgent.match(/MSIE/i) && parseFloat(navigator.appVersion.split('MSIE')[1]) < 9)){
@@ -21,13 +21,13 @@ else {
 </script>
 <noscript>Javascript appears to be disabled. Please enable it and try again.</noscript>";
 		} elseif ($ext == "flv") {
-			$html = "Video not playing? <a href='" . make_link("/image/" . $image->id) . "'>Click here</a> to download the file.<br><object data='$data_href/lib/Jaris/bin/JarisFLVPlayer.swf' id='VideoPlayer' type='application/x-shockwave-flash' height='" . strval($image->height + 1). "px' width='" . strval($image->width) . "px'><param value='#000000' name='bgcolor'><param name='allowFullScreen' value='true'><param value='high' name='quality'><param value='opaque' name='wmode'><param value='source=$ilink&amp;type=video&amp;streamtype=file&amp;controltype=0' name='flashvars'></object>";
+			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><object data='$data_href/lib/Jaris/bin/JarisFLVPlayer.swf' id='VideoPlayer' type='application/x-shockwave-flash' height='" . strval($image->height + 1). "px' width='" . strval($image->width) . "px'><param value='#000000' name='bgcolor'><param name='allowFullScreen' value='true'><param value='high' name='quality'><param value='opaque' name='wmode'><param value='source=$ilink&amp;type=video&amp;streamtype=file&amp;controltype=0' name='flashvars'></object>";
 		} elseif ($ext == "ogv") {
-			$html = "Video not playing? <a href='" . make_link("/image/" . $image->id) . "'>Click here</a> to download the file.<br><video controls='controls' autoplay='autoplay'>
+			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><video controls='controls' autoplay='autoplay'>
 			<source src='" . make_link("/image/" . $image->id) . "' type='video/ogg' /><br>
 			</video>";
 		} elseif ($ext == "webm") {
-			$html = "Video not playing? <a href='" . make_link("/image/" . $image->id) . "'>Click here</a> to download the file.<br><video controls='controls' autoplay='autoplay'>
+			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><video controls='controls' autoplay='autoplay'>
 			<source src='" . make_link("/image/" . $image->id) . "' type='video/webm' /><br>
 			</video>";
 		}
