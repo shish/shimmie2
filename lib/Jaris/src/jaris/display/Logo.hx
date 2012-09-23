c9bacdf56d2b4d234dc240f8ceb69cd1b48297d8
velocity37 <velocity37@gmail.com>
2012-09-23 14:18:12 -0700

    Handle Video: Flash/HTML5 video player
    Requires getID3() and Jaris FLV Player

---
+++ lib/Jaris/src/jaris/display/Logo.hx
@@ -0,0 +1,185 @@
+﻿/**    
+ * @author Jefferson González
+ * @copyright 2010 Jefferson González
+ *
+ * @license 
+ * This file is part of Jaris FLV Player.
+ *
+ * Jaris FLV Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License or GNU LESSER GENERAL 
+ * PUBLIC LICENSE as published by the Free Software Foundation, either version 
+ * 3 of the License, or (at your option) any later version.
+ *
+ * Jaris FLV Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License and 
+ * GNU LESSER GENERAL PUBLIC LICENSE along with Jaris FLV Player.  If not, 
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package jaris.display;
+
+import flash.display.Loader;
+import flash.display.MovieClip;
+import flash.display.Sprite;
+import flash.display.Stage;
+import flash.events.Event;
+import flash.events.IOErrorEvent;
+import flash.events.MouseEvent;
+import flash.Lib;
+import flash.net.URLRequest;
+
+/**
+ * To display an image in jpg, png or gif format as logo
+ */
+class Logo extends Sprite
+{
+	private var _stage:Stage;
+	private var _movieClip:MovieClip;
+	private var _loader:Loader;
+	private var _position:String;
+	private var _alpha:Float;
+	private var _source:String;
+	private var _width:Float;
+	private var _link:String;
+	private var _loading:Bool;
+	
+	public function new(source:String, position:String, alpha:Float, width:Float=0.0) 
+	{
+		super();
+		
+		_stage = Lib.current.stage;
+		_movieClip = Lib.current;
+		_loader = new Loader();
+		_position = position;
+		_alpha = alpha;
+		_source = source;
+		_width = width;
+		_loading = true;
+		
+		this.tabEnabled = false;
+		
+		_loader.contentLoaderInfo.addEventListener(Event.COMPLETE, onLoaderComplete);
+		_loader.contentLoaderInfo.addEventListener(IOErrorEvent.IO_ERROR, onNotLoaded);
+        _loader.load(new URLRequest(source));
+	}
+	
+	/**
+	 * Triggers when the logo image could not be loaded
+	 * @param	event
+	 */
+	private function onNotLoaded(event:IOErrorEvent):Void
+	{
+		//Image not loaded
+	}
+	
+	/**
+	 * Triggers when the logo image finished loading.
+	 * @param	event
+	 */
+	private function onLoaderComplete(event:Event):Void
+	{
+		addChild(_loader);
+		
+		setWidth(_width);
+		setPosition(_position);
+		setAlpha(_alpha);
+		_loading = false;
+		
+		_stage.addEventListener(Event.RESIZE, onStageResize);
+	}
+	
+	/**
+	 * Recalculate logo position on stage resize
+	 * @param	event
+	 */
+	private function onStageResize(event:Event):Void
+	{
+		setPosition(_position);
+	}
+	
+	/**
+	 * Opens the an url when the logo is clicked
+	 * @param	event
+	 */
+	private function onLogoClick(event:MouseEvent):Void
+	{
+		Lib.getURL(new URLRequest(_link), "_blank");
+	}
+	
+	/**
+	 * Position where logo will be showing
+	 * @param	position values could be top left, top right, bottom left, bottom right
+	 */
+	public function setPosition(position:String):Void
+	{
+		switch(position)
+		{
+			case "top left":
+				this.x = 25;
+				this.y = 25;
+			
+			case "top right":
+				this.x = _stage.stageWidth - this._width - 25;
+				this.y = 25;
+			
+			case "bottom left":
+				this.x = 25;
+				this.y = _stage.stageHeight - this.height - 25;
+			
+			case "bottom right":
+				this.x = _stage.stageWidth - this.width - 25;
+				this.y = _stage.stageHeight - this.height - 25;
+				
+			default: 
+				this.x = 25;
+				this.y = 25;
+		}
+	}
+	
+	/**
+	 * To set logo transparency
+	 * @param	alpha
+	 */
+	public function setAlpha(alpha:Float):Void
+	{
+		this.alpha = alpha;
+	}
+	
+	/**
+	 * Sets logo width and recalculates height keeping aspect ratio
+	 * @param	width
+	 */
+	public function setWidth(width:Float):Void
+	{
+		if (width > 0)
+		{
+			this.height = (this.height / this.width) * width;
+			this.width = width;
+		}
+	}
+	
+	/**
+	 * Link that opens when clicked the logo image is clicked
+	 * @param	link
+	 */
+	public function setLink(link:String):Void
+	{
+		_link = link;
+		this.buttonMode = true;
+		this.useHandCursor = true;
+		this.addEventListener(MouseEvent.CLICK, onLogoClick);
+	}
+	
+	/**
+	 * To check if the logo stills loading
+	 * @return true if loading false otherwise
+	 */
+	public function isLoading():Bool
+	{
+		return _loading;
+	}
+}
---
+++ lib/Jaris/src/jaris/display/Menu.hx
@@ -0,0 +1,246 @@
+﻿/**    
+ * @author Jefferson González
+ * @copyright 2010 Jefferson González
+ *
+ * @license 
+ * This file is part of Jaris FLV Player.
+ *
+ * Jaris FLV Player is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License or GNU LESSER GENERAL 
+ * PUBLIC LICENSE as published by the Free Software Foundation, either version 
+ * 3 of the License, or (at your option) any later version.
+ *
+ * Jaris FLV Player is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License and 
+ * GNU LESSER GENERAL PUBLIC LICENSE along with Jaris FLV Player.  If not, 
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package jaris.display;
+import flash.display.MovieClip;
+import flash.events.ContextMenuEvent;
+import flash.Lib;
+import flash.net.URLRequest;
+import flash.ui.ContextMenu;
+import flash.ui.ContextMenuItem;
+import jaris.player.Player;
+import jaris.player.AspectRatio;
+import jaris.Version;
+
+/**
+ * Modify original context menu
+ */
+class Menu 
+{
+	private var _movieClip:MovieClip;
+	public static var _player:Player;
+	
+	private var _contextMenu:ContextMenu;
+	private var _jarisVersionMenuItem:ContextMenuItem;
+	private var _playMenuItem:ContextMenuItem;
+	private var _fullscreenMenuItem:ContextMenuItem;
+	private var _aspectRatioMenuItem:ContextMenuItem;
+	private var _muteMenuItem:ContextMenuItem;
+	private var _volumeUpMenuItem:ContextMenuItem;
+	private var _volumeDownMenuItem:ContextMenuItem;
+	private var _qualityContextMenu:ContextMenuItem;
+	
+	public function new(player:Player) 
+	{
+		_movieClip = Lib.current;
+		_player = player;
+		
+		//Initialize context menu replacement
+		_contextMenu = new ContextMenu();
+		_contextMenu.hideBuiltInItems();
+		
+		_contextMenu.addEventListener(ContextMenuEvent.MENU_SELECT, onMenuOpen);
+		
+		//Initialize each menu item
+		_jarisVersionMenuItem = new ContextMenuItem("Jaris Player v" + Version.NUMBER + " " + Version.STATUS, true, true, true);
+		_jarisVersionMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onJarisVersion);
+		
+		_playMenuItem = new ContextMenuItem("Play (SPACE)", true, true, true);
+		_playMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onPlay);
+		
+		_fullscreenMenuItem = new ContextMenuItem("Fullscreen View (F)");
+		_fullscreenMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onFullscreen);
+		
+		_aspectRatioMenuItem = new ContextMenuItem("Aspect Ratio (original) (TAB)");
+		_aspectRatioMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onAspectRatio);
+		
+		_muteMenuItem = new ContextMenuItem("Mute (M)");
+		_muteMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onMute);
+		
+		_volumeUpMenuItem = new ContextMenuItem("Volume + (arrow UP)");
+		_volumeUpMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onVolumeUp);
+		
+		_volumeDownMenuItem = new ContextMenuItem("Volume - (arrow DOWN)");
+		_volumeDownMenuItem.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onVolumeDown);
+		
+		_qualityContextMenu = new ContextMenuItem("Lower Quality", true, true, true);
+		_qualityContextMenu.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onQuality);
+		
+		//add all context menu items to context menu object
+		_contextMenu.customItems.push(_jarisVersionMenuItem);
+		_contextMenu.customItems.push(_playMenuItem);
+		_contextMenu.customItems.push(_fullscreenMenuItem);
+		_contextMenu.customItems.push(_aspectRatioMenuItem);
+		_contextMenu.customItems.push(_muteMenuItem);
+		_contextMenu.customItems.push(_volumeUpMenuItem);
+		_contextMenu.customItems.push(_volumeDownMenuItem);
+		_contextMenu.customItems.push(_qualityContextMenu);
+		
+		//override default context menu
+		_movieClip.contextMenu = _contextMenu;
+	}
+	
+	/**
+	 * Update context menu item captions depending on player status before showing them
+	 * @param	event
+	 */
+	private function onMenuOpen(event:ContextMenuEvent):Void
+	{
+		if (_player.isPlaying())
+		{
+			_playMenuItem.caption = "Pause (SPACE)";
+		}
+		else
+		{
+			_playMenuItem.caption = "Play (SPACE)";
+		}
+		
+		if (_player.isFullscreen())
+		{
+			_fullscreenMenuItem.caption = "Normal View";
+		}
+		else
+		{
+			_fullscreenMenuItem.caption = "Fullscreen View (F)";
+		}
+		
+		if (_player.getMute())
+		{
+			_muteMenuItem.caption = _player.isFullscreen()?"Unmute":"Unmute (M)";
+		}
+		else
+		{
+			_muteMenuItem.caption = _player.isFullscreen()?"Mute":"Mute (M)";
+		}
+		
+		switch(_player.getAspectRatioString())
+		{
+			case "original":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (1:1) (TAB)";
+				
+			case "1:1":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (3:2) (TAB)";
+				
+			case "3:2":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (4:3) (TAB)";
+				
+			case "4:3":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (5:4) (TAB)";
+				
+			case "5:4":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (14:9) (TAB)";
+				
+			case "14:9":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (14:10) (TAB)";
+				
+			case "14:10":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (16:9) (TAB)";
+				
+			case "16:9":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (16:10) (TAB)";
+				
+			case "16:10":
+				_aspectRatioMenuItem.caption = "Aspect Ratio (original) (TAB)";
+		}
+		
+		if (_player.getQuality())
+		{
+			_qualityContextMenu.caption = "Lower Quality";
+		}
+		else
+		{
+			_qualityContextMenu.caption = "Higher Quality";
+		}
+	}
+	
+	/**
+	 * Open jaris player website
+	 * @param	event
+	 */
+	private function onJarisVersion(event:ContextMenuEvent)
+	{
+		Lib.getURL(new URLRequest("http://jaris.sourceforge.net"), "_blank");
+	}
+	
+	/**
+	 * Toggles playback
+	 * @param	event
+	 */
+	private function onPlay(event:ContextMenuEvent)
+	{
+		_player.togglePlay();
+	}
+	
+	/**
+	 * Toggles fullscreen
+	 * @param	event
+	 */
+	private function onFullscreen(event:ContextMenuEvent)
+	{
+		_player.toggleFullscreen();
+	}
+	
+	/**
+	 * Toggles aspect ratio
+	 * @param	event
+	 */
+	private function onAspectRatio(event:ContextMenuEvent)
+	{
+		_player.toggleAspectRatio();
+	}
+	
+	/**
+	 * Toggles mute
+	 * @param	event
+	 */
+	private function onMute(event:ContextMenuEvent)
+	{
+		_player.toggleMute();
+	}
+	
+	/**
+	 * Raise volume
+	 * @param	event
+	 */
+	private function onVolumeUp(event:ContextMenuEvent)
+	{
+		_player.volumeUp();
+	}
+	
+	/**
+	 * Lower volume
+	 * @param	event
+	 */
+	private function onVolumeDown(event:ContextMenuEvent)
+	{
+		_player.volumeDown();
+	}
+	
+	/**
+	 * Toggle video quality
+	 * @param	event
+	 */
+	private function onQuality(event:ContextMenuEvent)
+	{
+		_player.toggleQuality();
+	}
+}
