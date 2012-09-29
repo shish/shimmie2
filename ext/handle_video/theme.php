f5b177fd113f8a3506b14fbf108d96a5b7ab100d
velocity37 <velocity37@gmail.com>
2012-09-29 15:21:43 -0700

    <br> not needed here

--- ext/handle_video/theme.php
+++ ext/handle_video/theme.php
@@ -24,11 +24,11 @@ else {
 			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><object data='$data_href/lib/Jaris/bin/JarisFLVPlayer.swf' id='VideoPlayer' type='application/x-shockwave-flash' height='" . strval($image->height + 1). "px' width='" . strval($image->width) . "px'><param value='#000000' name='bgcolor'><param name='allowFullScreen' value='true'><param value='high' name='quality'><param value='opaque' name='wmode'><param value='source=$ilink&amp;type=video&amp;streamtype=file&amp;controltype=0' name='flashvars'></object>";
 		} elseif ($ext == "ogv") {
 			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><video controls='controls' autoplay='autoplay'>
-			<source src='" . make_link("/image/" . $image->id) . "' type='video/ogg' /><br>
+			<source src='" . make_link("/image/" . $image->id) . "' type='video/ogg' />
 			</video>";
 		} elseif ($ext == "webm") {
 			$html = "Video not playing? <a href='" . $image->parse_link_template(make_link('image/$id/$id%20-%20$tags.$ext')) . "'>Click here</a> to download the file.<br><video controls='controls' autoplay='autoplay'>
-			<source src='" . make_link("/image/" . $image->id) . "' type='video/webm' /><br>
+			<source src='" . make_link("/image/" . $image->id) . "' type='video/webm' />
 			</video>";
 		}
 		$page->add_block(new Block("Video", $html, "main", 10));
