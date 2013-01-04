471bc1805a0df6fb43e2b97cd0557ced6ec22dd2
Shish <shish@shishnet.org>
2013-01-04 22:40:16 +0000

    escape title too

--- ext/comment/theme.php
+++ ext/comment/theme.php
@@ -198,7 +198,7 @@ class CommentListTheme extends Themelet {
 		$h_index = "<a href='".make_link("post/list")."'>Index</a>";
 		$h_next = ($page_number >= $total_pages) ? "Next" : "<a href='$next'>Next</a>";
 
-		$page->set_title($user->name."'s comments");
+		$page->set_title(html_escape($user->name)."'s comments");
 		$page->add_block(new Block("Navigation", $h_prev.' | '.$h_index.' | '.$h_next, "left", 0));
 		$this->display_paginator($page, "comment/beta-search/{$user->name}", null, $page_number, $total_pages);
 	}
