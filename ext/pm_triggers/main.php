bb0ebc5d429bd548a297a9d6f5fb2348a3324143
Shish <shish@shishnet.org>
2013-04-26 17:27:22 +0100

    first version of pm_trigger ext

---
+++ ext/pm_triggers/main.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Name: [beta] PM triggers
+ * Author: Shish <webmaster@shishnet.org>
+ * License: GPLv2
+ * Description: Send PMs in response to certain events (eg image deletion)
+ */
+
+class PMTrigger extends Extension {
+	public function onImageDeletion(ImageDeletionEvent $event) {
+		$this->send(
+			$event->image->owner_id,
+			"[System] An image you uploaded has been deleted",
+			"Image le gone~ (#{$image->id}, {$image->get_tag_list()})"
+		);
+	}
+
+	private function send($to_id, $subject, $body) {
+		global $user;
+		send_event(new SendPMEvent(new PM(
+			$user->id,
+			$_SERVER["REMOTE_ADDR"],
+			$to_id,
+			$subject,
+			$body
+		)));
+	}
+}
+?>
