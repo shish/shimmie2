393a9e60df0b282ddd130b08c2c5012749fc6d7e
jgen <jeffgenovy@gmail.com>
2013-10-03 21:17:15 -0400

    No need to have HTML in the image thumb title. Derp.

--- core/imageboard.pack.php
+++ core/imageboard.pack.php
@@ -563,7 +563,7 @@ class Image {
 		$tmpl = str_replace('$filesize', to_shorthand_int($this->filesize), $tmpl);
 		$tmpl = str_replace('$filename', $_escape($base_fname), $tmpl);
 		$tmpl = str_replace('$title', $_escape($config->get_string("title")), $tmpl);
-		$tmpl = str_replace('$date', $_escape(autodate($this->posted)), $tmpl);
+		$tmpl = str_replace('$date', $_escape(autodate($this->posted), false), $tmpl);
 
 		// nothing seems to use this, sending the event out to 50 exts is a lot of overhead
 		if(!SPEED_HAX) {
