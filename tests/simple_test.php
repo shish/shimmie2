8d8ebddadcd340af56c64c003a95491c3f37d19e
jgen <jeffgenovy@gmail.com>
2014-02-10 00:44:30 -0500

    Change the include to the SimpleTest files.

--- tests/simple_test.php
+++ tests/simple_test.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once('../lib/simpletest/autorun.php');
+require_once('lib/simpletest/autorun.php');
+require_once('lib/simpletest/unit_tester.php');
+require_once('lib/simpletest/reporter.php');
 
 class SimpleTestCase extends UnitTestCase {
 	
