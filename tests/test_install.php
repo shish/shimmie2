630f5c2c779717c491717961e5ac83094d197a76
jgen <jeffgenovy@gmail.com>
2014-02-17 15:03:27 -0500

    This include is necessary.

--- tests/test_install.php
+++ tests/test_install.php
@@ -2,6 +2,7 @@
 
 require_once('lib/simpletest/autorun.php');
 require_once('lib/simpletest/unit_tester.php');
+require_once('lib/simpletest/web_tester.php');
 require_once('lib/simpletest/reporter.php');
 
 class ShimmieSimpleTestCase extends WebTestCase {
