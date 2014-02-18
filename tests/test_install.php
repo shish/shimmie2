f12b32b5d9b3b81ce4dd11bd07bcf11f2670ee15
jgen <jeffgenovy@gmail.com>
2014-02-18 15:41:42 -0500

    Fail if there is no database defined.

--- tests/test_install.php
+++ tests/test_install.php
@@ -3,15 +3,27 @@
 $options = getopt("d:");
 $db = $options["d"];
 
+if (is_empty($db)){
+	die("Error: need to specifiy a database for the test environment.");
+}
+
 require_once('lib/simpletest/autorun.php');
 require_once('lib/simpletest/unit_tester.php');
 require_once('lib/simpletest/web_tester.php');
 require_once('lib/simpletest/reporter.php');
 
 class ShimmieSimpleTestCase extends WebTestCase {
+	var $database;
+	
+	function ShimmieTestCase() {
+		$this->database = $db;
+	}
 	
 	function testInstallShimmie()
 	{
+		// Make sure that we know what database to use.
+		$this->assertFalse(is_empty($this->database));
+		
 		$this->get('http://127.0.0.1/');
 		$this->assertResponse(200);
 		$this->assertTitle("Shimmie Installation");
@@ -20,10 +32,10 @@ class ShimmieSimpleTestCase extends WebTestCase {
 		$this->setField("database_type", $this->database);
 		$this->assertField("database_host", "localhost");
 		
-		if ($db === "mysql") {
+		if ($this->database === "mysql") {
 			$this->setField("database_user", "root");
 			$this->setField("database_password", "");
-		} elseif ($db === "pgsql") {
+		} elseif ($this->database === "pgsql") {
 			$this->setField("database_user", "postgres");
 			$this->setField("database_password", "");
 		}		
