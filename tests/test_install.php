8ade958dd11862cd44fa3ea6c191a0508ffe88d8
jgen <jeffgenovy@gmail.com>
2014-02-22 16:04:04 -0500

    Working on getting PostgreSQL to work on Travis.

--- tests/test_install.php
+++ tests/test_install.php
@@ -15,22 +15,37 @@ require_once('lib/simpletest/reporter.php');
 // Enable all errors.
 error_reporting(E_ALL);
 
-// Get the command line option telling us what database to use.
+// Get the command line option telling us what database and host to use.
 $options = getopt("d:h:");
 $db = $options["d"];
 $host = rtrim(trim(trim($options["h"], '"')), "/");
 
+// Check if they are empty.
 if (empty($db)){ die("Error: need to specifiy a database for the test environment."); }
 if (empty($host)){ $host = "http://127.0.0.1"; }
 
 define("_TRAVIS_DATABASE", $db);
 define("_TRAVIS_WEBHOST", $host);
 
+// Currently the tests only support MySQL and PostgreSQL.
+if ($db == "mysql") {
+	define("_TRAVIS_DATABASE_USERNAME", "root");
+	define("_TRAVIS_DATABASE_PASSWORD", "");
+} elseif ($db == "pgsql") {
+	define("_TRAVIS_DATABASE_USERNAME", "postgres");
+	define("_TRAVIS_DATABASE_PASSWORD", "");
+} else {
+	die("Unsupported Database Option");
+}
+
 class ShimmieInstallerTest extends WebTestCase {
 	function testInstallShimmie()
 	{
+		// Get the settings from the global constants.
 		$db = constant("_TRAVIS_DATABASE");
 		$host = constant("_TRAVIS_WEBHOST");
+		$username = constant("_TRAVIS_DATABASE_USERNAME");
+		$password = constant("_TRAVIS_DATABASE_PASSWORD");
 
 		// Make sure that we know where the host is.
 		$this->assertFalse(empty($host));
@@ -44,17 +59,8 @@ class ShimmieInstallerTest extends WebTestCase {
 
 		$this->setField("database_type", $db);
 		$this->assertField("database_host", "localhost");
-
-		if ($db === "mysql") {
-			$this->setField("database_user", "root");
-			$this->setField("database_password", "");
-		} elseif ($db === "pgsql") {
-			$this->setField("database_user", "postgres");
-			$this->setField("database_password", "");
-		} else {
-			die("Unsupported Database Option");
-		}
-
+		$this->setField("database_user", $username);
+		$this->setField("database_password", $password);
 		$this->assertField("database_name", "shimmie");
 		$this->clickSubmit("Go!");
 
