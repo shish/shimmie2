be385deaabab2fa9b6f360297c1ddbb56e90234b
jgen <jeffgenovy@gmail.com>
2014-04-19 02:58:36 -0400

    Clean up this js file as well.

--- ext/update/script.js
+++ ext/update/script.js
@@ -2,9 +2,9 @@ $(function() {
 	if($('#updatecheck').length !== 0){
 		$.getJSON('https://api.github.com/repos/shish/shimmie2/commits', function(data){
 			var c = data[0];
-			$('#updatecheck').html('<a href="'+c['html_url']+'">'+c['sha']+'</a>' + " ("+c['commit']['message']+")");
+			$('#updatecheck').html('<a href="'+ c.html_url+'">'+ c.sha+'</a>' + " ("+ c.commit.message+")");
 
-			var params = $.param({sha: c['sha'], date: c['commit']['committer']['date']});
+			var params = $.param({sha: c.sha, date: c.commit.committer.date});
 			$('#updatelink').attr('href', function(i, val){ return val + "?" + params; });
 			$('#updatelink').text("Update");
 		}).fail(function(){
