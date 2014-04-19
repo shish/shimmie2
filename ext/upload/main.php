bdf49b33cbcbaef3a456fd158fb85d435ef4421e
jgen <jeffgenovy@gmail.com>
2014-04-19 01:18:49 -0400

    Some more linting thanks to scrutinizer-ci

--- ext/upload/main.php
+++ ext/upload/main.php
@@ -277,6 +277,7 @@ class Upload extends Extension {
 				}
 				
 				$pathinfo = pathinfo($file['name']);
+				$metadata = array();
 				$metadata['filename'] = $pathinfo['basename'];
 				$metadata['extension'] = $pathinfo['extension'];
 				$metadata['tags'] = $tags;
@@ -347,6 +348,7 @@ class Upload extends Extension {
 		}else{
 			global $user;
 			$pathinfo = pathinfo($url);
+			$metadata = array();
 			$metadata['filename'] = $filename;
 			$metadata['extension'] = getExtension($headers['Content-Type']) ?: $pathinfo['extension'];
 			$metadata['tags'] = $tags;
