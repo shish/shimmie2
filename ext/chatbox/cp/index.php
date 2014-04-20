a7ba0f887b9c2e2b1729db4ac5b562221d913a74
jgen <jeffgenovy@gmail.com>
2014-04-20 05:00:59 -0400

    Again, the chatbox needs help from the jQuery migrate plugin.

--- ext/chatbox/cp/index.php
+++ ext/chatbox/cp/index.php
@@ -8,7 +8,8 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title>YShout: Admin CP</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		<script type="text/javascript" src="../js/jquery.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<script type="text/javascript" src="js/admincp.js"></script>
 	</head>
 	<body>
