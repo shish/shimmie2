60980c8839bc9643ba0ad4e9b6df4ab7cac6864b
jgen <jeffgenovy@gmail.com>
2014-04-20 05:04:45 -0400

    One more jQuery reference.

--- ext/chatbox/history/index.php
+++ ext/chatbox/history/index.php
@@ -90,7 +90,8 @@ if (isset($_POST['p'])) {
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title>YShout: History</title>
-		<script type="text/javascript" src="../js/jquery.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<script type="text/javascript" src="js/history.js"></script>
 
 		<link rel="stylesheet" href="../example/css/example.yshout.css" />
