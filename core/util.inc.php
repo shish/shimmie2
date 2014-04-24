993aacaea5d980664cc853f0c6207f2506502493
jgen <jeffgenovy@gmail.com>
2014-04-24 16:55:34 -0400

    Update comment to reflect that a bug has been filed with PHP-analyzer.

--- core/util.inc.php
+++ core/util.inc.php
@@ -870,9 +870,11 @@ function transload($url, $mfile) {
 		fwrite($fp, $data);
 		fclose($fp);
 
-		// Scrutinizer-ci complains that $http_response_header not defined.
-		// However, it is auto defined by PHP.
-		// See: http://us2.php.net/manual/en/reserved.variables.httpresponseheader.php
+		//
+		// Scrutinizer-ci complains that $http_response_header does not exist,
+		// however, $http_response_header is actually a super-global.
+		// I have filed a bug with PHP-Analyzer here: https://github.com/scrutinizer-ci/php-analyzer/issues/212
+		//
 		$headers = http_parse_headers(implode("\n", $http_response_header));
 
 		return $headers;
