9acf6c7f19d5c258f34cf3871c700fc7beb9c5bb
jgen <jeffgenovy@gmail.com>
2014-04-25 15:57:23 -0400

    Rename these class functions to prevent warnings from scrutinizer.

--- themes/futaba/themelet.class.php
+++ themes/futaba/themelet.class.php
@@ -5,24 +5,24 @@ class Themelet extends BaseThemelet {
 	 */
 	public function display_paginator(Page $page, $base, $query, $page_number, $total_pages, $position=90) {
 		if($total_pages == 0) $total_pages = 1;
-		$body = $this->build_paginator($page_number, $total_pages, $base, $query);
+		$body = $this->futaba_build_paginator($page_number, $total_pages, $base, $query);
 		$page->add_block(new Block(null, $body, "main", $position));
 	}
 
-	private function gen_page_link($base_url, $query, $page, $name) {
+	private function futaba_gen_page_link($base_url, $query, $page, $name) {
 		$link = make_link("$base_url/$page", $query);
 	    return "[<a href='$link'>{$name}</a>]";
 	}
 	
-	private function gen_page_link_block($base_url, $query, $page, $current_page, $name) {
+	private function futaba_gen_page_link_block($base_url, $query, $page, $current_page, $name) {
 		$paginator = "";
 	    if($page == $current_page) $paginator .= "<b>";
-	    $paginator .= $this->gen_page_link($base_url, $query, $page, $name);
+	    $paginator .= $this->futaba_gen_page_link($base_url, $query, $page, $name);
 	    if($page == $current_page) $paginator .= "</b>";
 	    return $paginator;
 	}
 					
-	private function build_paginator($current_page, $total_pages, $base_url, $query) {
+	private function futaba_build_paginator($current_page, $total_pages, $base_url, $query) {
 		$next = $current_page + 1;
 		$prev = $current_page - 1;
 		//$rand = mt_rand(1, $total_pages);
@@ -31,9 +31,9 @@ class Themelet extends BaseThemelet {
 		$at_end = ($current_page >= $total_pages);
 
 		//$first_html   = $at_start ? "First" : $this->litetheme_gen_page_link($base_url, $query, 1,            "First");
-		$prev_html      = $at_start ? "Prev"  : $this->gen_page_link($base_url, $query, $prev,        "Prev");
+		$prev_html      = $at_start ? "Prev"  : $this->futaba_gen_page_link($base_url, $query, $prev,           "Prev");
 		//$random_html  =                       $this->litetheme_gen_page_link($base_url, $query, $rand,        "Random");
-		$next_html      = $at_end   ? "Next"  : $this->gen_page_link($base_url, $query, $next,        "Next");
+		$next_html      = $at_end   ? "Next"  : $this->futaba_gen_page_link($base_url, $query, $next,           "Next");
 		//$last_html    = $at_end   ? "Last"  : $this->litetheme_gen_page_link($base_url, $query, $total_pages, "Last");
 
 		$start = $current_page-5 > 1 ? $current_page-5 : 1;
@@ -41,7 +41,7 @@ class Themelet extends BaseThemelet {
 
 		$pages = array();
 		foreach(range($start, $end) as $i) {
-			$pages[] = $this->gen_page_link_block($base_url, $query, $i, $current_page, $i);
+			$pages[] = $this->futaba_gen_page_link_block($base_url, $query, $i, $current_page, $i);
 		}
 		$pages_html = implode(" ", $pages);
 
