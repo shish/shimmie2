be4ae9da320152fa4475b6c5b81942870e1a5b60
Shish <shish@shishnet.org>
2014-12-18 14:38:22 +0000

    fix missing global (how did this pass tests? o.o)

--- ext/ban_words/main.php
+++ ext/ban_words/main.php
@@ -54,6 +54,7 @@ xanax
 	}
 
 	public function onCommentPosting(CommentPostingEvent $event) {
+		global $user;
 		if(!$user->can("bypass_comment_checks")) {
 			$this->test_text($event->comment, new CommentPostingException("Comment contains banned terms"));
 		}
