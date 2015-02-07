610b144fb2e265c3bbd0bc9fd4742fe4f2d4fb9a
Shish <shish@shishnet.org>
2015-02-07 22:29:07 +0000

    minor CSS tweak

--- ext/tag_edit/theme.php
+++ ext/tag_edit/theme.php
@@ -84,15 +84,16 @@ class TagEditTheme extends Themelet {
 		global $user;
 		$h_source = html_escape($image->get_source());
 		$f_source = $this->format_source($image->get_source());
+		$style = "overflow: hidden; white-space: nowrap; max-width: 350px; text-overflow: ellipsis;";
 		return "
 			<tr>
 				<th>Source</th>
 				<td>
 		".($user->can("edit_image_source") ? "
-					<span class='view' style='overflow: hidden; white-space: nowrap;'>$f_source</span>
+					<div class='view' style='$style'>$f_source</div>
 					<input class='edit' type='text' name='tag_edit__source' value='$h_source'>
 		" : "
-					<span style='overflow: hidden; white-space: nowrap;'>$f_source</span>
+					<div style='$style'>$f_source</div>
 		")."
 				</td>
 			</tr>
