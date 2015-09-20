ba3a61052f5b6b57b2279aa965531544b83f155b
Shish <shish@shishnet.org>
2015-09-20 16:46:49 +0100

    timeout on varnish purger

--- ext/varnish/main.php
+++ ext/varnish/main.php
@@ -11,8 +11,9 @@ class VarnishPurger extends Extension {
 	private function curl_purge($path) {
 		$url = make_http(make_link($path));
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PURGE");
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 		$result = curl_exec($ch);
 		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
