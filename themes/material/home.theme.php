d21e86aeb0de727885630089527cf2cb84fbb2bb
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2015-12-28 02:37:05 +0100

    Cheated a bit to test it out ;)

--- themes/material/home.theme.php
+++ themes/material/home.theme.php
@@ -26,6 +26,9 @@ class CustomHomeTheme extends HomeTheme {
 		}
 	</style>
 	<body>
+		<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-pink.min.css">
+		<script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 		$body
 	</body>
 </html>
