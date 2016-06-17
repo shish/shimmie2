7bc9a73357699bdcce9ff1b1a31346475c27ba2e
Shish <shish@shishnet.org>
2016-06-18 00:00:02 +0100

    migrate to a non-deprecated (and dependency-free) cookie library

--- ext/blotter/script.js
+++ ext/blotter/script.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
 	$(".shm-blotter2-toggle").click(function() {
 		$(".shm-blotter2").slideToggle("slow", function() {
 			if($(".shm-blotter2").is(":hidden")) {
-				$.cookie("ui-blotter2-hidden", 'true', {path: '/'});
+				Cookies.set("ui-blotter2-hidden", 'true');
 			}
 			else {
-				$.cookie("ui-blotter2-hidden", 'false', {path: '/'});
+				Cookies.set("ui-blotter2-hidden", 'false');
 			}
 		});
 	});
-	if($.cookie("ui-blotter2-hidden") === 'true') {
+	if(Cookies.get("ui-blotter2-hidden") === 'true') {
 		$(".shm-blotter2").hide();
 	}
 });
