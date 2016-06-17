f3a3c7698824ba1c48a71ae13073eb76c7ee53ff
Shish <shish@shishnet.org>
2016-06-18 00:45:58 +0100

    better test router

--- tests/router.php
+++ tests/router.php
@@ -18,5 +18,6 @@ if(preg_match('/\.(?:png|jpg|jpeg|gif|css|js|php)(\?.*)?$/', $_SERVER["REQUEST_U
 
 // all other requests (use shimmie routing based on URL)
 $_SERVER["PHP_SELF"] = '/';
-$_GET['q'] = $_SERVER["REQUEST_URI"];
+$_GET['q'] = explode("?", $_SERVER["REQUEST_URI"])[0];
+error_log($_GET['q']);
 require_once "index.php";
