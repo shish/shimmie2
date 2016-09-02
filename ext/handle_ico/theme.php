f26fc5925b405c745c34b47ff13c486bb962dfbc
im-mi <im.mi.mail.mi@gmail.com>
2016-09-01 23:08:28 -0400

    Added zoom support to ico

--- ext/handle_ico/theme.php
+++ ext/handle_ico/theme.php
@@ -4,7 +4,8 @@ class IcoFileHandlerTheme extends Themelet {
 	public function display_image(Page $page, Image $image) {
 		$ilink = make_link("get_ico/{$image->id}/{$image->id}.ico");
 		$html = "
-			<img id='main_image' src='$ilink'>
+			<img id='main_image' class='shm-main-image' alt='main image' src='$ilink'
+			data-width='{$image->width}' data-height='{$image->height}'>
 		";
 		$page->add_block(new Block("Image", $html, "main", 10));
 	}
