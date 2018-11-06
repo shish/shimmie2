fc6fb3c6b88919b1320a2881f3a833468a29d634
Shish <shish@shishnet.org>
2018-11-06 00:02:07 +0000

    use current protocol for niceurl test, see #632

--- ext/setup/script.js
+++
@@ -1,8 +0,0 @@
-function getHTTPObject() {
-	if (window.XMLHttpRequest){
-		return new XMLHttpRequest();
-	}
-	else if(window.ActiveXObject){
-		return new ActiveXObject("Microsoft.XMLHTTP");
-	}
-}
