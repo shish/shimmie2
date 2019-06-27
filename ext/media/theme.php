d2d6c433e68f2ea14442be6183392cd82e32f3e4
Matthew Barbour <matthew@darkholme.net>
2019-06-26 23:13:15 -0500

    Make the media admin block look a little nicer

--- ext/media/theme.php
+++ ext/media/theme.php
@@ -8,13 +8,14 @@ class MediaTheme extends Themelet
 
         $html = "Use this to force scanning for media properties.";
         $html .= make_form(make_link("admin/media_rescan"));
-        $html .= "<select name='media_rescan_type'><option value=''>All</option>";
+        $html .= "<table class='form'>";
+        $html .= "<tr><th>Image Type</th><td><select name='media_rescan_type'><option value=''>All</option>";
         foreach ($types as $type) {
             $html .= "<option value='".$type["ext"]."'>".$type["ext"]." (".$type["count"].")</option>";
         }
-        $html .= "</select><br/>";
-        $html .= "<input type='submit' value='Scan Media Information'>";
-        $html .= "</form>\n";
+        $html .= "</select></td></tr>";
+        $html .= "<tr><td colspan='2'><input type='submit' value='Scan Media Information'></td></tr>";
+        $html .= "</table></form>\n";
         $page->add_block(new Block("Media Tools", $html));
     }
 
