0fa2adfdd5d538135ca98d503558e8f57fea9a20
matthew <matthew@darkholme.net>
2019-06-27 08:12:15 -0500

    Added $user_config global based on existing config object for storing user-specific settings. Added event to the user page so that extensions can hook into it, providing user-specific setting controls

--- ext/upgrade/main.php
+++ ext/upgrade/main.php
@@ -224,7 +224,24 @@ class Upgrade extends Extension
             $config->set_bool("in_upgrade", false);
         }
 
+        if ($config->get_int("db_version") < 18) {
+            $config->set_bool("in_upgrade", true);
+            $config->set_int("db_version", 18);
 
+            log_info("upgrade", "Adding user config table");
+
+            $database->create_table("user_config", "
+                user_id INTEGER NOT NULL,
+                name VARCHAR(128) NOT NULL,
+                value TEXT,
+                PRIMARY KEY (user_id, name),
+			    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+		    ");
+            $database->execute("CREATE INDEX user_config_user_id_idx ON user_config(user_id)");
+
+            log_info("upgrade", "Database at version 18");
+            $config->set_bool("in_upgrade", false);
+        }
 
     }
 
