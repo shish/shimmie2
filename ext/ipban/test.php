56bb03f01aaf9e13e27ab13a36e76d9b7853ac12
Shish <shish@shishnet.org>
2019-11-24 13:25:41 +0000

    and tests

--- ext/ipban/test.php
+++ ext/ipban/test.php
@@ -14,16 +14,16 @@ class IPBanTest extends ShimmiePHPUnitTestCase
 
         $this->markTestIncomplete();
 
-        $this->set_field('ip', '42.42.42.42');
-        $this->set_field('reason', 'unit testing');
-        $this->set_field('end', '1 week');
+        $this->set_field('c_ip', '42.42.42.42');
+        $this->set_field('c_reason', 'unit testing');
+        $this->set_field('c_expires', '1 week');
         $this->click("Ban");
 
         $this->assert_text("42.42.42.42");
         $this->click("Remove"); // FIXME: remove which ban? :S
         $this->assert_no_text("42.42.42.42");
 
-        $this->get_page('ip_ban/list?all=on'); // just test it doesn't crash for now
+        $this->get_page('ip_ban/list?r_all=on'); // just test it doesn't crash for now
 
         # FIXME: test that the IP is actually banned
     }
