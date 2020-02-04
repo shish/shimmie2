35aca4fa9a96a1400cc7f4ab486b9f808b330fde
Shish <shish@shishnet.org>
2020-02-04 01:22:26 +0000

    separate calls for A and LINK??

--- ext/view/script.js
+++ ext/view/script.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
 	if(document.location.hash.length > 3) {
 		var query = document.location.hash.substring(1);
 
-		$('#prevlink').attr('href', function(i, attr) {
+		$('LINK#prevlink').attr('href', function(i, attr) {
 			return attr + '?' + query;
 		});
-		$('#nextlink').attr('href', function(i, attr) {
+		$('LINK#nextlink').attr('href', function(i, attr) {
+			return attr + '?' + query;
+		});
+		$('A#prevlink').attr('href', function(i, attr) {
+			return attr + '?' + query;
+		});
+		$('A#nextlink').attr('href', function(i, attr) {
 			return attr + '?' + query;
 		});
 	}
