081aa2c1c65f29eb1b322bcbffef3b4d9de361d3
Shish <shish@shishnet.org>
2020-03-24 22:00:28 +0000

    update composer dependencies to avoid depending on a bad version of eventtracer

--- core/sys_config.php
+++ core/sys_config.php
@@ -38,7 +38,7 @@ _d("COMPILE_ELS", false);    // boolean  pre-build the list of event listeners
 _d("NICE_URLS", false);      // boolean  force niceurl mode
 _d("SEARCH_ACCEL", false);   // boolean  use search accelerator
 _d("WH_SPLITS", 1);          // int      how many levels of subfolders to put in the warehouse
-_d("VERSION", '2.7.0');      // string   shimmie version
+_d("VERSION", '2.7.1');      // string   shimmie version
 _d("TIMEZONE", null);        // string   timezone
 _d("EXTRA_EXTS", "");        // string   optional extra extensions
 _d("BASE_URL", null);        // string   force a specific base URL (default is auto-detect)
