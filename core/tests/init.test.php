b0c5043892f28766521ffc015557ff10321e1674
Shish <shish@shishnet.org>
2020-03-26 19:06:30 +0000

    test InitExt / DatabaseUpgrade

---
+++ core/tests/init.test.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class TestInit extends TestCase {
+    public function testInitExt()
+    {
+        send_event(new InitExtEvent());
+        $this->assertTrue(true);
+    }
+
+    public function testDatabaseUpgrade()
+    {
+        send_event(new DatabaseUpgradeEvent());
+        $this->assertTrue(true);
+    }
+}
