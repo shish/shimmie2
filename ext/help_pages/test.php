1f50f14672f35ad14522b4eec63bf5c7d26e8ac8
Shish <shish@shishnet.org>
2020-03-26 21:28:23 +0000

    basic tests for help pages

---
+++ ext/help_pages/test.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+class HelpPagesTest extends ShimmiePHPUnitTestCase
+{
+
+    public function test_list()
+    {
+        send_event(new HelpPageListBuildingEvent());
+        $this->assertTrue(true);
+    }
+    
+    public function test_page()
+    {
+        send_event(new HelpPageBuildingEvent("test"));
+        $this->assertTrue(true);
+    }
+}
