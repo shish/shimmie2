d85f4d3799bbda37812c75f151a67ac110b74611
Shish <shish@shishnet.org>
2020-03-27 12:24:47 +0000

    automatic version

--- core/sys_config.php
+++ core/sys_config.php
@@ -18,6 +18,7 @@ function _d(string $name, $value): void
         define($name, $value);
     }
 }
+$_g = file_exists(".git") ? '+' : '';
 _d("DATABASE_DSN", null);    // string   PDO database connection details
 _d("DATABASE_TIMEOUT", 10000);// int     Time to wait for each statement to complete
 _d("CACHE_DSN", null);       // string   cache connection details
@@ -25,7 +26,7 @@ _d("DEBUG", false);          // boolean  print various debugging details
 _d("COOKIE_PREFIX", 'shm');  // string   if you run multiple galleries with non-shared logins, give them different prefixes
 _d("SPEED_HAX", false);      // boolean  do some questionable things in the name of performance
 _d("WH_SPLITS", 1);          // int      how many levels of subfolders to put in the warehouse
-_d("VERSION", '2.8.2+');     // string   shimmie version
+_d("VERSION", "2.8.2$_g");   // string   shimmie version
 _d("TIMEZONE", null);        // string   timezone
 _d("EXTRA_EXTS", "");        // string   optional extra extensions
 _d("BASE_HREF", null);       // string   force a specific base URL (default is auto-detect)
