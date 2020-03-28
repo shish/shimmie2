ab4b7453105e336c711b1f26d75b7bb6dcfa2842
Shish <shish@shishnet.org>
2020-03-28 14:33:48 +0000

    test

--- ext/image_hash_ban/test.php
+++ ext/image_hash_ban/test.php
@@ -3,6 +3,13 @@ class ImageBanTest extends ShimmiePHPUnitTestCase
 {
     private $hash = "feb01bab5698a11dd87416724c7a89e3";
 
+    public function testPages()
+    {
+        $this->log_in_as_admin();
+        $page = $this->get_page("image_hash_ban/list");
+        $this->assertEquals(200, $page->code);
+    }
+
     public function testBan()
     {
         $this->log_in_as_admin();
