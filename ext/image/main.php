a3a3aba0404e671cce139708ccd5add2afd295c2
Matthew Barbour <matthew@darkholme.net>
2020-06-16 18:40:13 -0500

    Added ImageDownloadingEvent that allows extensions to stop a user from viewing an image or perform other operations on the image before the image is downloaded to the user

--- ext/image/main.php
+++ ext/image/main.php
@@ -348,7 +348,7 @@ class ImageIO extends Extension
                 $page->add_http_header('Expires: ' . $expires);
             }
 
-            send_event(new ImageDownloadingEvent($image, $file, $image->get_mime()));
+            send_event(new ImageDownloadingEvent($image, $file, $mime));
         } else {
             $page->set_title("Not Found");
             $page->set_heading("Not Found");
