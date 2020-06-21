9aedc9354915bcc40d9aef2c83994daddba2268f
Matthew Barbour <matthew@darkholme.net>
2020-06-21 14:27:01 -0500

    Extended autocomplete plugin to work for just about all tag entry fields

--- ext/autocomplete/main.php
+++ ext/autocomplete/main.php
@@ -1,5 +1,15 @@
 <?php declare(strict_types=1);
 
+
+class AutoCompleteConfig
+{
+    public const SEARCH_LIMIT = "autocomplete_search_limit";
+    public const SEARCH_CATEGORIES = "autocomplete_search_categories";
+    public const NAVIGATION = "autocomplete_navigation";
+    public const TAGGING = "autocomplete_tagging";
+}
+
+
 class AutoComplete extends Extension
 {
     /** @var AutoCompleteTheme */
@@ -10,60 +20,24 @@ class AutoComplete extends Extension
         return 30;
     } // before Home
 
+    public function onInitExt(InitExtEvent $event)
+    {
+        global $config;
+        $config->set_default_bool(AutoCompleteConfig::NAVIGATION, true);
+        $config->set_default_bool(AutoCompleteConfig::TAGGING, false);
+        $config->set_default_int(AutoCompleteConfig::SEARCH_LIMIT, 20);
+        $config->set_default_bool(AutoCompleteConfig::SEARCH_CATEGORIES, false);
+    }
+
     public function onPageRequest(PageRequestEvent $event)
     {
-        global $cache, $page, $database;
-
-        if ($event->page_matches("api/internal/autocomplete")) {
-            if (!isset($_GET["s"])) {
-                return;
-            }
-
-            $page->set_mode(PageMode::DATA);
-            $page->set_mime(MimeType::JSON);
-
-            $s = strtolower($_GET["s"]);
-            if (
-                $s == '' ||
-                $s[0] == '_' ||
-                $s[0] == '%' ||
-                strlen($s) > 32
-            ) {
-                $page->set_data("{}");
-                return;
-            }
-
-            //$limit = 0;
-            $cache_key = "autocomplete-$s";
-            $limitSQL = "";
-            $s = str_replace('_', '\_', $s);
-            $s = str_replace('%', '\%', $s);
-            $SQLarr = ["search"=>"$s%"]; #, "cat_search"=>"%:$s%"];
-            if (isset($_GET["limit"]) && $_GET["limit"] !== 0) {
-                $limitSQL = "LIMIT :limit";
-                $SQLarr['limit'] = $_GET["limit"];
-                $cache_key .= "-" . $_GET["limit"];
-            }
-
-            $res = $cache->get($cache_key);
-            if (!$res) {
-                $res = $database->get_pairs(
-                    "
-					SELECT tag, count
-					FROM tags
-					WHERE LOWER(tag) LIKE LOWER(:search)
-					-- OR LOWER(tag) LIKE LOWER(:cat_search)
-					AND count > 0
-					ORDER BY count DESC
-					$limitSQL",
-                    $SQLarr
-                );
-                $cache->set($cache_key, $res, 600);
-            }
-
-            $page->set_data(json_encode($res));
-        }
+        global $page;
 
         $this->theme->build_autocomplete($page);
+    }
+
+    public function onSetupBuilding(SetupBuildingEvent $event)
+    {
+        $this->theme->display_admin_block($event);
     }
 }
