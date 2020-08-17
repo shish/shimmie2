a75ec6409880f684189771179f7fca5ddf37eb0e
Matthew Barbour <matthew@darkholme.net>
2020-08-17 17:05:51 -0500

    Added understanding of video codecs, primarily to allow us to determine whether a file is a webm or not, but also to support my forthcoming video transcoding extension

--- themes/lite/view.theme.php
+++ themes/lite/view.theme.php
@@ -34,6 +34,9 @@ class CustomViewImageTheme extends ViewImageTheme
 		<br>Filesize: $h_filesize
 		<br>Type: ".$h_type."
 		";
+        if($image->video_codec!=null) {
+            $html .= "<br/>Video Codec: $image->video_codec";
+        }
         if ($image->length!=null) {
             $h_length = format_milliseconds($image->length);
             $html .= "<br/>Length: $h_length";
