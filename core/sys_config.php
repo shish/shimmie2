7cb36da4c6a81fb8ec3d2ed1b7e092f6db664376
Shish <shish@shishnet.org>
2020-10-25 21:25:38 +0000

    line up comments

--- core/sys_config.php
+++ core/sys_config.php
@@ -19,16 +19,16 @@ function _d(string $name, $value): void
     }
 }
 $_g = file_exists(".git") ? '+' : '';
-_d("DATABASE_DSN", null);    // string   PDO database connection details
-_d("DATABASE_TIMEOUT", 10000);// int     Time to wait for each statement to complete
-_d("CACHE_DSN", null);       // string   cache connection details
-_d("DEBUG", false);          // boolean  print various debugging details
-_d("COOKIE_PREFIX", 'shm');  // string   if you run multiple galleries with non-shared logins, give them different prefixes
-_d("SPEED_HAX", false);      // boolean  do some questionable things in the name of performance
-_d("WH_SPLITS", 1);          // int      how many levels of subfolders to put in the warehouse
-_d("VERSION", "2.8.4$_g");   // string   shimmie version
-_d("TIMEZONE", null);        // string   timezone
-_d("EXTRA_EXTS", "");        // string   optional extra extensions
-_d("BASE_HREF", null);       // string   force a specific base URL (default is auto-detect)
-_d("TRACE_FILE", null);      // string   file to log performance data into
-_d("TRACE_THRESHOLD", 0.0);  // float    log pages which take more time than this many seconds
+_d("DATABASE_DSN", null);       // string   PDO database connection details
+_d("DATABASE_TIMEOUT", 10000);  // int      Time to wait for each statement to complete
+_d("CACHE_DSN", null);          // string   cache connection details
+_d("DEBUG", false);             // boolean  print various debugging details
+_d("COOKIE_PREFIX", 'shm');     // string   if you run multiple galleries with non-shared logins, give them different prefixes
+_d("SPEED_HAX", false);         // boolean  do some questionable things in the name of performance
+_d("WH_SPLITS", 1);             // int      how many levels of subfolders to put in the warehouse
+_d("VERSION", "2.8.4$_g");      // string   shimmie version
+_d("TIMEZONE", null);           // string   timezone
+_d("EXTRA_EXTS", "");           // string   optional extra extensions
+_d("BASE_HREF", null);          // string   force a specific base URL (default is auto-detect)
+_d("TRACE_FILE", null);         // string   file to log performance data into
+_d("TRACE_THRESHOLD", 0.0);     // float    log pages which take more time than this many seconds
