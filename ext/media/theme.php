52bce9ffd0ce208e0a19f5b3c71c36ef9e404091
Matthew Barbour <matthew@darkholme.net>
2020-10-26 10:14:50 -0500

    Image to Post - media

--- ext/media/theme.php
+++ ext/media/theme.php
@@ -20,16 +20,16 @@ class MediaTheme extends Themelet
 
     public function get_help_html()
     {
-        return '<p>Search for items based on the type of media.</p>
+        return '<p>Search for posts based on the type of media.</p>
         <div class="command_example">
         <pre>content:audio</pre>
-        <p>Returns items that contain audio, including videos and audio files.</p>
+        <p>Returns posts that contain audio, including videos and audio files.</p>
         </div>
         <div class="command_example">
         <pre>content:video</pre>
-        <p>Returns items that contain video, including animated GIFs.</p>
+        <p>Returns posts that contain video, including animated GIFs.</p>
         </div>
-        <p>These search terms depend on the items being scanned for media content. Automatic scanning was implemented in mid-2019, so items uploaded before, or items uploaded on a system without ffmpeg, will require additional scanning before this will work.</p>
+        <p>These search terms depend on the posts being scanned for media content. Automatic scanning was implemented in mid-2019, so posts uploaded before, or posts uploaded on a system without ffmpeg, will require additional scanning before this will work.</p>
         ';
     }
 }
