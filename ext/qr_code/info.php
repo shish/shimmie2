a1fc842af62c9e3f6e7c02b6ecb2d0de4b8d144b
Matthew Barbour <matthew@darkholme.net>
2020-10-26 10:21:55 -0500

    Image to Post -  qr code

--- ext/qr_code/info.php
+++ ext/qr_code/info.php
@@ -11,7 +11,7 @@ class QRImageInfo extends ExtensionInfo
     public $license = self::LICENSE_GPLV2;
     public $description = "Turns BBCode into HTML";
     public $documentation =
-"Shows a QR Code for downloading an image to cell phones.
-Based on Artanis's Link to Image Extension <artanis.00@gmail.com>
+"Shows a QR Code for downloading a post to cell phones.
+Based on Artanis's Link to Post Extension <artanis.00@gmail.com>
 Further modified by Shish to remove the 7MB local QR generator and replace it with a link to google chart APIs";
 }
