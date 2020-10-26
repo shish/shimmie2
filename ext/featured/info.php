840e661a42da61ab1408a7bc199ae94460cc866a
Matthew Barbour <matthew@darkholme.net>
2020-10-26 10:23:13 -0500

    Image to Post - featured

--- ext/featured/info.php
+++ ext/featured/info.php
@@ -12,14 +12,14 @@ class FeaturedInfo extends ExtensionInfo
     public $description = "Bring a specific image to the users' attentions";
     public $documentation =
 "Once enabled, a new \"feature this\" button will appear next
-to the other image control buttons (delete, rotate, etc).
+to the other post control buttons (delete, rotate, etc).
 Clicking it will set the image as the site's current feature,
 which will be shown in the side bar of the post list.
 <p><b>Viewing a featured post</b>
 <br>Visit <code>/featured_image/view</code>
 <p><b>Downloading a featured post</b>
 <br>Link to <code>/featured_image/download</code>. This will give
-the raw data for an image (no HTML). This is useful so that you
+the raw data for a post (no HTML). This is useful so that you
 can set your desktop wallpaper to be the download URL, refreshed
 every couple of hours.";
 }
