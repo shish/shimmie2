fa2a982303afd9099e21510f9bde5acdd825235f
Matthew Barbour <matthew@darkholme.net>
2020-10-26 10:21:45 -0500

    Image to Post - regen thumb

--- ext/regen_thumb/info.php
+++ ext/regen_thumb/info.php
@@ -11,7 +11,7 @@ class RegenThumbInfo extends ExtensionInfo
     public $license = self::LICENSE_GPLV2;
     public $description = "Regenerate a thumbnail image";
     public $documentation =
-"This adds a button in the image control section on an image's view page, which allows an admin to regenerate
-an image's thumbnail; useful for instance if the first attempt failed due to lack of memory, and memory has
+"This adds a button in the post control section on a post's view page, which allows an admin to regenerate
+a post's thumbnail; useful for instance if the first attempt failed due to lack of memory, and memory has
 since been increased.";
 }
