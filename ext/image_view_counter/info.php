a8c39085cb343e77c5ab912c9f2fe9a02730d499
Matthew Barbour <matthew@darkholme.net>
2020-10-26 10:13:28 -0500

    Image to Post - view counter

--- ext/image_view_counter/info.php
+++ ext/image_view_counter/info.php
@@ -5,13 +5,13 @@ class ImageViewCounterInfo extends ExtensionInfo
     public const KEY = "image_view_counter";
 
     public $key = self::KEY;
-    public $name = "Image View Counter";
+    public $name = "Post View Counter";
     public $url = "http://www.drudexsoftware.com/";
     public $authors = ["Drudex Software"=>"support@drudexsoftware.com"];
     public $license = self::LICENSE_GPLV2;
-    public $description = "Tracks & displays how many times an image is viewed";
+    public $description = "Tracks & displays how many times a post is viewed";
     public $documentation =
-"Whenever anyone views an image, a view will be added to that image.
+"Whenever anyone views a post, a view will be added to that image.
 This extension will also track any username & the IP address.
 This is done to prevent duplicate views.
 A person can only count as a view again 1 hour after viewing the image initially.";
