6599973b01a7dd30eb39e24cef0eb7d6e4d174c8
Shish <shish@shishnet.org>
2020-10-26 19:38:51 +0000

    also update test

--- ext/relationships/test.php
+++ ext/relationships/test.php
@@ -99,7 +99,7 @@ class RelationshipsTest extends ShimmiePHPUnitTestCase
         global $database;
         $database->execute(
             "UPDATE images SET parent_id=NULL, has_children=:false",
-            ["false"=>$database->scoresql_value_prepare(false)]
+            ["false"=>false]
         );
         // FIXME: send_event(new ImageRelationshipSetEvent($image_2->id, null));
 
