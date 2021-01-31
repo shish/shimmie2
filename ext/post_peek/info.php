9f402b6f9d7026d99de9bbcaf090afd380332870
Shish <shish@shishnet.org>
2021-01-31 17:21:32 +0000

    tests

--- ext/post_peek/info.php
+++ ext/post_peek/info.php
@@ -7,6 +7,6 @@ class PostPeekInfo extends ExtensionInfo
     public $key = self::KEY;
     public $name = "Postt Peek";
     public $url = self::SHIMMIE_URL;
-    public $authors = "Matthew Barbour";
+    public $authors = ["Matthew Barbour"];
     public $license = self::LICENSE_WTFPL;
 }
