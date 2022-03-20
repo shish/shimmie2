fa8d20d3b5f9a51369bddb3fdf27ac2fc6ca0ecb
Jessica Stokes <hello@jessicastokes.net>
2022-03-20 16:46:54 -0700

    Use a tag instead of uploader name

--- ext/filename_metadata/main.php
+++ ext/filename_metadata/main.php
@@ -14,30 +14,13 @@ class FilenameMetadata extends Extension
     public function onImageAddition(ImageAdditionEvent $event)
     {
         if (preg_match(self::FILENAME_REGEX, $event->image->filename, $matches)) {
-            $name = $matches["username"];
-            $user = User::by_name($name);
-
-            if (is_null($user)) {
-                throw new UserDoesNotExist("Can't find any user named $name");
+            if (!is_array($event->image->tag_array)) {
+                $event->image->tag_array = [];
             }
+            $event->image->tag_array[] = $matches["username"]."_(uploader)";
 
-            $event->image->owner_id = $user->id;
             $event->image->posted = $matches["date"];
             $event->image->filename = $matches["filename"];
-
-            print_r($event->image);
-
-            throw new UploadException("Testing filename regex metadata, sorry!");
         }
     }
-
-    // public function onSetupBuilding(SetupBuildingEvent $event)
-    // {
-    //     $sb = $event->panel->create_new_block("EOKM Filter");
-
-    //     $sb->start_table();
-    //     $sb->add_text_option("eokm_username", "Username", true);
-    //     $sb->add_text_option("eokm_password", "Password", true);
-    //     $sb->end_table();
-    // }
 }
