9ceb4f892ae7b0f89d5263611d0d5deece040789
Jessica Stokes <hello@jessicastokes.net>
2022-03-20 18:22:59 -0700

    Revert "Move the invite row"
    This reverts commit 4ee279988e5a27ef773d9d2d111b02b18285f485.

--- ext/user/theme.php
+++ ext/user/theme.php
@@ -90,11 +90,11 @@ class UserPageTheme extends Themelet
                         TD(INPUT(["type"=>'email', "name"=>'email']))
                     ),
                     TR(
-                        TD(["colspan"=>"2"], rawHTML(captcha_get_html()))
-                    ),
-                    TR(
                         TH(rawHTML("Invite code")),
                         TD(INPUT(["type"=>'text', "name"=>'invite', "required"=>true]))
+                    ),
+                    TR(
+                        TD(["colspan"=>"2"], rawHTML(captcha_get_html()))
                     ),
                 ),
                 TFOOT(
