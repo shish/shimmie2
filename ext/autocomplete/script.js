ed018fb2c46c8a62b6b3b5fd535930b0d6ded664
Jessica Stokes <hello@jessicastokes.net>
2022-03-20 22:07:20 -0700

    Autocomplete `order:posted`

--- ext/autocomplete/script.js
+++ ext/autocomplete/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-	var metatags = ['order:id', 'order:width', 'order:height', 'order:filesize', 'order:filename'];
+	var metatags = ['order:id', 'order:width', 'order:height', 'order:filesize', 'order:filename', 'order:posted'];
 
 	$('[name="search"]').tagit({
 		singleFieldDelimiter: ' ',
