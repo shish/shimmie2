c1344958d49becd8353f97840fcb5b372c8aa8a1
Shish <shish@shishnet.org>
2022-05-20 13:10:35 +0100

    [themes/warm] fix type annotations, fixes #865

--- themes/warm/upload.theme.php
+++ themes/warm/upload.theme.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 class CustomUploadTheme extends UploadTheme
 {
-    public function display_block(Page $page)
+    public function display_block(Page $page): void
     {
         $page->add_block(new Block("Upload", $this->build_upload_block(), "head", 20));
     }
 
-    public function display_full(Page $page)
+    public function display_full(Page $page): void
     {
         $page->add_block(new Block("Upload", "Disk nearly full, uploads disabled", "head", 20));
     }
