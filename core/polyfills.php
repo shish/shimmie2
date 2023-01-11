8434785da3e404890bf96147623c6cd4c053cc3b
Shish <shish@shishnet.org>
2023-01-11 00:48:18 +0000

    ??

--- core/polyfills.php
+++ core/polyfills.php
@@ -336,12 +336,12 @@ function get_base_href(): string
 function unparse_url(array $parsed_url): string
 {
     $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-    $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+    $host     = $parsed_url['host'] ?? '';
     $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-    $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+    $user     = $parsed_url['user'] ?? '';
     $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass'] : '';
     $pass     = ($user || $pass) ? "$pass@" : '';
-    $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+    $path     = $parsed_url['path'] ?? '';
     $query    = !empty($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
     $fragment = !empty($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
     return "$scheme$user$pass$host$port$path$query$fragment";
