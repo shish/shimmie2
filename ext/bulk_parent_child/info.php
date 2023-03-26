a352a02b2e4b686a786b46bc0c5e109b1ebc55ea
myname <mymail@example.com>
2023-03-26 17:01:46 -0500

    Bulk Actions for setting a chain of parent child relationships in the order of images selected. Does not support setting multiple children to one parent in bulk.

---
+++ ext/bulk_parent_child/info.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+class BulkParentChildInfo extends ExtensionInfo
+{
+    public const KEY = "bulk_parent_child";
+
+    public string $key = self::KEY;
+    public string $name = "Bulk Parent Child";
+    public array $authors = ["Flatty"=>""];
+    public string $license = self::LICENSE_WTFPL;
+    public string $description = "Allows bulk setting of parent-child relationships, in order of manual selection";
+    public array $dependencies = [BulkActionsInfo::KEY];
+}
