f1bb2dc172e85c964d4c33bc19a42ca30f2d6c10
Shish <shish@shishnet.org>
2023-06-27 11:25:57 +0100

    Add Material theme
    This seems to be broken, so I'm moving it to a separate pull-request
    instead of having it included in the default release

---
+++ themes/material/themelet.class.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class Themelet extends BaseThemelet
+{
+}
---
+++ themes/material/upload.theme.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class CustomUploadTheme extends UploadTheme
+{
+    // public function display_block(Page $page) {
+    // 	$page->add_block(new Block("Upload", $this->build_upload_block(), "left", 20));
+    // }
+    //
+    // public function display_full(Page $page) {
+    // 	$page->add_block(new Block("Upload", "Disk nearly full, uploads disabled", "left", 20));
+    // }
+}
