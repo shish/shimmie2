133f80530244548fe3a4d7baff3f1378d75022ce
Shish <shish@shishnet.org>
2023-06-27 11:27:11 +0100

    more efficient docker build

--- tests/docker-init.sh
+++ tests/docker-init.sh
@@ -4,7 +4,7 @@ useradd -ms /bin/bash -u $UID -g $GID shimmie
 mkdir -p /app/data
 chown $UID:$GID /app/data
 export PHP_CLI_SERVER_WORKERS=8
-exec /usr/local/bin/su-exec shimmie:shimmie \
+exec gosu shimmie:shimmie \
   /usr/bin/php \
     -d upload_max_filesize=$UPLOAD_MAX_FILESIZE \
     -d post_max_size=$UPLOAD_MAX_FILESIZE \
