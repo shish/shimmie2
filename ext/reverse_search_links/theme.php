0cbea9bc2b8c78b40484a2090704bc830241077e
Joe <joe@thisisjoes.site>
2023-08-19 15:12:11 -0500

    add reverse image search links extension

---
+++ ext/reverse_search_links/theme.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class ReverseSearchLinksTheme extends Themelet
+{
+    public function reverse_search_block(Page $page, Image $image)
+    {
+        global $config;
+
+        $links = [
+            'SauceNAO' => 'https://saucenao.com/search.php?url=' . url_escape(make_http($image->get_thumb_link())),
+            'TinEye' => 'https://www.tineye.com/search/?url=' . url_escape(make_http($image->get_thumb_link())),
+            'trace.moe' => 'https://trace.moe/?auto&url=' . url_escape(make_http($image->get_thumb_link())),
+            'ascii2d' => 'https://ascii2d.net/search/url/' . url_escape(make_http($image->get_thumb_link())),
+            'Yandex' => 'https://yandex.com/images/search?rpt=imageview&url=' . url_escape(make_http($image->get_thumb_link()))
+        ];
+
+        // only generate links for enabled reverse search services
+        $enabled_services = $config->get_array(ReverseSearchLinksConfig::ENABLED_SERVICES);
+
+        $html = "";
+        foreach($links as $name => $link) {
+            if (in_array($name, $enabled_services)) {
+                $icon_link = make_link("/ext/reverse_search_links/icons/" . strtolower($name) . ".ico");
+                $html .= "<a href='$link' rel='nofollow'><img title='Search with $name' src='$icon_link' alt='$name icon'></a>";
+            }
+        }
+
+        $position = $config->get_string(ReverseSearchLinksConfig::POSITION) ?? "main";
+        $priority = $config->get_int(ReverseSearchLinksConfig::PRIORITY) ?? 20;
+
+        $page->add_block(new Block("Reverse Image Search", $html, $position, $priority));
+    }
+}
