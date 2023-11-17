8d869bef9fadb03f2004d62861d7384f0643d683
myname <mymail@example.com>
2023-11-16 22:10:14 -0600

    Refactoring auto-complete functionality for alias editor and auto tagger

---
+++ core/imageboard/autocomplete_column.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use MicroCRUD\TextColumn;
+
+use function MicroHTML\INPUT;
+
+class AutoCompleteColumn extends TextColumn
+{
+    public function read_input(array $inputs)
+    {
+        return INPUT([
+            "type" => "text",
+            "name" => "r_{$this->name}",
+            "class" => "autocomplete_tags",
+            "placeholder" => $this->title,
+            "value" => @$inputs["r_{$this->name}"]
+        ]);
+    }
+
+    public function create_input(array $inputs)
+    {
+        return INPUT([
+            "type" => "text",
+            "name" => "c_{$this->name}",
+            "class" => "autocomplete_tags",
+            "placeholder" => $this->title,
+            "value" => @$inputs["c_{$this->name}"]
+        ]);
+    }
+}
