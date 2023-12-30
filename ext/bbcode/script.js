de96ce812994cbd384bd49e25833d42c56e57633
Shish <shish@shishnet.org>
2023-12-30 13:59:27 +0000

    [bbcode] remove jquery

--- ext/bbcode/script.js
+++ ext/bbcode/script.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-	$(".shm-clink").each(function(idx, elm) {
-		var target_id = $(elm).data("clink-sel");
-		if(target_id && $(target_id).length > 0) {
+	document.querySelectorAll(".shm-clink").forEach(function(el) {
+		var target_id = el.getAttribute("data-clink-sel");
+		if(target_id && document.querySelectorAll(target_id).length > 0) {
 			// if the target comment is already on this page, don't bother
 			// switching pages
-			$(elm).attr("href", target_id);
+			el.setAttribute("href", target_id);
+
 			// highlight it when clicked
-			$(elm).click(function(e) {
+			el.addEventListener("click", function(e) {
 				// This needs jQuery UI
 				$(target_id).highlight();
 			});
+
 			// vanilla target name should already be in the URL tag, but this
 			// will include the anon ID as displayed on screen
-			$(elm).html("@"+$(target_id+" .username").html());
+			el.innerHTML = "@"+document.querySelector(target_id+" .username").innerHTML;
 		}
 	});
 });
