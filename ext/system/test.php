2f2f8e2eda4410abd7b524a84ebd474b1305ee05
Shish <shish@shishnet.org>
2024-01-02 23:57:38 +0000

    [system] add a basic test, see #986

---
+++ ext/system/test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class SystemTest extends ShimmiePHPUnitTestCase
+{
+    public function testView()
+    {
+        global $page;
+        $this->get_page("system");
+        $this->assertEquals(PageMode::REDIRECT, $page->mode);
+    }
+}
