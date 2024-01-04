fd403c2b842b9475eb9831f1d0ccd191af8866bb
Shish <shish@shishnet.org>
2024-01-04 16:45:29 +0000

    [handle_archive] add tests

---
+++ ext/handle_archive/test.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class ArchiveFileHandlerTest extends ShimmiePHPUnitTestCase
+{
+    public function testArchiveHander()
+    {
+        $this->log_in_as_user();
+        system("zip -q tests/test.zip tests/pbx_screenshot.jpg tests/favicon.png");
+        $this->post_image("tests/test.zip", "a z");
+
+        $images = Search::find_images();
+        $this->assertEquals(2, count($images));
+        $this->assertEquals("a tests z", $images[0]->get_tag_list());
+        $this->assertEquals("a tests z", $images[1]->get_tag_list());
+    }
+
+    public function tearDown(): void
+    {
+        if(file_exists("tests/test.zip")) {
+            unlink("tests/test.zip");
+        }
+    }
+}
