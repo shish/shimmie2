b5ad2a6bababee619edfef388a2ef81b1b936079
Shish <shish@shishnet.org>
2024-01-06 21:27:34 +0000

    [blotter] fix doc

--- ext/blotter/info.php
+++ ext/blotter/info.php
@@ -13,8 +13,6 @@ class BlotterInfo extends ExtensionInfo
     public string $url = "http://seemslegit.com/";
     public array $authors = ["Zach Hall" => "zach@sosguy.net"];
     public string $license = self::LICENSE_GPLV2;
-    public string $description = "Displays brief updates about whatever you want on every page.
-Colors and positioning can be configured to match your site's design.
-
-Development TODO at https://github.com/zshall/shimmie2/issues";
+    public string $description = "Displays brief updates about whatever you want on every page.";
+    public ?string $documentation = "Colors and positioning can be configured to match your site's design.<p>Development TODO at https://github.com/zshall/shimmie2/issues";
 }
