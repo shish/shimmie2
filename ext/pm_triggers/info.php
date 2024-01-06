9b4ddc2e5c38265d1cde9d4076b7692ad87ec087
Shish <shish@shishnet.org>
2024-01-06 21:22:35 +0000

    [pm_triggers] WIP extension to send PMs on certain events

---
+++ ext/pm_triggers/info.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class PMTriggerInfo extends ExtensionInfo
+{
+    public const KEY = "pm_triggers";
+
+    public string $key = self::KEY;
+    public string $name = "PM triggers";
+    public string $url = self::SHIMMIE_URL;
+    public array $authors = self::SHISH_AUTHOR;
+    public string $license = self::LICENSE_GPLV2;
+    public string $description = "Send PMs in response to certain events (eg post deletion)";
+    public bool $beta = true;
+}
