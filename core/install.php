e960f73fb670d823ddc3784b17407f775ece312a
Shish <shish@shishnet.org>
2024-01-15 21:49:16 +0000

    fmt

--- core/install.php
+++ core/install.php
@@ -46,7 +46,7 @@ function install(): void
     if ($dsn) {
         do_install($dsn);
     } else {
-        if(PHP_SAPI == 'cli') {
+        if (PHP_SAPI == 'cli') {
             print("INSTALL_DSN needs to be set for CLI installation\n");
         } else {
             ask_questions();
@@ -314,7 +314,7 @@ function write_config(string $dsn): void
     }
 
     if (file_put_contents("data/config/shimmie.conf.php", $file_content, LOCK_EX)) {
-        if(PHP_SAPI == 'cli') {
+        if (PHP_SAPI == 'cli') {
             print("Installation Successful\n");
             exit(0);
         } else {
