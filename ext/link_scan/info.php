6a0c5c4e2d83f060445e0de01c056cfff727d161
Shish <shish@shishnet.org>
2024-01-16 13:29:29 +0000

    more docs

--- ext/link_scan/info.php
+++ ext/link_scan/info.php
@@ -14,5 +14,15 @@ class LinkScanInfo extends ExtensionInfo
     public array $authors = self::SHISH_AUTHOR;
     public string $license = self::LICENSE_GPLV2;
     public string $description = "Find posts that are referenced in a block of text";
-    public ?string $documentation = "With this extension enabled, you can paste a block of text into the search field, and the code will scan the text for URLs referencing the site, and show them as search results";
+    public ?string $documentation = "
+        With this extension enabled, you can paste a block of text into
+        the search field, and the code will scan the text for URLs
+        referencing the site, and show them as search results
+
+        <p>By default scan-for-URLs mode will be activated if somebody
+        searches for text which includes <code>http://</code>
+        or <code>https://</code> - but you can change this by setting
+        <code>link_scan_trigger</code> in the config table, eg to
+        <code>https?://www.mysite.com/</code>
+    ";
 }
