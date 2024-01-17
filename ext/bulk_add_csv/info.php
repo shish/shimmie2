2667d635ad0a5e97d4571bb70dcee3526a0f6042
Shish <shish@shishnet.org>
2024-01-17 22:42:52 +0000

    [bulk_add_csv] better docs, see #1015

--- ext/bulk_add_csv/info.php
+++ ext/bulk_add_csv/info.php
@@ -15,14 +15,16 @@ class BulkAddCSVInfo extends ExtensionInfo
     public string $license = self::LICENSE_GPLV2;
     public string $description = "Bulk add server-side posts with metadata from CSV file";
     public ?string $documentation =
-"Modification of \"Bulk Add\" by Shish.<br><br>
-Adds posts from a CSV with the five following values: <br>
-\"/path/to/image.jpg\",\"spaced tags\",\"source\",\"rating s/q/e\",\"/path/thumbnail.jpg\" <br>
-<b>e.g.</b> \"/tmp/cat.png\",\"shish oekaki\",\"shimmie.shishnet.org\",\"s\",\"tmp/custom.jpg\" <br><br>
-Any value but the first may be omitted, but there must be five values per line.<br>
-<b>e.g.</b> \"/why/not/try/bulk_add.jpg\",\"\",\"\",\"\",\"\"<br><br>
-Post thumbnails will be displayed at the AR of the full post. Thumbnails that are
-normally static (e.g. SWF) will be displayed at the board's max thumbnail size<br><br>
-Useful for importing tagged posts without having to do database manipulation.<br>
+"Adds posts from a CSV with the five following values:
+<pre>\"/path/to/image.jpg\",\"spaced tags\",\"source\",\"rating s/q/e\",\"/path/thumbnail.jpg\"</pre>
+
+<b>e.g.</b>
+<pre>\"/tmp/cat.png\",\"shish oekaki\",\"http://shimmie.shishnet.org\",\"s\",\"tmp/custom.jpg\"</pre>
+
+Any value but the first may be omitted, but there must be five values per line.
+<b>e.g.</b> <pre>\"/why/not/try/bulk_add.jpg\",\"\",\"\",\"\",\"\"</pre>
+
+Useful for importing tagged posts without having to do database manipulation.
+
 <p><b>Note:</b> requires \"Admin Controls\" and optionally \"Post Ratings\" to be enabled<br><br>";
 }
