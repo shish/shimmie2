2a561392d03058926dbd229c41a98ab6e762387c
Shish <shish@shishnet.org>
2024-01-18 19:47:48 +0000

    [sitemap] add tests, and big rewrite to pass tests, fixes #1016

--- ext/sitemap/test.php
+++ ext/sitemap/test.php
@@ -8,6 +8,20 @@ class XMLSitemapTest extends ShimmiePHPUnitTestCase
 {
     public function testBasic(): void
     {
+        // check empty DB
+        @unlink(data_path("cache/sitemap.xml"));
+        $page = $this->get_page('sitemap.xml');
+        $this->assertEquals(200, $page->code);
+
+        $this->log_in_as_user();
+        $this->post_image("tests/pbx_screenshot.jpg", "pbx computer screenshot");
+
+        // check DB with one image
+        @unlink(data_path("cache/sitemap.xml"));
+        $page = $this->get_page('sitemap.xml');
+        $this->assertEquals(200, $page->code);
+
+        // check caching
         $page = $this->get_page('sitemap.xml');
         $this->assertEquals(200, $page->code);
     }
