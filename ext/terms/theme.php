68be2c7123c91616ad274a668821f6a204f0d7c1
discomrade <83621080+discomrade@users.noreply.github.com>
2024-06-07 07:56:03 +0000

    [terms] implement terms and conditions blur modal, fixes #1166

--- ext/terms/theme.php
+++ ext/terms/theme.php
@@ -8,30 +8,16 @@ class TermsTheme extends Themelet
 {
     public function display_page(Page $page, string $sitename, string $path, string $body): void
     {
-        $page->set_mode(PageMode::DATA);
-        $page->add_auto_html_headers();
-        $hh = $page->get_all_html_headers();
-        $page->set_data(
-            <<<EOD
-<!doctype html>
-<html lang="en">
-	<head>
-		<title>$sitename</title>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		$hh
-	</head>
-	<body>
-		<div id="front-page">
-			<h1><span>$sitename</span></h1>
-			$body
-			<form action="/accept_terms/$path" method="POST">
-				<button>Enter</button>
-			</form>
-		</div>
-	</body>
-</html>
-EOD
-        );
+        $html =
+        "<div id='terms-modal-bg'>
+		    <dialog id='terms-modal' class='setupblock' open>
+				<h1><span>$sitename</span></h1>
+				$body
+				<form action='/accept_terms/$path' method='POST'>
+					<button class='terms-modal-enter' autofocus>Enter</button>
+				</form>
+			</dialog>
+		</div>";
+        $page->add_block(new Block(null, $html, "main", 1));
     }
 }
