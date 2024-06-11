7be1e88efad10a19b7857b400a7836933724020f
discomrade <83621080+discomrade@users.noreply.github.com>
2024-06-11 13:15:22 +0000

    [blocks] add documentation

--- ext/blocks/info.php
+++ ext/blocks/info.php
@@ -14,4 +14,13 @@ class BlocksInfo extends ExtensionInfo
     public array $authors = self::SHISH_AUTHOR;
     public string $license = self::LICENSE_GPLV2;
     public string $description = "Add HTML to some space (News, Ads, etc)";
+    public ?string $documentation =
+    "Blocks with lower priority number appear higher up the page.<br><br>
+     The userclass parameter can be left empty for blocks to show to everyone, or specified as a comma-separated, case-insensitive list of user classes that will see that block. Spaces around the comma get stripped.<br><br>
+     For example, to show ads only to regular users and anonymous user classes:
+     <ul>
+     <li>\"&lt;ad here&gt;\": <code>user, anonymous</code></li>
+     <li>\"Thanks for supporting us!\": <code>supporter</code></li>
+     </ul>
+     ";
 }
