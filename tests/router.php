ddfa7d4f2b376c148b6aa27f29f4a404c04d9a46
Shish <shish@shishnet.org>
2024-10-02 14:26:30 +0100

    also serve png/ico/svg files raw, if they exist

--- tests/router.php
+++ tests/router.php
@@ -6,7 +6,10 @@ if (preg_match('/_(images|thumbs)\/([0-9a-f]{32})\//', $_SERVER["REQUEST_URI"], 
     $ha = substr($hash, 0, 2);
     header("Content-Type: image/jpeg");
     print(file_get_contents("data/$silo/$ha/$hash"));
-} elseif (preg_match('/.*\.(jpg|jpeg|gif|js|css)/', $_SERVER["REQUEST_URI"])) {
+} elseif (
+    preg_match('/.*\.(jpg|jpeg|gif|png|ico|svg|js|css)/', $_SERVER["REQUEST_URI"])
+    && file_exists($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])
+) {
     return false;
 } else {
     require_once("index.php");
