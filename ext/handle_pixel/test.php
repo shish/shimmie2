e84df53e78bfa2d5a09925d2646704f45d516132
Shish <shish@shishnet.org>
2024-12-10 00:30:54 +0000

    tests for pixel format support, see #1302

--- ext/handle_pixel/test.php
+++ ext/handle_pixel/test.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
+use PHPUnit\Framework\Attributes\DataProvider;
+
 class PixelFileHandlerTest extends ShimmiePHPUnitTestCase
 {
     public function testPixelHander(): void
@@ -16,5 +18,38 @@ class PixelFileHandlerTest extends ShimmiePHPUnitTestCase
 
         # FIXME: test that the thumb works
         # FIXME: test that it gets displayed properly
+    }
+
+    /**
+     * @return array<array<string>>
+     */
+    public static function formatList(): array
+    {
+        return [
+            ["jpeg"],
+            ["png"],
+            ["gif"],
+        ];
+    }
+
+    #[DataProvider('formatList')]
+    public function testFormats(string $format): void
+    {
+        $tmp = shm_tempnam("test-format");
+        unlink($tmp);
+        $img = \Safe\imagecreatefromstring(\Safe\file_get_contents("tests/pbx_screenshot.jpg"));
+        ("image$format")($img, "$tmp.$format");
+
+        $image_id = $this->post_image("$tmp.$format", "pbx computer screenshot $format");
+        $page = $this->get_page("post/view/$image_id");
+        $this->assertEquals(200, $page->code);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        foreach (\Safe\glob("data/temp/test-format*") as $file) {
+            unlink($file);
+        }
     }
 }
