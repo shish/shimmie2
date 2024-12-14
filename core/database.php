a087c015200840cbdc6f5dbc8f1a1c4b5475cd20
Shish <shish@shishnet.org>
2024-12-14 12:52:40 +0000

    [core] cleaner count_tables

--- core/database.php
+++ core/database.php
@@ -398,22 +398,14 @@ class Database
      */
     public function count_tables(): int
     {
-        if ($this->get_engine()->id === DatabaseDriverID::MYSQL) {
-            return count(
-                $this->get_all("SHOW TABLES")
-            );
-        } elseif ($this->get_engine()->id === DatabaseDriverID::PGSQL) {
-            return count(
-                $this->get_all("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-            );
-        } elseif ($this->get_engine()->id === DatabaseDriverID::SQLITE) {
-            return count(
-                $this->get_all("SELECT name FROM sqlite_master WHERE type = 'table'")
-            );
-        } else {
-            $did = (string)$this->get_engine()->id;
-            throw new ServerError("Can't count tables for database type {$did}");
-        }
+        $sql = match ($this->get_engine()->id) {
+            DatabaseDriverID::MYSQL => "SHOW TABLES",
+            DatabaseDriverID::PGSQL => "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'",
+            // @phpstan-ignore-next-line - phpstan thinks this is a constant?
+            DatabaseDriverID::SQLITE => "SELECT name FROM sqlite_master WHERE type = 'table'",
+            default => throw new ServerError("Can't count tables for database type {$this->get_engine()->id}"),
+        };
+        return count($this->get_col($sql));
     }
 
     public function raw_db(): PDO
