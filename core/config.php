06020c2ace14d6b289b738fb51dfb2e9068283eb
Shish <shish@shishnet.org>
2024-12-16 15:47:44 +0000

    [setup] show extension in advanced config

--- core/config.php
+++ core/config.php
@@ -323,4 +323,17 @@ class DatabaseConfig extends Config
 
 abstract class ConfigGroup
 {
+    public static function get_group_for_entry_by_name(string $name): ?ConfigGroup
+    {
+        foreach (get_subclasses_of(ConfigGroup::class) as $class) {
+            $config = new $class();
+            assert(is_a($config, ConfigGroup::class));
+            foreach ((new \ReflectionClass($class))->getConstants() as $const => $value) {
+                if ($value === $name) {
+                    return $config;
+                }
+            }
+        }
+        return null;
+    }
 }
