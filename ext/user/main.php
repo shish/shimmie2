c7ff3baa4f09db71f23f4a4569b7977160ec617f
Shish <shish@shishnet.org>
2025-02-03 15:55:24 +0000

    [user] custom signup-disabled message

--- ext/user/main.php
+++ ext/user/main.php
@@ -136,6 +136,10 @@ class UserPage extends Extension
     {
         global $config;
         $config->set_default_bool(UserAccountsConfig::SIGNUP_ENABLED, true);
+        $config->set_default_string(
+            UserAccountsConfig::SIGNUP_DISABLED_MESSAGE,
+            "The board admin has disabled the ability to sign up for new accounts"
+        );
         $config->set_default_int(UserAccountsConfig::LOGIN_MEMORY, 365);
         $config->set_default_bool(UserAccountsConfig::LOGIN_TAC_BBCODE, true);
         $config->set_default_bool(UserAccountsConfig::USER_EMAIL_REQUIRED, false);
