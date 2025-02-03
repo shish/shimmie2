aa35ad6fadd444343ff8d95c0633338377a9e41b
Shish <shish@shishnet.org>
2025-02-03 15:55:24 +0000

    [user] custom signup-disabled message

--- ext/user/config.php
+++ ext/user/config.php
@@ -8,6 +8,7 @@ class UserAccountsConfig extends ConfigGroup
 {
     public const ANON_ID = "anon_id";
     public const SIGNUP_ENABLED = "login_signup_enabled";
+    public const SIGNUP_DISABLED_MESSAGE = "login_signup_disabled_message";
     public const LOGIN_MEMORY = "login_memory";
     public const LOGIN_TAC = "login_tac";
     public const LOGIN_TAC_BBCODE = "login_tac_bbcode";
