b16ebf650cd508f8a1c16102934e847df67146f8
Shish <shish@shishnet.org>
2025-02-12 20:55:20 +0000

    [setup] remove unused config

--- ext/setup/config.php
+++ ext/setup/config.php
@@ -10,7 +10,7 @@ class SetupConfig extends ConfigGroup
     public const FRONT_PAGE = "front_page";
     public const MAIN_PAGE = "main_page";
     public const THEME = "theme";
-    public const WORD_WRAP = "word_wrap";
     public const COMMENT_CAPTCHA = "comment_captcha";
     public const NICE_URLS = "nice_urls";
+    public const CONTACT_LINK = "contact_link";
 }
