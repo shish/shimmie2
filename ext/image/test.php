da5165982bb08d9a7e69e9e787fe13dd66ca061e
Shish <shish@shishnet.org>
2025-02-21 14:40:04 +0000

    [image] option for users to delete their own posts

--- ext/image/test.php
+++ ext/image/test.php
@@ -12,26 +12,63 @@ class ImageIOTest extends ShimmiePHPUnitTestCase
         $image_id = $this->post_image("tests/pbx_screenshot.jpg", "test");
 
         // broken with sqlite?
-        //$this->get_page("user/test");
-        //$this->assert_text("Images uploaded: 1");
+        $this->get_page("user/test");
+        $this->assert_text("Posts uploaded</a>: 1");
+    }
 
-        //$this->click("Images uploaded");
-        //$this->assert_title("Image $image_id: test");
-
-        # test that serving manually doesn't cause errors
+    public function testServeImage(): void
+    {
+        $this->log_in_as_user();
+        $image_id = $this->post_image("tests/pbx_screenshot.jpg", "test");
         $page = $this->get_page("image/$image_id/moo.jpg");
         $this->assertEquals(200, $page->code);
+    }
 
+    public function testServeThumb(): void
+    {
+        $this->log_in_as_user();
+        $image_id = $this->post_image("tests/pbx_screenshot.jpg", "test");
         $page = $this->get_page("thumb/$image_id/moo.jpg");
         $this->assertEquals(200, $page->code);
     }
 
-    public function testDeleteRequest(): void
+    public function testDelete(): void
     {
-        global $page;
         $this->log_in_as_admin();
         $image_id = $this->post_image("tests/pbx_screenshot.jpg", "test");
-        send_event(new PageRequestEvent("POST", "image/delete", [], ['image_id' => "$image_id"]));
+
+        $this->log_in_as_user();
+        $this->assertException(PermissionDenied::class, function () use ($image_id) {
+            $this->post_page("image/delete", ['image_id' => "$image_id"]);
+        });
+
+        $this->log_in_as_admin();
+        # delete twice because Trash extension
+        $page = $this->post_page("image/delete", ['image_id' => "$image_id"]);
         $this->assertEquals(PageMode::REDIRECT, $page->mode);
+        $page = $this->post_page("image/delete", ['image_id' => "$image_id"]);
+        $this->assertEquals(PageMode::REDIRECT, $page->mode);
+
+        $this->assertException(PostNotFound::class, function () use ($image_id) {
+            $this->get_page("image/$image_id/moo.jpg");
+        });
+    }
+
+    public function testDeleteOwn(): void
+    {
+        UserClass::$known_classes["user"]->setPermission(ImagePermission::DELETE_OWN_IMAGE, true);
+        $this->log_in_as_user();
+        $image_id = $this->post_image("tests/pbx_screenshot.jpg", "test");
+
+        $this->log_in_as_user();
+        # delete twice because Trash extension
+        $page = $this->post_page("image/delete", ['image_id' => "$image_id"]);
+        $this->assertEquals(PageMode::REDIRECT, $page->mode);
+        $page = $this->post_page("image/delete", ['image_id' => "$image_id"]);
+        $this->assertEquals(PageMode::REDIRECT, $page->mode);
+
+        $this->assertException(PostNotFound::class, function () use ($image_id) {
+            $this->get_page("image/$image_id/moo.jpg");
+        });
     }
 }
