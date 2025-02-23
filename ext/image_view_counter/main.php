e807b05a7bc5bb064ec13ee0fb1048a6f811cd20
Shish <shish@shishnet.org>
2025-02-23 14:20:32 +0000

    [image_view_counter] backport version-as-int

--- ext/image_view_counter/main.php
+++ ext/image_view_counter/main.php
@@ -78,14 +78,18 @@ class ImageViewCounter extends Extension
     {
         global $database, $config;
 
-        if ($config->get_bool("image_viewcounter_installed") == false) { //todo
+        if ($config->get_bool("image_viewcounter_installed")) {
+            $this->set_version("ext_image_view_counter", 1);
+            $config->delete("image_viewcounter_installed");
+        }
+        if ($this->get_version("ext_image_view_counter") < 1) {
             $database->create_table("image_views", "
                 id SCORE_AIPK,
                 image_id INTEGER NOT NULL,
                 user_id INTEGER NOT NULL,
                 timestamp INTEGER NOT NULL,
                 ipaddress SCORE_INET NOT NULL");
-            $config->set_bool("image_viewcounter_installed", true);
+            $this->set_version("ext_image_view_counter", 1);
         }
     }
 
