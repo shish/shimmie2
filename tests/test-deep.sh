3a213390c8f2a8fec901bb5e1b11eb90c8819c2b
Shish <shish@shishnet.org>
2025-03-03 22:47:32 +0000

    remove deprecated test script

--- tests/test-deep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-php \
-	-d xdebug.profiler_output_dir=./data/prof/ \
-	-d xdebug.profiler_enable=1 \
-	./vendor/bin/phpunit \
-		--config tests/phpunit.xml \
-		--coverage-clover data/coverage.clover
-#	-d extension.dir=/usr/local/Cellar/php71-xdebug/2.5.5/ \
-#	-d extension=xdebug.so \
