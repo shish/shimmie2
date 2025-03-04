064385db8948382923da2d336ccf3a20b0453825
Shish <shish@shishnet.org>
2025-03-04 00:04:28 +0000

    [robots_txt] split robots functionality out from static_files

---
+++ ext/robots_txt/info.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class RobotsTxtInfo extends ExtensionInfo
+{
+    public const KEY = "robots_txt";
+
+    public string $key = self::KEY;
+    public string $name = "robots.txt generator";
+    public string $url = self::SHIMMIE_URL;
+    public array $authors = self::SHISH_AUTHOR;
+    public string $license = self::LICENSE_GPLV2;
+    public ExtensionVisibility $visibility = ExtensionVisibility::HIDDEN;
+    public string $description = 'Generate robots.txt file';
+    public bool $core = true;
+}
--- ext/static_files/main.php
+++ ext/static_files/main.php
@@ -4,22 +4,6 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class RobotsBuildingEvent extends Event
-{
-    /** @var string[] */
-    public array $parts = [
-        "User-agent: *",
-        // Site is rate limited to 1 request / sec,
-        // returns 503 for more than that
-        "Crawl-delay: 3",
-    ];
-
-    public function add_disallow(string $path): void
-    {
-        $this->parts[] = "Disallow: /$path";
-    }
-}
-
 class StaticFiles extends Extension
 {
     public const KEY = "static_files";
@@ -27,13 +11,6 @@ class StaticFiles extends Extension
     public function onPageRequest(PageRequestEvent $event): void
     {
         global $config, $page;
-
-        if ($event->page_matches("robots.txt")) {
-            $rbe = send_event(new RobotsBuildingEvent());
-            $page->set_mode(PageMode::DATA);
-            $page->set_mime("text/plain");
-            $page->set_data(join("\n", $rbe->parts));
-        }
 
         // hax.
         if ($page->mode == PageMode::PAGE && $this->count_main($page->blocks) == 0) {
