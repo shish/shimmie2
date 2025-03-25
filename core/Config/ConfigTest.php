13b78460ab75694b19def6b3c6c241060a06271e
Shish <shish@shishnet.org>
2025-03-25 16:41:11 +0000

    [core] Remove default param from config->get_XXX()
    being able to set a default in `get()` made it possible for multiple callsites to request the same config and get different values

--- core/Config/ConfigTest.php
+++ core/Config/ConfigTest.php
@@ -14,21 +14,14 @@ final class ConfigTest extends ShimmiePHPUnitTestCase
             "get_int should return the value of a setting when it is set correctly"
         );
 
-        self::assertEquals(
-            (new TestConfig(["foo" => "waffo"]))->get_int("foo", 42),
-            42,
-            "get_int should return the default value when a setting is set incorrectly"
-        );
-
-        self::assertEquals(
-            (new TestConfig([]))->get_int("foo", 42),
-            42,
-            "get_int should return the default value when a setting is not set"
+        self::assertNull(
+            (new TestConfig(["foo" => "waffo"]))->get_int("foo"),
+            "get_int should return null when a setting is set incorrectly"
         );
 
         self::assertNull(
             (new TestConfig([]))->get_int("foo"),
-            "get_int should return null when a setting is not set and no default is specified"
+            "get_int should return null when a setting is not set"
         );
 
         self::assertException(ConfigException::class, function () {
