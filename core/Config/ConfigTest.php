1873724684731a1d6a51f65476e2d7eed55084df
Shish <shish@shishnet.org>
2025-03-27 18:36:09 +0000

    [core] keep config defaults and values separate

--- core/Config/ConfigTest.php
+++ core/Config/ConfigTest.php
@@ -9,23 +9,35 @@ final class ConfigTest extends ShimmiePHPUnitTestCase
     public function testGetInt(): void
     {
         self::assertEquals(
-            (new TestConfig(["foo" => "42"]))->get_int("foo"),
+            (new TestConfig([], ["foo" => "42"]))->get_int("foo"),
             42,
             "get_int should return the value of a setting when it is set correctly"
         );
 
-        self::assertNull(
-            (new TestConfig(["foo" => "waffo"]))->get_int("foo"),
-            "get_int should return null when a setting is set incorrectly"
+        self::assertEquals(
+            (new TestConfig(["foo" => "42"], []))->get_int("foo"),
+            42,
+            "get_int should return default when a value is not set"
+        );
+
+        self::assertEquals(
+            (new TestConfig(["foo" => "42"], ["foo" => "123"]))->get_int("foo"),
+            123,
+            "get_int should return value when value and default are set"
         );
 
         self::assertNull(
-            (new TestConfig([]))->get_int("foo"),
+            (new TestConfig(["foo" => "42"], ["foo" => "waffo"]))->get_int("foo"),
+            "get_int should return default when a setting is set incorrectly"
+        );
+
+        self::assertNull(
+            (new TestConfig([], []))->get_int("foo"),
             "get_int should return null when a setting is not set"
         );
 
         self::assertException(ConfigException::class, function () {
-            (new TestConfig([]))->req_int("foo");
+            (new TestConfig([], []))->req_int("foo");
         }, "req_int should throw an exception when a setting is not set");
     }
 }
