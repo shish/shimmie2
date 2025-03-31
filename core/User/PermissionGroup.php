40caaecc797119da55ad183c6ed9454959491c51
Shish <shish@shishnet.org>
2025-03-31 20:22:18 +0100

    [core] fix derp, and add unit test to prevent derp...

--- core/User/PermissionGroup.php
+++ core/User/PermissionGroup.php
@@ -13,10 +13,10 @@ abstract class PermissionGroup extends Enablable
     {
         return $this->title ?? implode(
             " ",
-            \Safe\preg_split(
+            array_filter(\Safe\preg_split(
                 '/(?=[A-Z])/',
-                \Safe\preg_replace("/^Shimmie2.(.*?)(User)?Config$/", "\$1", get_class($this))
-            )
+                \Safe\preg_replace("/^Shimmie2.(.*?)(User)?Permission/", "\$1", get_class($this))
+            ))
         );
     }
 }
