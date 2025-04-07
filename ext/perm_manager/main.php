e23c3a714fa8ace0a4cb5d94939864e5bbdcef3b
Shish <shish@shishnet.org>
2025-04-07 21:31:53 +0100

    [perm-manager] add & remove classes

--- ext/perm_manager/main.php
+++ ext/perm_manager/main.php
@@ -65,11 +65,120 @@ final class PermManager extends Extension
 
     public function onPageRequest(PageRequestEvent $event): void
     {
+        // Overview
         if ($event->page_matches("perm_manager", method: "GET")) {
+            Ctx::$page->set_title("Permission Manager");
+            $this->theme->display_navigation();
             $this->theme->display_user_classes(
                 UserClass::$known_classes,
                 PermissionGroup::get_all_metas_grouped(),
             );
+            if (Ctx::$user->can(PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+                $counts = Ctx::$database->get_pairs("SELECT class, COUNT(*) FROM users GROUP BY class");
+                $this->theme->display_create_delete(UserClass::$known_classes, $counts);
+            }
+        }
+
+        // Create from scratch
+        if ($event->page_matches("user_class", method: "POST", permission: PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+            $class_name = $event->POST->req("name");
+            $parent = $event->POST->req("parent");
+            $description = $event->POST->req("description");
+            Ctx::$database->execute(
+                "INSERT INTO user_classes (name, parent, description) VALUES (:name, :parent, :description)",
+                ["name" => $class_name, "parent" => $parent, "description" => $description]
+            );
+            Ctx::$page->flash("User Class $class_name created");
+            Ctx::$page->set_redirect(make_link("user_class/$class_name"));
+        }
+
+        // Create from existing
+        // (copy settings from defaults into the DB)
+        if ($event->page_matches("user_class/{class}/migrate", method: "POST", permission: PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+            $class_name = $event->get_arg("class");
+            $class = UserClass::$known_classes[$class_name];
+            $parent = $class->get_parent();
+            assert($parent !== null);
+            Ctx::$database->execute(
+                "INSERT INTO user_classes (name, parent, description) VALUES (:name, :parent, :description)",
+                ["name" => $class->name, "parent" => $parent->name, "description" => $class->description]
+            );
+            $class_id = Ctx::$database->get_one(
+                "SELECT id FROM user_classes WHERE name = :class_name",
+                ["class_name" => $class_name]
+            );
+            assert($class_id !== null);
+            foreach (PermissionGroup::get_all_metas_grouped() as $ext => $group) {
+                foreach ($group as $perm => $meta) {
+                    if ($class->has_own_permission($perm)) {
+                        Ctx::$database->execute(
+                            "INSERT INTO user_class_permissions (user_class_id, permission, value) VALUES (:id, :permission, :value)",
+                            ["id" => $class_id, "permission" => $perm, "value" => $class->can($perm)]
+                        );
+                    }
+                }
+            }
+            Ctx::$page->flash("Migrated '$class_name' to database");
+            Ctx::$page->set_redirect(make_link("user_class/$class_name"));
+        }
+
+        // Read
+        if ($event->page_matches("user_class/{class}", method: "GET", permission: PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+            $class_name = $event->get_arg("class");
+            $class = UserClass::$known_classes[$class_name];
+            Ctx::$page->set_title("Permission Manager");
+            $this->theme->display_navigation();
+            $this->theme->display_edit_class($class);
+            $this->theme->display_edit_permissions($class, PermissionGroup::get_all_metas_grouped());
+        }
+
+        // Update (General)
+        if ($event->page_matches("user_class/{class}", method: "POST", permission: PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+            $class_name = $event->get_arg("class");
+            $parent = $event->POST->req("parent");
+            $description = $event->POST->req("description");
+            Ctx::$database->execute(
+                "UPDATE user_classes SET parent = :parent, description = :description WHERE name = :name",
+                ["name" => $class_name, "parent" => $parent, "description" => $description]
+            );
+            Ctx::$page->flash("Updated settings for '$class_name'");
+            Ctx::$page->set_redirect(make_link("user_class/$class_name"));
+        }
+
+        // Update (Permissions)
+        if ($event->page_matches("user_class/{class}/permissions", method: "POST", permission: PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+            $class_name = $event->get_arg("class");
+            $class_id = Ctx::$database->get_one("SELECT id FROM user_classes WHERE name = :class_name", ["class_name" => $class_name]);
+            assert($class_id !== null);
+            $perms = $event->POST->getAll("permissions");
+            Ctx::$database->execute(
+                "DELETE FROM user_class_permissions WHERE user_class_id = :id",
+                ["id" => $class_id]
+            );
+            foreach ($perms as $perm => $value) {
+                $sql_value = match($value) {
+                    "null" => null,
+                    "true" => true,
+                    "false" => false,
+                    default => throw new UserError("Invalid value for permission '$perm': $value"),
+                };
+                if ($sql_value !== null) {
+                    Ctx::$database->execute(
+                        "INSERT INTO user_class_permissions (user_class_id, permission, value) VALUES (:id, :permission, :value)",
+                        ["id" => $class_id, "permission" => $perm, "value" => $sql_value]
+                    );
+                }
+            }
+            Ctx::$page->flash("Updated permissions for '$class_name'");
+            Ctx::$page->set_redirect(make_link("user_class/$class_name"));
+        }
+
+        // Delete
+        if ($event->page_matches("user_class/{class}/delete", method: "POST", permission: PermManagerPermission::MANAGE_USER_PERMISSIONS)) {
+            $class_name = $event->get_arg("class");
+            Ctx::$database->execute("DELETE FROM user_classes WHERE name = :name", ["name" => $class_name]);
+            Ctx::$page->flash("Deleted '$class_name'");
+            Ctx::$page->set_redirect(Url::referer_or());
         }
     }
 
--- ext/perm_manager/theme.php
+++ ext/perm_manager/theme.php
@@ -4,18 +4,178 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\{SPAN, TABLE, TD, TH, TR};
+use function MicroHTML\{BR, BUTTON, FORM, INPUT, SPAN, TABLE, TBODY, TD, TEXTAREA, TFOOT, TH, THEAD, TR, emptyHTML};
 
 class PermManagerTheme extends Themelet
 {
     /**
      * @param UserClass[] $classes
-     * @param array<string,array<string,PermissionMeta>> $permissions
+     * @param array<string, int> $counts
      */
-    public function display_user_classes(array $classes, array $permissions): void
+    public function display_create_delete(array $classes, array $counts): void
     {
-        $table = TABLE(["class" => "zebra", "id" => "permission_table"]);
+        $tbody = TBODY();
+        foreach ($classes as $class) {
+            if ($class->get_parent() === null) {
+                continue;
+            }
+            $actions = emptyHTML();
 
+            // every visible class can be edited, (base and admin
+            // can't be edited, but they aren't visible)
+            if (in_array(UserClassSource::DATABASE, $class->sources)) {
+                $actions->appendChild(FORM(
+                    ["action" => make_link("user_class/{$class->name}"), "method" => "GET"],
+                    BUTTON("Edit"),
+                ));
+            } else {
+                $actions->appendChild(SHM_SIMPLE_FORM(
+                    make_link("user_class/{$class->name}/migrate"),
+                    BUTTON("Edit"),
+                ));
+
+            }
+
+            // - If a database class overrides a default or file class, it can
+            //   be reverted (ie delete the database and fall back to file)
+            // - If a database class is unused, it can be deleted
+            // - Default and file-based classes can't be deleted
+            $form = SHM_FORM(action: make_link("user_class/{$class->name}/delete"));
+            if (in_array(UserClassSource::DATABASE, $class->sources) && in_array(UserClassSource::FILE, $class->sources)) {
+                $form->appendChild(SHM_SUBMIT("Revert to File"));
+            } elseif (in_array(UserClassSource::DATABASE, $class->sources) && in_array(UserClassSource::DEFAULT, $class->sources)) {
+                $form->appendChild(SHM_SUBMIT("Revert to Default"));
+            } elseif (in_array(UserClassSource::DATABASE, $class->sources)) {
+                if ($counts[$class->name] == 0) {
+                    $form->appendChild(SHM_SUBMIT("Delete"));
+                } else {
+                    $form->appendChild(BUTTON(["disabled" => true], "Can't delete in-use classes"));
+                }
+            } elseif (in_array(UserClassSource::FILE, $class->sources)) {
+                $form->appendChild(BUTTON(["disabled" => true], "Can't delete classes from user-classes.conf.php"));
+            } elseif (in_array(UserClassSource::DEFAULT, $class->sources)) {
+                $form->appendChild(BUTTON(["disabled" => true], "Can't delete default classes"));
+            } else {
+                $form->appendChild("Confused by" . implode(", ", array_map(fn ($source) => $source->name, $class->sources)));
+            }
+            $actions->appendChild($form);
+
+            $tbody->appendChild(TR(
+                TH($class->name, BR(), "(" . $class->get_parent()->name . ")"),
+                TD($counts[$class->name] ?? 0),
+                TD($class->description),
+                TD($actions)
+            ));
+        }
+
+        $html = TABLE(
+            ["class" => "zebra"],
+            THEAD(TR(
+                TH("Class", BR(), "(Parent)"),
+                TH("Count"),
+                TH("Description"),
+                TH("Actions"),
+            )),
+            $tbody,
+            TFOOT(SHM_SIMPLE_FORM(
+                make_link("user_class"),
+                TR(
+                    TD(
+                        ["colspan" => "2"],
+                        INPUT(["type" => "text", "name" => "name"]),
+                        BR(),
+                        SHM_SELECT("parent", $this->class_list()),
+                    ),
+                    TD(TEXTAREA(["name" => "description"])),
+                    TD(SHM_SUBMIT("Create"))
+                )
+            ))
+        );
+        Ctx::$page->add_block(new Block("User Classes", $html, "main", 10));
+    }
+
+    /**
+     * @return array<string,string>
+     */
+    protected function class_list(?string $exclude = null): array
+    {
+        $class_list = [];
+        foreach (UserClass::$known_classes as $class) {
+            if ($class->name === $exclude) {
+                continue;
+            }
+            $class_list[$class->name] = $class->name;
+        }
+        return $class_list;
+    }
+
+    public function display_edit_class(UserClass $class): void
+    {
+        $parent = $class->get_parent();
+        assert($parent !== null, "Can't edit a class without a parent");
+        $parent_name = $parent->name;
+
+        $class_edit = SHM_SIMPLE_FORM(make_link("user_class/{$class->name}"), TABLE(
+            ["class" => "form"],
+            TR(TH("Name"), TD(INPUT(["type" => "text", "name" => "name", "value" => $class->name, "disabled" => true]))),
+            TR(TH("Parent"), TD(SHM_SELECT("parent", $this->class_list(exclude: $class->name), [$parent_name]))),
+            TR(TH("Descr."), TD(TEXTAREA(["name" => "description"], $class->description))),
+            TR(TD(["colspan" => 2], SHM_SUBMIT("Save")))
+        ));
+        Ctx::$page->add_block(new Block($class->name, $class_edit, "main", 20));
+    }
+
+    /**
+     * @param array<string,array<string,PermissionMeta>> $metas
+     */
+    public function display_edit_permissions(UserClass $class, array $metas): void
+    {
+        $parent = $class->get_parent();
+        assert($parent !== null, "Can't edit permissions without a parent");
+        $parent_name = $parent->name;
+
+        $tbody = TBODY();
+        foreach ($metas as $ext => $group) {
+            foreach ($group as $permission => $meta) {
+                $tbody->appendChild(TR(
+                    TD($ext),
+                    TD(["title" => $permission], $meta->label),
+                    TD($parent->can($permission)
+                        ? SPAN(["class" => "allowed"], "✔")
+                        : SPAN(["class" => "denied"], "✘")),
+                    TD(SHM_SELECT(
+                        "permissions[$permission]",
+                        [
+                            "null" => $parent->can($permission) ? "✔ Inherit" : "✘ Inherit",
+                            "true" => "✔ Allow",
+                            "false" => "✘ Deny"
+                        ],
+                        [$class->has_own_permission($permission)
+                            ? $class->can($permission)
+                                ? "true"
+                                : "false"
+                            : "null"
+                        ]
+                    ))
+                ));
+            }
+        }
+        $perms_edit = SHM_SIMPLE_FORM(make_link("user_class/{$class->name}/permissions"), TABLE(
+            ["class" => "zebra", "id" => "permission_table"],
+            THEAD(TR(TH("Extension"), TH("Permission"), TH("Parent ($parent_name)"), TH("Setting"))),
+            $tbody,
+            TFOOT(TR(TD(["colspan" => 4], SHM_SUBMIT("Save Permissions"))))
+        ));
+
+        Ctx::$page->add_block(new Block("Edit Permissions", $perms_edit, "main", 30));
+    }
+
+    /**
+     * @param UserClass[] $classes
+     * @param array<string,array<string,PermissionMeta>> $metas
+     */
+    public function display_user_classes(array $classes, array $metas): void
+    {
         $row = TR();
         $row->appendChild(TH("Extension"));
         $row->appendChild(TH("Permission"));
@@ -27,18 +187,19 @@ class PermManagerTheme extends Themelet
             $row->appendChild(TH($n));
         }
         $row->appendChild(TH("Description"));
-        $table->appendChild($row);
+        $thead = THEAD($row);
 
-        foreach ($permissions as $ext => $group) {
-            foreach ($group as $name => $meta) {
+        $tbody = TBODY();
+        foreach ($metas as $ext => $group) {
+            foreach ($group as $permission => $meta) {
                 $row = TR();
                 $row->appendChild(TH($ext));
-                $row->appendChild(TH($meta->label));
+                $row->appendChild(TH(["title" => $permission], $meta->label));
 
                 foreach ($classes as $class) {
-                    $inherited = $class->has_own_permission($name) ? "" : "inherited";
-                    if ($class->can($name)) {
-                        $cell = SPAN(["class" => "allowed $inherited;"], "✔");
+                    $inherited = $class->has_own_permission($permission) ? "" : "inherited";
+                    if ($class->can($permission)) {
+                        $cell = SPAN(["class" => "allowed $inherited"], "✔");
                     } else {
                         $cell = SPAN(["class" => "denied $inherited"], "✘");
                     }
@@ -46,12 +207,16 @@ class PermManagerTheme extends Themelet
                 }
 
                 $row->appendChild(TD(["style" => "text-align: left;"], $meta->help));
-                $table->appendChild($row);
+                $tbody->appendChild($row);
             }
         }
 
-        Ctx::$page->set_title("User Classes");
-        $this->display_navigation();
-        Ctx::$page->add_block(new Block("Classes", $table, "main", 10));
+        $table = TABLE(
+            ["class" => "zebra", "id" => "permission_table"],
+            $thead,
+            $tbody
+        );
+
+        Ctx::$page->add_block(new Block("Permissions Comparison", $table, "main", 20));
     }
 }
