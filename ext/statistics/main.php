shish/shimmie2

commit 384100cbc5a9f053c674c0da0aa70d88de2e93c6

less rawHTML

ext/statistics/theme.php
@@ -7,10 +7,10 @@ namespace Shimmie2;
 use MicroHTML\HTMLElement;
 
 use function MicroHTML\{TABLE,THEAD,TBODY,TR,TH,TD};
+use function MicroHTML\A;
 use function MicroHTML\B;
 use function MicroHTML\DIV;
 use function MicroHTML\emptyHTML;
-use function MicroHTML\rawHTML;
 
 class StatisticsTheme extends Themelet
 {
@@ -39,8 +39,8 @@ class StatisticsTheme extends Themelet
             $rows->appendChild(
                 TR(
                     TD([], $n),
-                    TD([], rawHTML((string)$value)),
-                    TD([], rawHTML('<a class="username" href="'.make_link('user/'.$user).'">'.$user.'</a>'))
+                    TD([], $value),
+                    TD([], A(["class" => "username", "href" => make_link('user/'.$user)], $user)),
                 )
             );
             $n++;

ext/alias_editor/theme.php
@@ -7,7 +7,6 @@ namespace Shimmie2;
 use MicroHTML\HTMLElement;
 
 use function MicroHTML\emptyHTML;
-use function MicroHTML\rawHTML;
 use function MicroHTML\{A,BR,CODE,INPUT};
 
 class AliasEditorTheme extends Themelet
@@ -19,9 +18,22 @@ class AliasEditorTheme extends Themelet
     {
         global $page, $user;
 
-        $info_html = rawHTML("A tag alias replaces a tag with another tag or tags.".BR()."A tag implication (where the old tag stays and adds a new tag) is made by including the old tag in the list of new tags (".CODE("fox")."&nbsp;→&nbsp;".CODE("fox canine").")".BR());
-
-        $html = emptyHTML($info_html, BR(), $table, BR(), $paginator, BR(), A(["href" => make_link("alias/export/aliases.csv", ["download" => "aliases.csv"])], "Download as CSV"));
+        $html = emptyHTML(
+            "A tag alias replaces a tag with another tag or tags.",
+            BR(),
+            "A tag implication (where the old tag stays and adds a new tag) is made by including the old tag in the list of new tags (",
+            CODE("fox"),
+            " → ",
+            CODE("fox canine"),
+            ")",
+            BR(),
+            BR(),
+            $table,
+            BR(),
+            $paginator,
+            BR(),
+            A(["href" => make_link("alias/export/aliases.csv", ["download" => "aliases.csv"])], "Download as CSV")
+        );
 
         $bulk_form = SHM_FORM(make_link("alias/import"), multipart: true);
         $bulk_form->appendChild(

ext/auto_tagger/theme.php
@@ -6,7 +6,10 @@ namespace Shimmie2;
 
 use MicroHTML\HTMLElement;
 
-use function MicroHTML\rawHTML;
+use function MicroHTML\A;
+use function MicroHTML\INPUT;
+use function MicroHTML\P;
+use function MicroHTML\emptyHTML;
 
 class AutoTaggerTheme extends Themelet
 {
@@ -19,25 +22,27 @@ class AutoTaggerTheme extends Themelet
     {
         global $page, $user;
 
-        $can_manage = $user->can(AutoTaggerPermission::MANAGE_AUTO_TAG);
-        $html = "
-            $table
-            $paginator
-			<p><a href='".make_link("auto_tag/export/auto_tag.csv")."' download='auto_tag.csv'>Download as CSV</a></p>
-		";
-
-        $bulk_html = "
-			".make_form(make_link("auto_tag/import"), multipart: true)."
-				<input type='file' name='auto_tag_file'>
-				<input type='submit' value='Upload List'>
-			</form>
-		";
-
         $page->set_title("Auto-Tag List");
         $this->display_navigation();
-        $page->add_block(new Block("Auto-Tag", rawHTML($html)));
-        if ($can_manage) {
-            $page->add_block(new Block("Bulk Upload", rawHTML($bulk_html), "main", 51));
+
+        $page->add_block(new Block("Auto-Tag", emptyHTML(
+            $table,
+            $paginator,
+            P(A(
+                ["href" => make_link("auto_tag/export/auto_tag.csv"), "download" => "auto_tag.csv"],
+                "Download as CSV"
+            ))
+        )));
+
+        if ($user->can(AutoTaggerPermission::MANAGE_AUTO_TAG)) {
+            $page->add_block(new Block("Bulk Upload", SHM_FORM(
+                action: make_link("auto_tag/import"),
+                multipart: true,
+                children: [
+                    INPUT(["type" => "file", "name" => "auto_tag_file"]),
+                    SHM_SUBMIT("Upload List")
+                ]
+            )));
         }
     }
 }

ext/biography/theme.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\{TEXTAREA,rawHTML};
+use function MicroHTML\{TEXTAREA};
 use function MicroHTML\TABLE;
 use function MicroHTML\TD;
 use function MicroHTML\TR;
@@ -13,7 +13,7 @@ class BiographyTheme extends Themelet
 {
     public function display_biography(Page $page, string $bio): void
     {
-        $page->add_block(new Block("About Me", rawHTML(format_text($bio)), "main", 30, "about-me"));
+        $page->add_block(new Block("About Me", format_text($bio), "main", 30, "about-me"));
     }
 
     public function display_composer(Page $page, User $duser, string $bio): void

ext/blocks/main.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\{rawHTML};
-
 final class Blocks extends Extension
 {
     public const KEY = "blocks";
@@ -60,7 +58,7 @@ final class Blocks extends Extension
         foreach ($blocks as $block) {
             $path = implode("/", $event->args);
             if (strlen($path) < 4000 && fnmatch($block['pages'], $path)) {
-                $b = new Block($block['title'], rawHTML($block['content']), $block['area'], (int)$block['priority']);
+                $b = new Block($block['title'], \MicroHTML\rawHTML($block['content']), $block['area'], (int)$block['priority']);
                 $b->is_content = false;
 
                 # Split by comma, trimming whitespaces, and not allowing empty elements.

ext/blocks/theme.php
@@ -10,7 +10,6 @@ use function MicroHTML\TH;
 use function MicroHTML\TD;
 use function MicroHTML\INPUT;
 use function MicroHTML\TEXTAREA;
-use function MicroHTML\rawHTML;
 use function MicroHTML\SELECT;
 use function MicroHTML\OPTION;
 
@@ -47,7 +46,7 @@ class BlocksTheme extends Themelet
                     TD(["colspan" => "13"], TEXTAREA(["rows" => "5", "name" => "content"], $block['content']))
                 ),
                 TR(
-                    TD(["colspan" => "13"], rawHTML("&nbsp;"))
+                    TD(["colspan" => "13"], " ")
                 ),
             ));
         }

ext/blotter/theme.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\rawHTML;
+use function MicroHTML\A;
+use function MicroHTML\BR;
+use function MicroHTML\DIV;
+use function MicroHTML\INPUT;
+use function MicroHTML\LI;
+use function MicroHTML\P;
+use function MicroHTML\SPAN;
+use function MicroHTML\TABLE;
+use function MicroHTML\TBODY;
+use function MicroHTML\TD;
+use function MicroHTML\TEXTAREA;
+use function MicroHTML\TFOOT;
+use function MicroHTML\TH;
+use function MicroHTML\THEAD;
+use function MicroHTML\TR;
+use function MicroHTML\UL;
+use function MicroHTML\emptyHTML;
 
 /**
  * @phpstan-type BlotterEntry array{id:int,entry_date:string,entry_text:string,important:bool}
@@ -17,9 +33,48 @@ class BlotterTheme extends Themelet
     public function display_editor(array $entries): void
     {
         global $page;
-        $html = $this->get_html_for_blotter_editor($entries);
+
+        $tbody = TBODY();
+        foreach ($entries as $entry) {
+            $tbody->appendChild(
+                TR(
+                    TD($entry['entry_date']),
+                    TD($entry['entry_text']),
+                    TD($entry['important'] ? "Yes" : "No"),
+                    TD(
+                        SHM_SIMPLE_FORM(
+                            make_link("blotter/remove"),
+                            INPUT(["type" => "hidden", "name" => "id", "value" => $entry['id']]),
+                            SHM_SUBMIT("Remove")
+                        )
+                    )
+                )
+            );
+        }
+        $html = emptyHTML(
+            TABLE(
+                ["id" => "blotter_entries", "class" => "zebra"],
+                THEAD(
+                    TR(
+                        TH("Date"),
+                        TH("Message"),
+                        TH("Important?"),
+                        TH("Action")
+                    )
+                ),
+                $tbody,
+                TFOOT(
+                    TR(SHM_SIMPLE_FORM(
+                        make_link("blotter/add"),
+                        TD(["colspan" => 2], TEXTAREA(["name" => "entry_text", "rows" => 2])),
+                        TD(INPUT(["type" => "checkbox", "name" => "important"])),
+                        TD(SHM_SUBMIT("Add"))
+                    ))
+                )
+            ),
+        );
         $page->set_title("Blotter Editor");
-        $page->add_block(new Block("Welcome to the Blotter Editor!", rawHTML($html), "main", 10));
+        $page->add_block(new Block("Blotter Editor", $html, "main", 10));
         $this->display_navigation();
     }
 
@@ -28,10 +83,23 @@ class BlotterTheme extends Themelet
      */
     public function display_blotter_page(array $entries): void
     {
-        global $page;
-        $html = $this->get_html_for_blotter_page($entries);
+        global $config, $page;
+
+        $i_color = $config->get_string(BlotterConfig::COLOR, "#FF0000");
+
+        $html = P();
+        foreach ($entries as $entry) {
+            $clean_date = date("Y/m/d", \Safe\strtotime($entry['entry_date']));
+            $entry_text = $entry['entry_text'];
+            $msg = "{$clean_date} - {$entry_text}";
+            if ($entry['important']) {
+                $msg = SPAN(["style" => "color: $i_color;"], $msg);
+            }
+            $html->appendChild(emptyHTML($msg, BR(), BR()));
+        }
+
         $page->set_title("Blotter");
-        $page->add_block(new Block("Blotter Entries", rawHTML($html), "main", 10));
+        $page->add_block(new Block("Blotter Entries", $html, "main", 10));
     }
 
     /**
@@ -40,150 +108,26 @@ class BlotterTheme extends Themelet
     public function display_blotter(array $entries): void
     {
         global $page, $config;
-        $html = $this->get_html_for_blotter($entries);
-        $position = $config->get_string(BlotterConfig::POSITION, "subheading");
-        $page->add_block(new Block(null, rawHTML($html), $position, 20));
-    }
 
-    /**
-     * @param BlotterEntry[] $entries
-     */
-    private function get_html_for_blotter_editor(array $entries): string
-    {
-        global $user;
+        $i_color = $config->get_string(BlotterConfig::COLOR);
+        $position = $config->get_string(BlotterConfig::POSITION);
 
-        /**
-         * Long function name, but at least I won't confuse it with something else ^_^
-         */
-
-        // Add_new stuff goes here.
-        $table_header =  "
-			<tr>
-			<th>Date</th>
-			<th>Message</th>
-			<th>Important?</th>
-			<th>Action</th>
-			</tr>";
-        $add_new = "
-			<tr class='even'>
-			".make_form(make_link("blotter/add"))."
-			<td colspan='2'><textarea style='text-align:left;' name='entry_text' rows='2' /></textarea></td>
-			<td><input type='checkbox' name='important' /></td>
-			<td><input type='submit' value='Add'></td>
-			</form>
-			</tr>";
-
-
-        // Now, time for entries list.
-        $table_rows = "";
-        $num_entries = count($entries);
-        for ($i = 0 ; $i < $num_entries ; $i++) {
-            /**
-             * Add table rows
-             */
-            $id = $entries[$i]['id'];
-            $entry_date = $entries[$i]['entry_date'];
-            $entry_text = $entries[$i]['entry_text'];
-            if ($entries[$i]['important'] == 'Y') {
-                $important = 'Y';
-            } else {
-                $important = 'N';
-            }
-
-            // Add the new table row(s)
-            $table_rows .=
-                "<tr>
-				<td>$entry_date</td>
-				<td>$entry_text</td>
-				<td>$important</td>
-				<td>".make_form(make_link("blotter/remove"), name: "remove$id")."
-				<input type='hidden' name='id' value='$id' />
-				<input type='submit' style='width: 100%;' value='Remove' />
-				</form>
-				</td>
-				</tr>";
-        }
-
-        $html = "
-			<table id='blotter_entries' class='zebra'>
-			<thead>$table_header</thead>
-			<tbody>$add_new</tbody>
-			<tfoot>$table_rows</tfoot>
-			</table>
-
-			<br />
-			<b>Help:</b><br />
-			<blockquote>Add entries to the blotter, and they will be displayed.</blockquote>";
-
-        return $html;
-    }
-
-    /**
-     * @param BlotterEntry[] $entries
-     */
-    private function get_html_for_blotter_page(array $entries): string
-    {
-        /**
-         * This one displays a list of all blotter entries.
-         */
-        global $config;
-        $i_color = $config->get_string(BlotterConfig::COLOR, "#FF0000");
-        $html = "<pre>";
-
-        $num_entries = count($entries);
-        for ($i = 0 ; $i < $num_entries ; $i++) {
-            /**
-             * Blotter entries
-             */
-            // Reset variables:
-            $i_open = "";
-            $i_close = "";
-            //$id = $entries[$i]['id'];
-            $messy_date = $entries[$i]['entry_date'];
-            $clean_date = date("y/m/d", \Safe\strtotime($messy_date));
-            $entry_text = $entries[$i]['entry_text'];
-            if ($entries[$i]['important'] == 'Y') {
-                $i_open = "<span style='color: $i_color;'>";
-                $i_close = "</span>";
-            }
-            $html .= "{$i_open}{$clean_date} - {$entry_text}{$i_close}<br /><br />";
-        }
-        $html .= "</pre>";
-        return $html;
-    }
-
-    /**
-     * @param BlotterEntry[] $entries
-     */
-    private function get_html_for_blotter(array $entries): string
-    {
-        global $config;
-        $i_color = $config->get_string(BlotterConfig::COLOR, "#FF0000");
-        $position = $config->get_string(BlotterConfig::POSITION, "subheading");
-        $entries_list = "";
+        $entries_list = UL();
         foreach ($entries as $entry) {
-            /**
-             * Blotter entries
-             */
-            // Reset variables:
-            $i_open = "";
-            $i_close = "";
-            //$id = $entry['id'];
-            $messy_date = $entry['entry_date'];
-            $clean_date = date("m/d/y", \Safe\strtotime($messy_date));
+            $clean_date = date("m/d/y", \Safe\strtotime($entry['entry_date']));
             $entry_text = $entry['entry_text'];
+            $text = "{$clean_date} - {$entry_text}";
             if ($entry['important'] == 'Y') {
-                $i_open = "<span style='color: #$i_color'>";
-                $i_close = "</span>";
+                $text = SPAN(["style" => "color: $i_color"], $text);
             }
-            $entries_list .= "<li>{$i_open}{$clean_date} - {$entry_text}{$i_close}</li>";
+            $entries_list->appendChild(LI($text));
         }
 
         $pos_break = "";
         $pos_align = "text-align: right; position: absolute; right: 0px;";
 
         if ($position === "left") {
-            $pos_break = "<br />";
+            $pos_break = BR();
             $pos_align = "";
         }
 
@@ -191,22 +135,26 @@ class BlotterTheme extends Themelet
             $out_text = "No blotter entries yet.";
             $in_text = "Empty.";
         } else {
-            $clean_date = date("m/d/y", \Safe\strtotime($entries[0]['entry_date']));
-            $out_text = "Blotter updated: {$clean_date}";
-            $in_text = "<ul>$entries_list</ul>";
+            $out_text = "Blotter updated: " . date("m/d/y", \Safe\strtotime($entries[0]['entry_date']));
+            $in_text = $entries_list;
         }
 
-        $html = "
-			<div id='blotter1' class='shm-blotter1'>
-				<span>$out_text</span>
-				{$pos_break}
-				<span style='{$pos_align}'>
-					<a href='#' id='blotter2-toggle' class='shm-blotter2-toggle'>Show/Hide</a>
-					<a href='".make_link("blotter/list")."'>Show All</a>
-				</span>
-			</div>
-			<div id='blotter2' class='shm-blotter2'>$in_text</div>
-		";
-        return $html;
+        $html = emptyHTML(
+            DIV(
+                ["id" => "blotter1", "class" => "shm-blotter1"],
+                SPAN($out_text),
+                $pos_break,
+                SPAN(
+                    ["style" => $pos_align],
+                    A(["href" => "#", "id" => "blotter2-toggle", "class" => "shm-blotter2-toggle"], "Show/Hide"),
+                    " ",
+                    A(["href" => make_link("blotter/list")], "Show All")
+                ),
+            ),
+            DIV(["id" => "blotter2", "class" => "shm-blotter2"], $in_text)
+        );
+
+        $position = $config->get_string(BlotterConfig::POSITION, "subheading");
+        $page->add_block(new Block(null, $html, $position, 20));
     }
 }

commit 5a6044019a18648f70d643329be25a9a26cb06f3

[statistics] fix html string in tag history field
Fixes a minor oversight introduced in commit 384100cbc5a9f053c674c0da0aa70d88de2e93c6

ext/statistics/main.php
@@ -27,11 +27,7 @@ final class Statistics extends Extension
                 arsort($tallies[0], SORT_NUMERIC);
                 $stats = [];
                 foreach ($tallies[0] as $name => $tag_diff) {
-                    $entries = "";
-                    if (isset($tallies[1][$name])) {
-                        $entries = " <span class='tag_count' title='Total edits'>" . $tallies[1][$name] . "</span>";
-                    }
-                    $stats[$name] = "<span title='Tags changed (ignoring aliases)'>$tag_diff</span>$entries";
+                    $stats[$name] = $this->theme->build_tag_field($tallies[1][$name], $tag_diff);
                 }
                 $tag_table = $this->theme->build_table($stats, "Taggers", "Top $limit taggers", $limit);
             } else {

ext/statistics/theme.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\{A, B, DIV, emptyHTML};
+use function MicroHTML\{A, B, DIV, SPAN, emptyHTML};
 
 use MicroHTML\HTMLElement;
 
@@ -27,7 +27,7 @@ class StatisticsTheme extends Themelet
     }
 
     /**
-     * @param array<string, int|string> $data
+     * @param array<string, int|HTMLElement> $data
      */
     public function build_table(array $data, string $id, string $title, ?int $limit = 10): HTMLElement
     {
@@ -57,6 +57,15 @@ class StatisticsTheme extends Themelet
         return DIV(
             ["id" => "table$id", "class" => "stats-container"],
             $table
+        );
+    }
+
+    public function build_tag_field(int $tally, int $diff): HTMLElement
+    {
+        return emptyHTML(
+            SPAN(["title" => "Tags changed (ignoring aliases) edits"], $diff),
+            " ",
+            SPAN(["class" => "tag_count", "title" => "Total edits"], $tally)
         );
     }
 }