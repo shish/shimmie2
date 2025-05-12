9f6f1c7cf55b5bcc751cb7d8d09e3881720464e6
xifize <xifiz@gmail.com>
2025-05-12 16:31:27 -0400

    [image-description] Dummy description with formatting

--- ext/description/theme.php
+++ ext/description/theme.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
+use function MicroHTML\{rawHTML};
+
 use MicroHTML\HTMLElement;
 
 class ImageDescriptionTheme extends Themelet
 {
     public function get_description_editor_html(Image $image): HTMLElement
-    {
+        {
+        $raw_description = "Here is [i]sample[/i] description with [b]formatting[/b].";
+        $tfe = send_event(new TextFormattingEvent($raw_description));
+
         return SHM_POST_INFO(
             "Description",
-            "Put description here",
+            rawHTML($tfe->formatted),
             null
         );
     }
