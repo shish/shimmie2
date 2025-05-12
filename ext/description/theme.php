b5231fdefa2bfda7f28f80e2ce32f5033199577c
xifize <xifiz@gmail.com>
2025-05-12 16:24:53 -0400

    [image-decription] Added dummy description to post info

--- ext/description/theme.php
+++ ext/description/theme.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
+use MicroHTML\HTMLElement;
+
 class ImageDescriptionTheme extends Themelet
 {
+    public function get_description_editor_html(Image $image): HTMLElement
+    {
+        return SHM_POST_INFO(
+            "Description",
+            "Put description here",
+            null
+        );
+    }
 }
