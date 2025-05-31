2ea49d505362f5b4c852a8f8b942a8c541e07309
Shish <shish@shishnet.org>
2025-05-31 14:44:01 +0100

    consistent cookie api

--- ext/relationships/script.js
+++ ext/relationships/script.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
         $(".shm-relationships-parent-thumbs").slideToggle("fast", function () {
             if ($(".shm-relationships-parent-thumbs").is(":hidden")) {
                 $(".shm-relationships-parent-toggle").text("show »");
-                Cookies.set("ui-relationships-parent-hidden", "true");
+                shm_cookie_set("ui-relationships-parent-hidden", "true");
             } else {
                 $(".shm-relationships-parent-toggle").text("« hide");
-                Cookies.set("ui-relationships-parent-hidden", "false");
+                shm_cookie_set("ui-relationships-parent-hidden", "false");
             }
         });
     });
-    if (Cookies.get("ui-relationships-parent-hidden") === "true") {
+    if (shm_cookie_get("ui-relationships-parent-hidden") === "true") {
         $(".shm-relationships-parent-thumbs").hide();
         $(".shm-relationships-parent-toggle").text("show »");
     }
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
         $(".shm-relationships-child-thumbs").slideToggle("fast", function () {
             if ($(".shm-relationships-child-thumbs").is(":hidden")) {
                 $(".shm-relationships-child-toggle").text("show »");
-                Cookies.set("ui-relationships-child-hidden", "true");
+                shm_cookie_set("ui-relationships-child-hidden", "true");
             } else {
                 $(".shm-relationships-child-toggle").text("« hide");
-                Cookies.set("ui-relationships-child-hidden", "false");
+                shm_cookie_set("ui-relationships-child-hidden", "false");
             }
         });
     });
-    if (Cookies.get("ui-relationships-child-hidden") === "true") {
+    if (shm_cookie_get("ui-relationships-child-hidden") === "true") {
         $(".shm-relationships-child-thumbs").hide();
         $(".shm-relationships-child-toggle").text("show »");
     }
