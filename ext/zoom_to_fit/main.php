1e6975ddd76a495574245e2d2e6a3c18093c0667
Shish <shish@shishnet.org>
2025-08-25 03:16:07 +0100

    [handle_pixel] split zoom behaviours
    Having two zoom methods at once is a little weird, and I'm thinking to add a third which would be very weird - letting the admin choose which method (or which combination, if they really want a combination) seems better

---
+++ ext/zoom_to_fit/main.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+use function MicroHTML\{FORM, OPTION, SELECT};
+
+final class ZoomToFit extends Extension
+{
+    public const KEY = "zoom_to_fit";
+
+    public function onImageAdminBlockBuilding(ImageAdminBlockBuildingEvent $event): void
+    {
+        if (str_starts_with($event->image->get_mime()->base, "image/")) {
+            if ($event->context === "view") {
+                $event->add_part(FORM(
+                    SELECT(
+                        ["class" => "shm-zoomer"],
+                        OPTION(["value" => "full"], "Full Size"),
+                        OPTION(["value" => "width"], "Fit Width"),
+                        OPTION(["value" => "both"], "Fit Both"),
+                    )
+                ), 19);
+            }
+        }
+    }
+}
