97f385cc513bce59f6b40b7db2127a7736099346
Shish <shish@shishnet.org>
2025-08-26 15:34:42 +0100

    [core] add "format support" as its own extension category

--- ext/handle_archive/info.php
+++ ext/handle_archive/info.php
@@ -9,10 +9,10 @@ final class ArchiveFileHandlerInfo extends ExtensionInfo
     public const KEY = "handle_archive";
 
     public string $key = self::KEY;
-    public string $name = "Handle Archives";
+    public string $name = "ZIP Archives";
     public string $url = self::SHIMMIE_URL;
     public array $authors = self::SHISH_AUTHOR;
-    public string $description = "Allow users to upload archives (zip, etc)";
+    public string $description = "Upload multiple files in one go";
     public ExtensionCategory $category = ExtensionCategory::FILE_HANDLING;
     public ?string $documentation =
         "Note: requires exec() access and an external unzip command
--- ext/handle_ico/info.php
+++ ext/handle_ico/info.php
@@ -9,9 +9,9 @@ final class IcoFileHandlerInfo extends ExtensionInfo
     public const KEY = "handle_ico";
 
     public string $key = self::KEY;
-    public string $name = "Handle ICO";
+    public string $name = "Icon Files";
     public string $url = self::SHIMMIE_URL;
     public array $authors = self::SHISH_AUTHOR;
-    public ExtensionCategory $category = ExtensionCategory::FILE_HANDLING;
-    public string $description = "Handle windows icons";
+    public ExtensionCategory $category = ExtensionCategory::FORMAT_SUPPORT;
+    public string $description = "Handle windows ICO, ANI, CUR, etc";
 }
