692a7f48005dae35cd2d15622ff7dc008e01940b
Shish <shish@shishnet.org>
2025-08-26 15:58:34 +0100

    [transcode-image] move transcode-on-upload to its own extension

--- ext/transcode_image/config.php
+++ ext/transcode_image/config.php
@@ -12,9 +12,6 @@ final class TranscodeImageConfig extends ConfigGroup
     #[ConfigMeta("Enable GET args", ConfigType::BOOL, default: false)]
     public const GET_ENABLED = "transcode_get_enabled";
 
-    #[ConfigMeta("Transcode on upload", ConfigType::BOOL, default: false)]
-    public const UPLOAD = "transcode_upload";
-
     #[ConfigMeta("Engine", ConfigType::STRING, default: 'gd', options: ["GD" => "gd", "ImageMagick" => "convert"])]
     public const ENGINE = "transcode_engine";
 
@@ -23,48 +20,4 @@ final class TranscodeImageConfig extends ConfigGroup
 
     #[ConfigMeta("Alpha conversion color", ConfigType::STRING, default: "#00000000", input: ConfigInput::COLOR)]
     public const ALPHA_COLOR = "transcode_alpha_color";
-
-    #[ConfigMeta("MIME checks", ConfigType::BOOL, default: false)]
-    public const MIME_CHECK_ENABLED = "mime_check_enabled";
-
-    #[ConfigMeta("Allowed MIMEs", ConfigType::ARRAY, default: [], options: "Shimmie2\TranscodeImageConfig::get_mime_options")]
-    public const ALLOWED_MIME_STRINGS = "allowed_mime_strings";
-
-    /**
-     * @return array<string, MimeType>
-     */
-    public static function get_mime_options(): array
-    {
-        $output = [];
-        foreach (DataHandlerExtension::get_all_supported_mimes() as $mime) {
-            $output[MimeMap::get_name_for_mime($mime)] = $mime;
-        }
-        return $output;
-    }
-
-    /**
-     * @return array<string, ConfigMeta>
-     */
-    public function get_config_fields(): array
-    {
-        $fields = parent::get_config_fields();
-
-        $engine = MediaEngine::from(Ctx::$config->get(TranscodeImageConfig::ENGINE));
-        foreach (TranscodeImage::INPUT_MIMES as $display => $mime) {
-            $mime = new MimeType($mime);
-            if (MediaEngine::is_input_supported($engine, $mime)) {
-                $outputs = [];
-                foreach (TranscodeImage::get_supported_output_mimes($engine, $mime) as $name => $output_mime) {
-                    $outputs[$name] = (string)$output_mime;
-                }
-                $fields[TranscodeImage::get_mapping_name($mime)] = new ConfigMeta(
-                    $display,
-                    ConfigType::STRING,
-                    options: $outputs
-                );
-            }
-        }
-
-        return $fields;
-    }
 }
