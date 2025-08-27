c296f61352ab3ed3aae30179c02a5bdbb601019f
Shish <shish@shishnet.org>
2025-08-27 18:50:32 +0100

    [core] allow exceptions to have an $html property for user-friendlier error message rendering

--- core/Exceptions/SCoreException.php
+++ core/Exceptions/SCoreException.php
@@ -12,7 +12,8 @@ class SCoreException extends \RuntimeException
     public int $http_code = 500;
 
     public function __construct(
-        public string $error
+        public string $error,
+        public ?\MicroHTML\HTMLElement $html = null,
     ) {
         parent::__construct($error);
     }
