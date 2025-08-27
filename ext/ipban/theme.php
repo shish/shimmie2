7fad40842847eee3c119560f5e809b3cfd66dfeb
Shish <shish@shishnet.org>
2025-08-27 12:54:54 +0100

    [misc] tidy

--- ext/ipban/theme.php
+++ ext/ipban/theme.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\{A, emptyHTML};
+use function MicroHTML\{A, BR, emptyHTML};
 
 use MicroHTML\HTMLElement;
 
@@ -13,10 +13,13 @@ class IPBanTheme extends Themelet
     public function display_bans(HTMLElement $table, HTMLElement $paginator): void
     {
         Ctx::$page->set_title("IP Bans");
-        $this->display_navigation();
-        Ctx::$page->add_block(new Block(null, emptyHTML(
+        $this->display_navigation(extra: emptyHTML(
+            BR(),
             A(["href" => make_link("ip_ban/list", ["r__size" => "1000000"])], "Show All Active"),
-            A(["href" => make_link("ip_ban/list", ["r_all" => "on", "r__size" => "1000000"])], "Show EVERYTHING"),
+            BR(),
+            A(["href" => make_link("ip_ban/list", ["r_all" => "on", "r__size" => "1000000"])], "Show EVERYTHING")
+        ));
+        Ctx::$page->add_block(new Block(null, emptyHTML(
             $table,
             $paginator
         )));
