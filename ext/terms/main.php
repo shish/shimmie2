65d6984c9d82ca4d236d9062f3eb7dbe0bae230e
Shish <shish@shishnet.org>
2025-08-28 14:48:02 +0100

    [terms] style the T&C popup to match the rest of the site

--- ext/terms/main.php
+++ ext/terms/main.php
@@ -18,10 +18,10 @@ final class Terms extends Extension
         } else {
             // run on all pages unless any of:
             // - user is logged in
-            // - cookie exists
+            // - terms are already accepted
             // - user is viewing the wiki (because that's where the privacy policy / TOS / etc are)
             if (
-                Ctx::$user->is_anonymous()
+                !Ctx::$user->can(TermsPermission::SKIP_TERMS)
                 && !$page->get_cookie('accepted_terms')
                 && !$event->page_starts_with("wiki")
             ) {
