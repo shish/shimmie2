b6aa58f69a4f2e9f5ecf38232c68dc396847d79b
Shish <shish@shishnet.org>
2025-10-02 17:02:36 +0100

    [log_otlp] config name

--- ext/log_otlp/config.php
+++ ext/log_otlp/config.php
@@ -7,6 +7,7 @@ namespace Shimmie2;
 final class LogOTLPConfig extends ConfigGroup
 {
     public const KEY = "log_otlp";
+    public ?string $title = "Log OTLP";
 
     #[ConfigMeta("host:port", ConfigType::STRING, default: "http://localhost:4318/v1/logs")]
     public const HOST = "log_otlp";
