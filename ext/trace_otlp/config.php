b2bbfc84b5b493e5bb42088399ba69c29a031f5b
Shish <shish@shishnet.org>
2025-10-07 16:50:29 +0100

    [trace_otlp] new ext (replacing trace_chrome)

---
+++ ext/trace_otlp/config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+final class TraceOTLPConfig extends ConfigGroup
+{
+    public const KEY = "trace_chrome";
+    public ?string $title = "Tracing (OTLP)";
+
+    #[ConfigMeta("Trace Threshold (ms)", ConfigType::INT, default: 1000)]
+    public const TRACE_THRESHOLD = 'trace_otlp_threshold';
+}
