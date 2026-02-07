8c8e1d917454c17bf79e6c46e4c5a26617f3b424
discomrade <83621080+discomrade@users.noreply.github.com>
2026-02-07 22:58:37 +0000

    [pools] add javascript null check to avoid error

--- ext/pools/script.js
+++ ext/pools/script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document
-        .getElementById("order_pool")
-        .addEventListener("change", function () {
+    const order_pool = document.getElementById("order_pool");
+    if (order_pool) {
+        order_pool.addEventListener("change", function () {
             let val = this.options[this.selectedIndex].value;
             shm_cookie_set("ui-order-pool", val);
             window.location.href = "";
         });
+    }
 });
