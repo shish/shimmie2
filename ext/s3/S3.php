51ba97e3c6b307c7334ffbedcfbcb36858a41a81
Shish <shish@shishnet.org>
2026-02-14 23:40:26 +0000

    [s3] make it work with modern APIs

--- ext/s3/S3.php
+++ ext/s3/S3.php
@@ -30,36 +30,49 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+class S3Exception extends \Exception
+{
+    /** @param array<string, mixed> $context */
+    public function __construct(
+        string $message,
+        int $code = 0,
+        private array $context = [],
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /** @return array<string, mixed> */
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+}
+
 class S3
 {
     private string $access_key;
     private string $secret_key;
     private string $endpoint;
-    /** @var array<int,mixed> */
-    private array $curl_opts;
+    private string $region;
 
-    public function __construct(string $access_key, string $secret_key, string $endpoint = 's3.amazonaws.com')
-    {
+    public function __construct(
+        string $access_key,
+        string $secret_key,
+        string $endpoint = 's3.amazonaws.com',
+        string $region = 'auto',
+    ) {
+        // if endpoint doesn't start with http:// or https://, prepend https://
+        // this allows users to specify endpoints like "s3.amazonaws.com"
+        // without needing to include the protocol
+        if (!preg_match('/^https?:\/\//', $endpoint)) {
+            $endpoint = 'https://' . $endpoint;
+        }
+
         $this->access_key = $access_key;
         $this->secret_key = $secret_key;
         $this->endpoint = $endpoint;
-
-        if (!preg_match('/^https?:\/\//', $endpoint)) {
-            throw new \InvalidArgumentException('Endpoint must start with http:// or https://');
-        }
-
-        $this->curl_opts = [
-            CURLOPT_CONNECTTIMEOUT => 30,
-            CURLOPT_LOW_SPEED_LIMIT => 1,
-            CURLOPT_LOW_SPEED_TIME => 30
-        ];
-    }
-
-    /** @param array<int,mixed> $curl_opts */
-    public function useCurlOpts(array $curl_opts): S3
-    {
-        $this->curl_opts = $curl_opts;
-        return $this;
+        $this->region = $region;
     }
 
     /** @param array<string,string> $headers */
@@ -67,10 +80,9 @@ class S3
     {
         $uri = "$bucket/$path";
 
-        $request = (new S3Request('PUT', $this->endpoint, $uri))
+        $request = (new S3Request('PUT', $this->endpoint, $uri, $this->region))
             ->setFileContents($file)
             ->setHeaders($headers)
-            ->useCurlOpts($this->curl_opts)
             ->sign($this->access_key, $this->secret_key);
 
         return $request->getResponse();
@@ -81,9 +93,8 @@ class S3
     {
         $uri = "$bucket/$path";
 
-        $request = (new S3Request('HEAD', $this->endpoint, $uri))
+        $request = (new S3Request('HEAD', $this->endpoint, $uri, $this->region))
             ->setHeaders($headers)
-            ->useCurlOpts($this->curl_opts)
             ->sign($this->access_key, $this->secret_key);
 
         return $request->getResponse();
@@ -99,9 +110,8 @@ class S3
     ): S3Response {
         $uri = "$bucket/$path";
 
-        $request = (new S3Request('GET', $this->endpoint, $uri))
+        $request = (new S3Request('GET', $this->endpoint, $uri, $this->region))
             ->setHeaders($headers)
-            ->useCurlOpts($this->curl_opts)
             ->sign($this->access_key, $this->secret_key);
 
         return $request->getResponse();
@@ -112,9 +122,8 @@ class S3
     {
         $uri = "$bucket/$path";
 
-        $request = (new S3Request('DELETE', $this->endpoint, $uri))
+        $request = (new S3Request('DELETE', $this->endpoint, $uri, $this->region))
             ->setHeaders($headers)
-            ->useCurlOpts($this->curl_opts)
             ->sign($this->access_key, $this->secret_key);
 
         return $request->getResponse();
@@ -123,9 +132,8 @@ class S3
     /** @param array<string,string> $headers */
     public function getBucket(string $bucket, array $headers = []): S3Response
     {
-        $request = (new S3Request('GET', $this->endpoint, $bucket))
+        $request = (new S3Request('GET', $this->endpoint, $bucket, $this->region))
             ->setHeaders($headers)
-            ->useCurlOpts($this->curl_opts)
             ->sign($this->access_key, $this->secret_key);
 
         return $request->getResponse();
@@ -137,48 +145,33 @@ class S3Request
     /** @var array<string, string> */
     private array $headers;
     private \CurlHandle $curl;
-    private S3Response $response;
+    private string $body_content = '';
 
     public function __construct(
         private string $action,
         private string $endpoint,
-        private string $uri
+        private string $uri,
+        private string $region = 'auto',
     ) {
+        $host = parse_url($this->endpoint, PHP_URL_HOST);
+        if (!is_string($host)) {
+            throw new \InvalidArgumentException("Invalid endpoint URL: $endpoint");
+        }
+
         $this->headers = [
-            'Content-MD5' => '',
-            'Content-Type' => '',
-            'Date' => gmdate('D, d M Y H:i:s T'),
-            'Host' => $this->endpoint
+            'Host' => $host
         ];
 
         $this->curl = curl_init();
-        $this->response = new S3Response();
     }
 
     /**
-     * @param resource|string $file The file to send
+     * @param string $file The file to send
      */
-    public function setFileContents($file): S3Request
+    public function setFileContents(string $file): S3Request
     {
-        if (is_resource($file)) {
-            $hash_ctx = hash_init('md5');
-            $length = hash_update_stream($hash_ctx, $file);
-            $md5 = hash_final($hash_ctx, true);
-
-            rewind($file);
-
-            curl_setopt($this->curl, CURLOPT_PUT, true);
-            curl_setopt($this->curl, CURLOPT_INFILE, $file);
-            curl_setopt($this->curl, CURLOPT_INFILESIZE, $length);
-        } elseif (is_string($file)) {
-            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $file);
-            $md5 = md5($file, true);
-        } else {
-            throw new \InvalidArgumentException('Invalid file type');
-        }
-
-        $this->headers['Content-MD5'] = base64_encode($md5);
-
+        $this->body_content = $file;
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $file);
         return $this;
     }
 
@@ -193,35 +186,58 @@ class S3Request
 
     public function sign(string $access_key, string $secret_key): S3Request
     {
-        $canonical_amz_headers = $this->getCanonicalAmzHeaders();
+        // AWS Signature Version 4
+        $timestamp = gmdate('Ymd\THis\Z');
+        $date = gmdate('Ymd');
 
-        $string_to_sign = '';
-        $string_to_sign .= "{$this->action}\n";
-        $string_to_sign .= "{$this->headers['Content-MD5']}\n";
-        $string_to_sign .= "{$this->headers['Content-Type']}\n";
-        $string_to_sign .= "{$this->headers['Date']}\n";
+        $this->headers['x-amz-date'] = $timestamp;
+        $this->headers['x-amz-content-sha256'] = hash('sha256', $this->body_content);
 
-        if (count($canonical_amz_headers) > 0) {
-            $string_to_sign .= implode("\n", $canonical_amz_headers) . "\n";
+        // Step 1: Create canonical request
+        $canonical_uri = '/' . $this->uri;
+        $canonical_querystring = '';
+        $canonical_headers = '';
+        $signed_headers = '';
+
+        // Sort headers and build canonical headers
+        ksort($this->headers);
+        $header_names = [];
+        foreach ($this->headers as $key => $value) {
+            $key_lower = strtolower($key);
+            $header_names[] = $key_lower;
+            $canonical_headers .= $key_lower . ':' . trim($value) . "\n";
         }
+        $signed_headers = implode(';', $header_names);
 
-        $string_to_sign .= "/{$this->uri}";
+        $payload_hash = $this->headers['x-amz-content-sha256'];
 
-        $signature = base64_encode(
-            hash_hmac('sha1', $string_to_sign, $secret_key, true)
-        );
+        $canonical_request = $this->action . "\n" .
+            $canonical_uri . "\n" .
+            $canonical_querystring . "\n" .
+            $canonical_headers . "\n" .
+            $signed_headers . "\n" .
+            $payload_hash;
 
-        $this->headers['Authorization'] = "AWS $access_key:$signature";
+        // Step 2: Create string to sign
+        $algorithm = 'AWS4-HMAC-SHA256';
+        $credential_scope = $date . '/' . $this->region . '/s3/aws4_request';
+        $string_to_sign = $algorithm . "\n" .
+            $timestamp . "\n" .
+            $credential_scope . "\n" .
+            hash('sha256', $canonical_request);
 
-        return $this;
-    }
+        // Step 3: Calculate signature
+        $k_date = hash_hmac('sha256', $date, 'AWS4' . $secret_key, true);
+        $k_region = hash_hmac('sha256', $this->region, $k_date, true);
+        $k_service = hash_hmac('sha256', 's3', $k_region, true);
+        $k_signing = hash_hmac('sha256', 'aws4_request', $k_service, true);
+        $signature = hash_hmac('sha256', $string_to_sign, $k_signing);
 
-    /**
-     * @param array<int,mixed> $curl_opts
-     */
-    public function useCurlOpts(array $curl_opts): S3Request
-    {
-        curl_setopt_array($this->curl, $curl_opts);
+        // Step 4: Add authorization header
+        $this->headers['Authorization'] = $algorithm . ' ' .
+            'Credential=' . $access_key . '/' . $credential_scope . ', ' .
+            'SignedHeaders=' . $signed_headers . ', ' .
+            'Signature=' . $signature;
 
         return $this;
     }
@@ -237,18 +253,12 @@ class S3Request
         );
 
         curl_setopt_array($this->curl, [
-            CURLOPT_USERAGENT => 'ericnorris/amazon-s3-php',
+            CURLOPT_USERAGENT => 'shish/shimmie2',
             CURLOPT_URL => "{$this->endpoint}/{$this->uri}",
             CURLOPT_HTTPHEADER => $http_headers,
-            CURLOPT_HEADER => false,
-            CURLOPT_RETURNTRANSFER => false,
+            CURLOPT_HEADER => true,
+            CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_WRITEFUNCTION => [
-                $this->response, '__curlWriteFunction'
-            ],
-            CURLOPT_HEADERFUNCTION => [
-                $this->response, '__curlHeaderFunction'
-            ]
         ]);
 
         switch ($this->action) {
@@ -266,98 +276,68 @@ class S3Request
                 break;
         }
 
-        $success = curl_exec($this->curl);
-        $this->response->finalize($this->curl);
+        $response_raw = curl_exec($this->curl);
+
+        if ($response_raw === false) {
+            $error_msg = curl_error($this->curl);
+            $error_code = curl_errno($this->curl);
+            curl_close($this->curl);
+            throw new S3Exception("cURL error: $error_msg", $error_code);
+        }
+        if ($response_raw === true) {
+            // This can happen with HEAD requests where CURLOPT_NOBODY is true
+            $response_raw = '';
+        }
+
+        $header_size = curl_getinfo($this->curl, CURLINFO_HEADER_SIZE);
+        $http_code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
         curl_close($this->curl);
 
-        return $this->response;
-    }
+        $header_text = substr($response_raw, 0, $header_size);
+        $body = substr($response_raw, $header_size);
 
-    /**
-     * @return array<string,string>
-     */
-    private function getCanonicalAmzHeaders(): array
-    {
-        $canonical_amz_headers = [];
-
-        foreach ($this->headers as $header => $value) {
-            $header = trim(strtolower($header));
-            $value = trim($value);
-
-            if (strpos($header, 'x-amz-') === 0) {
-                $canonical_amz_headers[$header] = "$header:$value";
+        // Parse headers
+        $headers = [];
+        $header_lines = explode("\r\n", $header_text);
+        foreach ($header_lines as $line) {
+            $parts = explode(':', $line, 2);
+            if (count($parts) === 2) {
+                $headers[trim($parts[0])] = trim($parts[1]);
             }
         }
 
-        ksort($canonical_amz_headers);
+        // Check for HTTP errors
+        if ($http_code >= 300) {
+            $content_type = $headers['Content-Type'] ?? '';
+            $error_message = "S3 request failed with HTTP code $http_code";
+            $context = ['http_code' => $http_code, 'headers' => $headers];
 
-        return $canonical_amz_headers;
+            if (strpos($content_type, 'application/xml') !== false && $body) {
+                $xml = simplexml_load_string($body);
+                if ($xml) {
+                    $error_code = (string)($xml->Code ?? 'Unknown');
+                    $error_message = (string)($xml->Message ?? $error_message);
+                    $context['s3_error_code'] = $error_code;
+                    if (isset($xml->Resource)) {
+                        $context['resource'] = (string)$xml->Resource;
+                    }
+                }
+            }
+
+            throw new S3Exception($error_message, $http_code, $context);
+        }
+
+        return new S3Response($http_code, $headers, $body);
     }
-
 }
 
 class S3Response
 {
-    /** @var array{"code":int|string, "message":string, "resource"?:string}|null */
-    public ?array $error;
-    public ?int $code;
-    /** @var array<string,string> */
-    public array $headers;
-    public string $body;
-
-    public function __construct()
-    {
-        $this->error = null;
-        $this->code = null;
-        $this->headers = [];
-        $this->body = "";
-    }
-
-    public function __curlWriteFunction(\CurlHandle $ch, string $data): int
-    {
-        $this->body .= $data;
-        return strlen($data);
-    }
-
-    public function __curlHeaderFunction(\CurlHandle $ch, string $data): int
-    {
-        $header = explode(':', $data);
-
-        if (count($header) === 2) {
-            list($key, $value) = $header;
-            $this->headers[$key] = trim($value);
-        }
-
-        return strlen($data);
-    }
-
-    public function finalize(\CurlHandle $ch): void
-    {
-        if (curl_errno($ch) || curl_error($ch)) {
-            $this->error = [
-                'code' => curl_errno($ch),
-                'message' => curl_error($ch),
-            ];
-        } else {
-            $this->code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-            if ($this->code > 300 && $content_type === 'application/xml') {
-                $response = simplexml_load_string($this->body);
-
-                if ($response) {
-                    $error = [
-                        'code' => (string)$response->Code,
-                        'message' => (string)$response->Message,
-                    ];
-
-                    if (isset($response->Resource)) {
-                        $error['resource'] = (string)$response->Resource;
-                    }
-
-                    $this->error = $error;
-                }
-            }
-        }
+    public function __construct(
+        public int $code,
+        /** @var array<string,string> */
+        public array $headers,
+        public string $body,
+    ) {
     }
 }
--- ext/s3/config.php
+++ ext/s3/config.php
@@ -18,6 +18,9 @@ final class S3Config extends ConfigGroup
     #[ConfigMeta("Endpoint", ConfigType::STRING, default: "s3.amazonaws.com")]
     public const ENDPOINT = 's3_endpoint';
 
+    #[ConfigMeta("Region", ConfigType::STRING, default: "auto", advanced: true)]
+    public const REGION = 's3_region';
+
     #[ConfigMeta("Image bucket", ConfigType::STRING)]
     public const IMAGE_BUCKET = 's3_image_bucket';
 }
