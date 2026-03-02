54b5fa9a3562ff1bfb1004513003e3566d150935
Saetron <113186192+Saetron@users.noreply.github.com>
2026-03-02 15:16:25 +0100

    Fix broken link

--- ext/automatic1111_tagger/info.php
+++ ext/automatic1111_tagger/info.php
@@ -9,7 +9,7 @@ final class Automatic1111TaggerInfo extends ExtensionInfo
     public const KEY = "automatic1111_tagger";
 
     public string $name = "AI Tagger";
-    public array $authors = ["Saetron" => "https://github.com/Saetron/Shimmie2-AI-Tagger"];
+    public array $authors = ["Saetron" => "https://github.com/Saetron"];
     public string $license = self::LICENSE_MIT;
     public string $description = "Connects to the Automatic1111 API to interrogate images and suggest tags.";
     public ?string $documentation = "Adds an 'Interrogate' button to the posts overview, sending images to the Automatic1111 API for tag suggestions. Requires 'wd-v1-4-moat-tagger.v2' to function";
